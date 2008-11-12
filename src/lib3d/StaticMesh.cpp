3ebad2c7c4a05eaffe7ffb0d8b165ca2373aa1d9
twiemann <twiemann@bea721bf-f9f1-0310-a2d5-e0bfdc8dd81e>
2008-11-12 15:03:30 +0000

    Repaired libmc and lib3d
    git-svn-id: http://kos.informatik.uni-osnabrueck.de/software/meshing@2375 bea721bf-f9f1-0310-a2d5-e0bfdc8dd81e

--- src/lib3d/StaticMesh.cpp
+++ src/lib3d/StaticMesh.cpp
@@ -48,15 +48,15 @@ StaticMesh::StaticMesh(const StaticMesh &o){
 	vertices = new float[3 * o.number_of_vertices];
 	colors = new float[3 * o.number_of_vertices];
 
-	indices = new unsigned int[3 * number_of_faces];
+	indices = new unsigned int[3 * o.number_of_faces];
 
-	for(int i = 0; i < 3 * number_of_vertices; i++){
+	for(int i = 0; i < 3 * o.number_of_vertices; i++){
 		normals[i] = o.normals[i];
 		vertices[i] = o.vertices[i];
 		colors[i] = o.colors[i];
 	}
 
-	for(int i = 0; i < 3 * number_of_faces; i++){
+	for(int i = 0; i < 3 * o.number_of_faces; i++){
 		indices[i] = o.indices[i];
 	}
 
@@ -76,7 +76,7 @@ void StaticMesh::compileDisplayList(){
 		glVertexPointer(3, GL_FLOAT, 0, vertices);
 		glNormalPointer(GL_FLOAT, 0, normals);
 		glColorPointer(3, GL_FLOAT, 0, colors);
-		glDrawElements(GL_TRIANGLES, number_of_faces, GL_UNSIGNED_INT, indices);
+		glDrawElements(GL_TRIANGLES, 3 * number_of_faces, GL_UNSIGNED_INT, indices);
 		glEndList();
 
 	}
@@ -85,8 +85,155 @@ void StaticMesh::compileDisplayList(){
 
 void StaticMesh::load(string filename){
 
+	ifstream in;
+
+	PlyHeaderDescription head;
+	PlyVertexDescription vertex_dcr;
+	PlyFaceDescription face_dcr;
+
+	PlyFace ply_face;
+	PlyVertex ply_vertex;
+
+	in.open(filename.c_str(), fstream::in | fstream::binary);
+
+	in.read( (char*)&head, sizeof(head));
+	in.read( (char*)&vertex_dcr, sizeof(vertex_dcr));
+	in.read( (char*)&face_dcr, sizeof(face_dcr));
+
+	char* buffer = "end_header\n";
+	char dummy[20];
+	in.read( dummy, strlen(buffer));
+
+	//Save no. of vertices and faces
+	number_of_vertices = vertex_dcr.count;
+	number_of_faces = face_dcr.count;
+
+	//Create Arrays
+	if(normals != 0) delete[] normals;
+	if(vertices != 0) delete[] vertices;
+	if(colors != 0) delete[] colors;
+	if(indices != 0) delete[] indices;
+
+	normals = new float[3 * number_of_vertices];
+	vertices = new float[3 * number_of_vertices];
+	colors = new float[3 * number_of_vertices];
+
+	indices = new unsigned int[3 * number_of_faces];
+
+	for(unsigned int i = 0; i < vertex_dcr.count; i++){
+
+		in.read( (char*)&ply_vertex, sizeof(PlyVertex));
+
+		vertices[3 * i    ] = ply_vertex.x;
+		vertices[3 * i + 1] = ply_vertex.y;
+		vertices[3 * i + 2] = ply_vertex.z;
+
+		normals [3 * i    ] = ply_vertex.nx;
+		normals [3 * i + 1] = ply_vertex.ny;
+		normals [3 * 1 + 2] = ply_vertex.nz;
+
+		colors  [3 * i    ] = ply_vertex.r;
+		colors  [3 * i + 1] = ply_vertex.g;
+		colors  [3 * i + 2] = ply_vertex.b;
+
+	}
+
+	for(unsigned int i = 0; i < face_dcr.count; i++){
+		in.read( (char*)&ply_face, sizeof(ply_face));
+		for(int j = 0; j < 3; j++) indices[3 * i + j] = ply_face.indices[j];
+	}
+
+	finalized = true;
+	compileDisplayList();
+
 }
 
 void StaticMesh::save(string filename){
+
+	if(finalized){
+		ofstream out;
+		out.open(filename.c_str(), fstream::out | fstream::binary);
+
+		char* comment = "comment c\n";
+
+		//*********** WRITE PLY HEADER ***************
+
+		PlyHeaderDescription header_dcr;
+		PlyVertexDescription vertex_dcr;
+		PlyFaceDescription face_dcr;
+
+		//Setting up header
+		strcpy(header_dcr.ply, "ply");
+		strcpy(header_dcr.format, PLY_LITTLE_ENDIAN);
+		strcpy(header_dcr.comment, comment);
+
+		//Setting up vertex descripten
+		strcpy(vertex_dcr.element, "element vertex ");
+		strcpy(vertex_dcr.property_x, "property float x\n");
+		strcpy(vertex_dcr.property_y, "property float y\n");
+		strcpy(vertex_dcr.property_z, "property float z\n");
+		strcpy(vertex_dcr.property_nx, "property float nx\n");
+		strcpy(vertex_dcr.property_ny, "property float ny\n");
+		strcpy(vertex_dcr.property_nz, "property float nz\n");
+		vertex_dcr.count = 0;
+
+		//Setting up face desription
+		strcpy(face_dcr.face, "face ");
+		strcpy(face_dcr.property, "property list uchar int vertex_index\n");
+		face_dcr.count = 3;
+
+		//Local variables
+		PlyVertex ply_vertex;
+		PlyFace ply_face;
+
+		//Set vertex and face count
+		vertex_dcr.count = number_of_vertices;
+		face_dcr.count = number_of_faces;
+		ply_face.vertexCount = 0;
+
+		//Write header
+		out.write( (char*)&header_dcr, sizeof(header_dcr));
+		out.write( (char*)&vertex_dcr, sizeof(vertex_dcr));
+		out.write( (char*)&face_dcr, sizeof(face_dcr));
+
+		char* buffer = "end_header\n";
+		out.write(buffer, strlen(buffer));
+
+		cout << "VERTICES" << endl << flush;
+
+		//Write vertices and normals
+		for(unsigned int i = 0; i < vertex_dcr.count; i++){
+
+			ply_vertex.x = vertices[3 * i];
+			ply_vertex.y = vertices[3 * i + 1];
+			ply_vertex.z = vertices[3 * i + 2];
+
+			ply_vertex.nx = normals[3 * i];
+			ply_vertex.ny = normals[3 * i + 1];
+			ply_vertex.nz = normals[3 * 1 + 2];
+
+			ply_vertex.r = colors[3 * i];
+			ply_vertex.g = colors[3 * i + 1];
+			ply_vertex.b = colors[3 * i + 2];
+
+			ply_vertex.u = 0.0;
+			ply_vertex.v = 0.0;
+
+			ply_vertex.texture = 1;
+
+			out.write( (char*)&ply_vertex, sizeof(ply_vertex));
+		}
+
+		for(int i = 0; i < number_of_faces; i ++){
+
+			ply_face.indices[0] = indices[3 * i];
+			ply_face.indices[1] = indices[3 * i + 1];
+			ply_face.indices[2] = indices[3 * i + 2];
+
+			out.write( (char*)&ply_face, sizeof(ply_face));
+		}
+	} else {
+		cout << "##### Warning: Static Mesh: Buffers empty." << endl;
+	}
 
 }
