1826fbc9d09ea8c1c2faafe984c9fab3209a237f
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2011-02-10 15:00:57 +0100

    Create a .tcc file for Normal class and exported normalization code

--- src/rewrite/Normal.hpp
+++ src/rewrite/Normal.hpp
@@ -65,24 +65,7 @@ public:
 	/**
 	 * @brief	Normalizes the components.
 	 */
-	void normalize()
-	{
-		//Don't normalize if we don't have to
-		float l_square =
-				  this->m_x * this->m_x
-				+ this->m_y * this->m_y
-				+ this->m_z * this->m_z;
-
-		if( fabs(1 - l_square) > 0.001){
-
-			float length = sqrt(l_square);
-			if(length != 0){
-				this->m_x /= length;
-				this->m_y /= length;
-				this->m_z /= length;
-			}
-		}
-	}
+	void normalize();
 
 	Normal<CoordType> operator+(const Normal &n) const
 	{
@@ -117,5 +100,8 @@ inline ostream& operator<<(ostream& os, const Normal<T> &n)
 	return os;
 }
 
-}
+} // namespace lssr
+
+#include "Normal.tcc"
+
 #endif /* NORMAL_HPP_ */
