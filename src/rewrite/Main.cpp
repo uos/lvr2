12531e7c6ebc86c31f62492e21c5f958711f919e
Thomas Wiemann <twiemann@uos.de>
2011-04-12 11:08:53 +0200

    PLY export now working

--- src/rewrite/Main.cpp
+++ src/rewrite/Main.cpp
@@ -1,15 +1,56 @@
+/**
+ * @mainpage LSSR Toolkit Documentation
+ *
+ * @section Introduction
+ *
+ * My intrduction
+ *
+ * @section LSSR Software Usage
+ *
+ * How to use the program
+ *
+ * @section Tutorials
+ *
+ * Some tutorials
+ *
+ */
+
+#include "StannPointCloudManager.hpp"
+#include "FastReconstruction.hpp"
 #include "PLYIO.hpp"
-#include "PLYElement.hpp"
+#include "Options.hpp"
+#include "Matrix4.hpp"
+#include "TriangleMesh.hpp"
 
-#include <iostream>
+using namespace lssr;
 
+/**
+ * @brief   Main entry point for the LSSR surface executable
+ */
 int main(int argc, char** argv)
 {
-	lssr::PLYElement e("vertex", 10);
-	e.addProperty("x", "float");
-	e.addProperty("y", "double");
-	e.addProperty("indices", "uint", "int", 10);
-	e.printProperties(std::cout);
+    // Parse command line arguments
+    Options options(argc, argv);
+
+    // Create a point cloud manager
+    StannPointCloudManager<float> manager(options.getOutputFileName(),
+                                          options.getKn(),
+                                          options.getKi(),
+                                          options.getKd());
+
+    // Create an empty mesh
+    TriangleMesh<Vertex<float>, unsigned int > mesh;
+
+    // Create a new reconstruction object
+    FastReconstruction<float, unsigned int> reconstruction(manager, options.getVoxelsize());
+    reconstruction.getMesh(mesh);
+
+    // Save triangle mesh
+    mesh.finalize();
+    mesh.save("triangle_mesh.ply");
+
+    cout << timestamp << "Program end." << endl;
 
 	return 0;
 }
+
