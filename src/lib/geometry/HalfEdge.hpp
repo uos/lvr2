2b2dc874068c8d0bbfa16e8f5cf8daa1875622b8
Thomas Wiemann <twiemann@uos.de>
2011-05-19 11:26:07 +0200

    Continued docu for half edge mesh types

--- src/lib/geometry/HalfEdge.hpp
+++ src/lib/geometry/HalfEdge.hpp
@@ -4,25 +4,43 @@
 namespace lssr
 {
 
+/**
+ * @brief An edge representation in a half edge mesh.
+ */
 template<typename HVertexT, typename FaceT>
 class HalfEdge{
 public:
+
+    /**
+     * @brief   Ctor.
+     */
 	HalfEdge() : start(0), end(0), pair(0), next(0), face(0), used(false) {};
 
+	/**
+	 * @brief   Dtor.
+	 */
 	~HalfEdge()
 	{
 		delete next;
 		delete pair;
 	}
 
+	/// A pointer to the next edge in current contour
 	HalfEdge<HVertexT, FaceT>* next;
+
+	/// A pointer to the pair edge of this edge
 	HalfEdge<HVertexT, FaceT>* pair;
 
+	/// A pointer to the start vertex of this edge
 	HVertexT* start;
+
+	/// A pointer to the end vertex of this edge
 	HVertexT* end;
 
+	/// A pointer to the surrounded face
 	FaceT* face;
 
+	/// Used for clustering (may be removed soon)
 	bool used;
 };
 
