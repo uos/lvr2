ec9c2962262ecacb54059ace04692ca70d1348dc
Lars Kiesow <lkiesow@uos.de>
2011-09-22 17:07:00 +0200

    Fixed little bug in plyio (faceloader)

--- src/lib/io/MeshLoader.hpp
+++ src/lib/io/MeshLoader.hpp
@@ -42,17 +42,6 @@ class MeshLoader
      */
     virtual unsigned int* getIndexArray(size_t &n) = 0;
 
-protected:
-
-    float*                    m_vertices;
-    float*                  m_vertexNormals;
-    float*                  m_vertexColors;
-    unsigned int*           m_indices;
-
-    size_t                  m_numberOfNormals;
-    size_t                  m_numberOfVertices;
-    size_t                  m_numberOfFaces;
-
 };
 
 #endif /* MESHIO_HPP_ */
