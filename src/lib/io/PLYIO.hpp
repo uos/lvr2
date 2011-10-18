29f08f1cceb9817585426bfda12501acf02890d9
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2011-10-18 14:42:26 +0200

    Fixed licensing

--- src/lib/io/PLYIO.hpp
+++ src/lib/io/PLYIO.hpp
@@ -1,4 +1,23 @@
-/**
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+
+ /**
  * @file       PLYIO.hpp
  * @brief      I/O support for PLY files.
  * @details    I/O support for PLY files: Reading and writing meshes and
