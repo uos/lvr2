d7a45f8d7a03d02264fdcb50d2749781d5cd0735
Chofu <pg2011@chofu.(none)>
2011-11-11 08:54:49 +0100

    Obj I/O rewritten to work with MeshBuffer class

--- src/lib/io/ObjIO.cpp
+++ src/lib/io/ObjIO.cpp
@@ -17,7 +17,7 @@
  */
 
 
- /*
+/*
  * ObjIO.cpp
  *
  *  @date 07.11.2011
@@ -27,59 +27,174 @@
  */
 
 #include "ObjIO.hpp"
+#include <climits>
+#include "boost/tuple/tuple.hpp"
+#include <map>
+#include "../geometry/Vertex.hpp"
 
 namespace lssr
 {
+	using namespace std; // Bitte vergebt mir....
 
-void ObjIO::save(Model model, string filename)
+
+
+void ObjIO::save(Model *model, string filename)
 {
+	typedef Vertex<uchar> ObjColor;
 
 	m_model = model;
+	size_t lenVertices;
+	size_t lenNormals;
+	size_t lenColors;
+	size_t lenFaces;
+	size_t lenTextureCoordinates;
+	size_t lenFaceIndices;
+	size_t lenFaceColors;
+	float **vertices = m_model->m_mesh->getIndexedVertexArray( lenVertices );
+	float **normals  = m_model->m_mesh->getIndexedVertexNormalArray( lenNormals );
+	float **textureCoordinates = m_model->m_mesh->getIndexedVertexTextureCoordinateArray( lenTextureCoordinates );
+	unsigned int* faceIndices = m_model->m_mesh->getFaceArray( lenFaces );
+	unsigned int* faceTextureIndices = m_model->m_mesh->getFaceTextureIndexArray( lenFaceIndices );
+	uchar * faceColors = m_model->m_mesh->getFaceColorArray( lenFaceColors );
+	std::map<ObjColor, unsigned int> colorMap;
 
-    ofstream out(filename.c_str());
-    ofstream mtlFile("textures.mtl");
 
-    if(out.good())
-    {
-    	if(m_textureIndices != 0)
-    		out<<"mtllib textures.mtl"<<endl<<endl;
+	std::set<unsigned int> textureIndexSet;
+	std::set<unsigned int> colorIndexSet;
 
-        for(size_t i = 0; i < m_vertexCount; i++)
-        {
-            unsigned int index = i * 3;
-            out << "v " << m_vertices[index] << " "
-                        << m_vertices[index + 1] << " "
-                        << m_vertices[index + 2] << " " << endl;
-        }
+	ofstream out(filename.c_str());
+	ofstream mtlFile("textures.mtl");
 
-        out<<endl;
-        for(size_t i = 0; i < m_vertexCount; i++)
-        {
-        	unsigned int index = i * 3;
-            out << "vn " << m_normals[index] << " "
-                         << m_normals[index + 1] << " "
-                         << m_normals[index + 2] << " " << endl;
-        }
+	if(out.good())
+	{
+		out<<"mtllib textures.mtl"<<endl;
 
-        out<<endl;
-        if(m_textureIndices != 0)
-        {
-        	for(size_t i = 0; i < m_vertexCount; i++)
-        	{
-        		unsigned int index = i * 3;
-        		out << "vt " << m_textureCoords[index] << " "
-        				<< m_textureCoords[index + 1] << " "
-        				<< m_textureCoords[index + 2] << " " << endl;
-        	}
-        }
+		if ( !vertices )
+		{
+			cerr << "Received no vertices to store. Aborting save operation." << endl;
+			return;
+		}
+		out << endl << endl << "##  Beginning of vertex definitions.\n";
+		cout << lenVertices << "lV\n";
+		for( size_t i=0; i < lenVertices; ++i )
+		{
+			out << "v " << vertices[i][0] << " "
+						<< vertices[i][1] << " "
+						<< vertices[i][2] << endl;
+		}
 
-//        // write material file
+		out<<endl;
+
+		out << endl << endl << "##  Beginning of vertex normals.\n";
+		for( size_t i=0; i < lenNormals; ++i )
+		{
+			out << "vn " << normals[i][0] << " "
+						 << normals[i][1] << " "
+						 << normals[i][2] << endl;
+		}
+
+		out << endl << endl << "##  Beginning of vertexTextureCoordinates.\n";
+
+		for( size_t i=0; i < lenTextureCoordinates; ++i )
+		{
+			out << "vt " << textureCoordinates[i][0] << " "
+						 << textureCoordinates[i][1] << " "
+						 << textureCoordinates[i][2] << endl;
+		}
+
+		unsigned int prevTextureIndex = UINT_MAX;
+		out << endl << endl << "##  Beginning of faces.\n";
+		// format of a face: f v/vt/vn
+		for( size_t i = 0; i < lenFaces; ++i )
+		{
+			if( lenFaceIndices > 0 && faceTextureIndices[ i * 3 ] != UINT_MAX )
+			{
+				if( faceTextureIndices[ i * 3 ] != prevTextureIndex )
+				{
+					out << "usemtl texture_"<<faceTextureIndices[ i * 3 ]<<endl;
+					prevTextureIndex = faceTextureIndices[ i * 3 ];
+					textureIndexSet.insert(faceTextureIndices[ i * 3 ]);
+				}
+			}
+			else if( lenFaceColors > 0 )
+			{
+				ObjColor color(faceColors[ i * 3 + 0 ], faceColors[ i * 3 + 1 ], faceColors[ i * 3 + 2 ]);
+				pair<std::map<Vertex<uchar>, unsigned int>::iterator, bool> pommes = colorMap.insert( make_pair<ObjColor, unsigned int>( color, i ) );
+				if( pommes.second == false )
+				{
+					out << "usemtl color_" << colorMap[color] <<endl;
+				}
+				else
+				{
+					out << "usemtl color_" << i << endl;
+				}
+
+			}
+			//unsigned int* faceTextureIndices
+			//float**       textureCoordinates
+			//usemtl....
+			// +1 after every index since in obj the 0-th vertex has index 1.
+			out << "f " << faceIndices[i * 3 + 0] + 1 << "/" << faceIndices[i * 3 + 0] + 1 << "/" << faceIndices[i * 3 + 0] + 1 << " "
+					    << faceIndices[i * 3 + 1] + 1 << "/" << faceIndices[i * 3 + 1] + 1 << "/" << faceIndices[i * 3 + 1] + 1 << " "
+					    << faceIndices[i * 3 + 2] + 1 << "/" << faceIndices[i * 3 + 2] + 1 << "/" << faceIndices[i * 3 + 2] + 1 << endl;
+		}
+
+		out<<endl;
+		out.close();
+	}
+	else
+	{
+		cerr << "no good. file! \n";
+	}
+
+
+
+		if( mtlFile.good() )
+		{
+			if( textureIndexSet.size() > 0 )
+			{
+				std::set<unsigned int>::iterator index;
+				for ( index = textureIndexSet.begin(); index != textureIndexSet.end(); index++ )
+				{
+					mtlFile<<"newmtl texture_"<<*index<<endl;
+					mtlFile<<"Ka 1.000 1.000 1.000"<<endl;
+					mtlFile<<"Kd 1.000 1.000 1.000"<<endl;
+					mtlFile<<"map_Kd texture_"<<*index<<".ppm"<<endl<<endl;
+				}
+			}
+			if( colorMap.size() > 0 )
+			{
+				std::map<ObjColor, unsigned int>::iterator mapIter = colorMap.begin();
+				for( ; mapIter != colorMap.end(); mapIter++ )
+				{
+					mtlFile<<"newmtl color_"<<mapIter->second<<endl;
+					mtlFile<<"Ka "<< mapIter->first[0] / 255.0f << " " << mapIter->first[1] / 255.0f << " " << mapIter->first[2] / 255.0f <<endl;
+					mtlFile<<"Kd "<< mapIter->first[0] / 255.0f << " " << mapIter->first[1] / 255.0f << " " << mapIter->first[2] / 255.0f <<endl;
+				}
+			}
+		}
+		mtlFile.close();
+}
+
+} // Namespace lssr
 //        if(m_textureIndices != 0)
 //        {
-//        	if(mtlFile.good())
+//        	for(size_t i = 0; i < m_vertexCount; i++)
 //        	{
-//        		mtlFile<<"newmtl texture_"<< UINT_MAX <<endl;
-//        		mtlFile<<"Kd 0.000 1.000 1.000"<<endl;
+//        		unsigned int index = i * 3;
+//        		out << "vt " << m_textureCoords[index] << " "
+//        				<< m_textureCoords[index + 1] << " "
+//        				<< m_textureCoords[index + 2] << " " << endl;
+//        	}
+//        }
+////
+////        // write material file
+////        if(m_textureIndices != 0)
+////        {
+////        	if(mtlFile.good())
+////        	{
+////        		mtlFile<<"newmtl texture_"<< UINT_MAX <<endl;
+////        		mtlFile<<"Kd 0.000 1.000 1.000"<<endl;
 //        		mtlFile<<"Ka 0.000 1.000 0.000"<<endl<<endl;
 //
 //        		for (size_t i = 0; i<this->m_textureCount; i++)
@@ -90,7 +205,7 @@ void ObjIO::save(Model model, string filename)
 //        			mtlFile<<"map_Kd texture_"<<m_textures[i]<<".ppm"<<endl<<endl;
 //        		}
 //        		mtlFile.close();
-//
+
 //        	}
 //        }
 //        int oldTextureIndex=-1, facesUsed=0, j=0, pommes=0;
@@ -146,13 +261,3 @@ void ObjIO::save(Model model, string filename)
 //        	facesUsed++;
 //        }
 
-        out.close();
-
-    }
-    else
-    {
-    	cerr << "no good. file! \n";
-    }
-}
-
-}
