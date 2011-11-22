d90998688a23f2c6c798a9db394a34a2c946baf7
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2011-11-22 11:03:16 +0100

    Reverted Lars' last merge. Caused segfaults.

--- src/lib/geometry/Vertex.hpp
+++ src/lib/geometry/Vertex.hpp
@@ -17,11 +17,12 @@
  */
 
 
-#ifndef __BASE_VERTEX_HPP__
+ #ifndef __BASE_VERTEX_HPP__
 #define __BASE_VERTEX_HPP__
 
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -32,246 +33,230 @@ namespace lssr
 template<typename T> class Matrix4;
 
 /**
- * @brief         Basic vertex class. Supports all arithmetic operators
- *                 as well as indexed access and matrix multiplication
- *                 with the Matrix4 class
+ * @brief 		Basic vertex class. Supports all arithmetic operators
+ * 				as well as indexed access and matrix multiplication
+ * 				with the Matrix4 class
  */
 template<typename CoordType>
 class Vertex{
 
-    public:
+public:
 
-        /**
-         * @brief    Default constructor. All coordinates are initialized
-         *             with zeros.
-         */
-        Vertex()
-        {
-            x = y = z = 0;
-        }
+	/**
+	 * @brief	Default constructor. All coordinates are initialized
+	 * 			with zeros.
+	 */
+	Vertex()
+	{
+		x = y = z = 0;
+	}
 
-        /**
-         * @brief    Builds a Vertex with the given coordinates.
-         */
-        Vertex(const CoordType &_x, const CoordType &_y, const CoordType &_z)
-        {
-            x = _x;
-            y = _y;
-            z = _z;
-        }
+	/**
+	 * @brief	Builds a Vertex with the given coordinates.
+	 */
+	Vertex(const CoordType &_x, const CoordType &_y, const CoordType &_z)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+	}
 
-        /**
-         * @brief    Copy Ctor.
-         */
-        Vertex(const Vertex &o)
-        {
-            x = o.x;
-            y = o.y;
-            z = o.z;
-        }
+	/**
+	 * @brief	Copy Ctor.
+	 */
+	Vertex(const Vertex &o)
+	{
+		x = o.x;
+		y = o.y;
+		z = o.z;
+	}
 
-        /**
-         * @brief    Destructor
-         */
-        virtual ~Vertex(){};
+	/**
+	 * @brief	Destructor
+	 */
+	virtual ~Vertex(){};
 
-        /**
-         * @brief     Return the current length of the vector
-         */
-        CoordType length();
+	/**
+	 * @brief 	Return the current length of the vector
+	 */
+	CoordType length();
 
-        /**
-         * @brief    Calculates the cross product between this and
-         *             the given vector. Returns a new Vertex instance.
-         *
-         * @param    other        The second cross product vector
-         */
-        Vertex<CoordType> cross(const Vertex &other) const;
-
-
-        /**
-         * @brief    Applies the given matrix. Translational components
-         *             are ignored (matrix must be row major).
-         *
-         * @param    A 4x4 rotation matrix.
-         */
-        void rotate(const Matrix4<CoordType> &m);
-
-        /**
-         * @brief   Applies the given matrix. Translational components
-         *          are ignored
-         *
-         * @param   A 4x4 rotation matrix (column major).
-         */
-        void rotateCM(const Matrix4<CoordType> &m);
-
-        /**
-         * @brief   Applies the given matrix. Translational components
-         *          are ignored
-         *
-         * @param   A 4x4 rotation matrix (row major).
-         */
-        void rotateRM(const Matrix4<CoordType> &m);
-
-        /**
-         * @brief    Transforms the vertex according to the given matrix
-         *          (Default: matrix is row major)
-         *
-         * @param    A 4x4 tranformation matrix.
-         */
-        void transform(const Matrix4<CoordType> &m);
-
-        /**
-         * @brief   Transforms the vertex according to the given matrix
-         *          (Row major format)
-         *
-         * @param   A 4x4 tranformation matrix.
-         */
-        void transformRM(const Matrix4<CoordType> &m);
-
-        /**
-         * @brief   Transforms the vertex according to the given matrix
-         *          (Column major format)
-         *
-         * @param   A 4x4 tranformation matrix.
-         */
-        void transformCM(const Matrix4<CoordType> &m);
+	/**
+	 * @brief	Calculates the cross product between this and
+	 * 			the given vector. Returns a new Vertex instance.
+	 *
+	 * @param	other		The second cross product vector
+	 */
+	Vertex<CoordType> cross(const Vertex &other) const;
 
 
-        /**
-         * @brief    Calculates the cross product with the other given
-         *             Vertex and assigns the result to the current
-         *             instance.
-         */
-        virtual void crossTo(const Vertex &other);
+	/**
+	 * @brief	Applies the given matrix. Translational components
+	 * 			are ignored (matrix must be row major).
+	 *
+	 * @param	A 4x4 rotation matrix.
+	 */
+	void rotate(const Matrix4<CoordType> &m);
 
+    /**
+     * @brief   Applies the given matrix. Translational components
+     *          are ignored
+     *
+     * @param   A 4x4 rotation matrix (column major).
+     */
+    void rotateCM(const Matrix4<CoordType> &m);
 
-        /**
-         * @brief  Calculates the distance to another vertex.
-         *
-         * @param other  Another Vertex.
-         */
-        virtual CoordType distance( const Vertex &other ) const;
+    /**
+     * @brief   Applies the given matrix. Translational components
+     *          are ignored
+     *
+     * @param   A 4x4 rotation matrix (row major).
+     */
+    void rotateRM(const Matrix4<CoordType> &m);
+
+	/**
+	 * @brief	Transforms the vertex according to the given matrix
+	 *          (Default: matrix is row major)
+	 *
+	 * @param	A 4x4 tranformation matrix.
+	 */
+	void transform(const Matrix4<CoordType> &m);
+
+	/**
+	 * @brief   Transforms the vertex according to the given matrix
+	 *          (Row major format)
+	 *
+	 * @param   A 4x4 tranformation matrix.
+	 */
+	void transformRM(const Matrix4<CoordType> &m);
+
+    /**
+     * @brief   Transforms the vertex according to the given matrix
+     *          (Column major format)
+     *
+     * @param   A 4x4 tranformation matrix.
+     */
+    void transformCM(const Matrix4<CoordType> &m);
 
 
 
-        /**
-		 * @brief  Calculates the squared distance to another vertex.
-         *
-         * @param other  Another Vertex.
-         */
-		virtual CoordType sqrDistance( const Vertex &other ) const;
 
-        /**
-         * @brief    Multiplication operator (dot product)
-         */
-        virtual CoordType operator*(const Vertex &other) const;
+	/**
+	 * @brief	Calculates the cross product with the other given
+	 * 			Vertex and assigns the result to the current
+	 * 			instance.
+	 */
+	virtual void crossTo(const Vertex &other);
 
-
-        /**
-         * @brief     Multiplication operator (scaling)
-         */
-        virtual Vertex<CoordType> operator*(const CoordType &scale) const;
+	/**
+	 * @brief	Multiplication operator (dot product)
+	 */
+	virtual CoordType operator*(const Vertex &other) const;
 
 
-        virtual Vertex<CoordType> operator+(const Vertex &other) const;
+	/**
+	 * @brief 	Multiplication operator (scaling)
+	 */
+	virtual Vertex<CoordType> operator*(const CoordType &scale) const;
+
+	virtual Vertex<CoordType> operator+(const Vertex &other) const;
 
 
-        /**
-         * @brief    Coordinate subtraction
-         */
-        virtual Vertex<CoordType> operator-(const Vertex &other) const;
+	/**
+	 * @brief	Coordinate subtraction
+	 */
+	virtual Vertex<CoordType> operator-(const Vertex &other) const;
 
-        /**
-         * @brief    Coordinate substraction
-         */
-        virtual void operator-=(const Vertex &other);
+	/**
+	 * @brief	Coordinate substraction
+	 */
+	virtual void operator-=(const Vertex &other);
 
-        /**
-         * @brief    Coordinate addition
-         */
-        virtual void operator+=(const Vertex<CoordType> &other);
-
-
-        /**
-         * @brief    Scaling
-         */
-        virtual void operator*=(const CoordType &scale);
+	/**
+	 * @brief	Coordinate addition
+	 */
+	virtual void operator+=(const Vertex<CoordType> &other);
 
 
-        /**
-         * @brief    Scaling
-         */
-        virtual void operator/=(const CoordType &scale);
-
-        /**
-         * @brief    Compares two vertices
-         */
-        virtual bool operator==(const Vertex &other) const;
-
-        /**
-         * @brief    Compares two vertices
-         */
-        virtual bool operator!=(const Vertex &other) const
-        {
-            return !(*this == other);
-        }
+	/**
+	 * @brief	Scaling
+	 */
+	virtual void operator*=(const CoordType &scale);
 
 
-        virtual bool operator<(const Vertex &other) const
-        {
-            return ( this->x < other.x ) 
-                || ( ( this->x - other.x <= 0.00001 )
-                    && ( ( this->y < other.y ) 
-                        || ( ( this->z < other.z )
-                            && ( this->y - other.y <= 0.00001 ) ) ) );
-        }
+	/**
+	 * @brief	Scaling
+	 */
+	virtual void operator/=(const CoordType &scale);
 
-        /**
-         * @brief    Indexed coordinate access (reading)
-         */
-        virtual CoordType operator[](const int &index) const;
+	/**
+	 * @brief	Compares two vertices
+	 */
+	virtual bool operator==(const Vertex &other) const;
 
-        /**
-         * @brief   Indexed coordinate access (writing)
-         */
-        virtual CoordType& operator[](const int &index);
+	/**
+	 * @brief	Compares two vertices
+	 */
+	virtual bool operator!=(const Vertex &other) const
+	{
+		return !(*this == other);
+	}
 
 
-        /// The x-coordinate of the vertex
-        CoordType x;
+  virtual bool operator<(const Vertex &other) const
+  {
+     if( this->x < other.x ) return true;
+     if( (this->x - other.x) <= 0.00001 )
+     {
+        if(this->y < other.y ) return true;
+        if((this->y - other.y) <= 0.00001 ) 
+           if(this->z < other.z) return true;
+     }
+     return false;
+  }
 
-        /// The y-coordinate of the vertex
-        CoordType y;
+	/**
+	 * @brief	Indexed coordinate access (reading)
+	 */
+	virtual CoordType operator[](const int &index) const;
 
-        /// The z-coordinate of the vertex
-        CoordType z;
+    /**
+     * @brief   Indexed coordinate access (writing)
+     */
+    virtual CoordType& operator[](const int &index);
 
-    private:
 
-        /// Epsilon value for vertex comparism
-        static const float epsilon = 0.00001;
+	/// The x-coordinate of the vertex
+	CoordType x;
+
+	/// The y-coordinate of the vertex
+	CoordType y;
+
+	/// The z-coordinate of the vertex
+	CoordType z;
+
+private:
+
+	/// Epsilon value for vertex comparism
+	static const float epsilon = 0.00001;
 };
 
 
 /**
- * @brief    Output operator for vertex types
+ * @brief	Output operator for vertex types
  */
 template<typename CoordType>
-inline ostream& operator<<(ostream& os, const Vertex<CoordType> v)
-{
-    os << "Vertex: " << v.x << " " << v.y << " " << v.z << endl;
-    return os;
+inline ostream& operator<<(ostream& os, const Vertex<CoordType> v){
+	os << "Vertex: " << v.x << " " << v.y << " " << v.z << endl;
+	return os;
 }
 
 
 /// Convenience typedef for float vertices
-typedef Vertex<float>     Vertexf;
-
+typedef Vertex<float> 	Vertexf;
 
 /// Convenience typedef for double vertices
-typedef Vertex<double>    Vertexd;
-
+typedef Vertex<double>	Vertexd;
 
 } // namespace lssr
 
--- src/lib/reconstruction/PointCloudManager.tcc
+++ src/lib/reconstruction/PointCloudManager.tcc
@@ -26,9 +26,7 @@
 
 #include <cassert>
 #include <string>
-#include <cmath>
-#include <cstring>
-#include <cstdio>
+using std::string;
 
 #include "../io/Timestamp.hpp"
 #include "../io/ModelFactory.hpp"
@@ -38,23 +36,18 @@
 namespace lssr
 {
 
-
 template<typename VertexT, typename NormalT>
 BoundingBox<VertexT>& PointCloudManager<VertexT, NormalT>::getBoundingBox()
 {
     return m_boundingBox;
 }
 
-
 template<typename VertexT, typename NormalT>
 VertexT PointCloudManager<VertexT, NormalT>::getPoint(size_t index)
 {
     assert(index < m_numPoints);
-    return VertexT(
-            m_points[index][0], m_points[index][1], m_points[index][2], 
-            m_colors[index][0], m_colors[index][1], m_colors[index][2] );
+    return VertexT(m_points[index][0], m_points[index][1], m_points[index][2], m_colors[index][0], m_colors[index][1], m_colors[index][2]);
 }
-
 
 template<typename VertexT, typename NormalT>
 size_t PointCloudManager<VertexT, NormalT>::getNumPoints()
@@ -62,65 +55,10 @@ size_t PointCloudManager<VertexT, NormalT>::getNumPoints()
     return m_numPoints;
 }
 
-
 template<typename VertexT, typename NormalT>
-const VertexT PointCloudManager<VertexT, NormalT>::operator[]( const size_t& index ) const
+const VertexT PointCloudManager<VertexT, NormalT>::operator[](const size_t& index) const
 {
-    return VertexT(
-            m_points[index][0], m_points[index][1], m_points[index][2], 
-            m_colors[index][0], m_colors[index][1], m_colors[index][2] );
-}
-
-
-template<typename VertexT, typename NormalT>
-void PointCloudManager<VertexT, NormalT>::colorizePointCloud( 
-		PointCloudManager<VertexT, NormalT>* pcm, const float &sqrMaxDist,
-        const uchar* blankColor )
-{
-
-    /* Check if we already have a color buffer. */
-    if ( !m_colors )
-    {
-        uchar* c = new uchar[ m_numPoints * 3 ];
-        m_colors = new uchar*[ m_numPoints    ];
-        for ( size_t i = 0; i < m_numPoints; i++ )
-        {
-            m_colors[i] = c + ( 3 * i );
-        }
-    }
-
-#pragma omp parallel for
-    /* Run through laserscan cloud and find neighbours. */
-    for ( size_t i = 0; i < m_numPoints; i++ )
-    {
-
-        std::vector<VertexT> nearestPoint(1);
-
-        /* nearest neighbor search */
-        VertexT p( this->getPoint( i ) );
-        pcm->getkClosestVertices( p, 1, nearestPoint );
-        /* Check if vector contains point. */
-        if ( nearestPoint.size() )
-        {
-            if ( p.sqrDistance( nearestPoint[0] ) < sqrMaxDist )
-            {
-                /* Get color from other pointcloud. */
-                m_colors[i][0] = nearestPoint[0].r;
-                m_colors[i][1] = nearestPoint[0].g;
-                m_colors[i][2] = nearestPoint[0].b;
-            }
-            else if ( blankColor )
-            {
-                /* Set default color. */
-                m_colors[i][0] = blankColor[0];
-                m_colors[i][1] = blankColor[1];
-                m_colors[i][2] = blankColor[2];
-            }
-            /* TODO: Store the distance as confidence information. */
-
-        }
-    }
-
+    return VertexT(m_points[index][0], m_points[index][1], m_points[index][2]);
 }
 
 
