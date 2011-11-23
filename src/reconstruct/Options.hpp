c8472a7542d17e24a2ce89b8ed7a9a601c5091d4
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2011-11-23 13:51:31 +0100

    Updated options for MT decomposition

--- src/reconstruct/Options.hpp
+++ src/reconstruct/Options.hpp
@@ -164,6 +164,11 @@ public:
 	 */
 	string getPCM() const;
 
+    /**
+     * @brief   Returns the name of the used point cloud handler.
+     */
+    string getDecomposition() const;
+
 
 	/**
 	 * @brief   Returns the normal threshold for plane optimization.
@@ -277,15 +282,16 @@ inline ostream& operator<<(ostream& os, const Options &o)
 	{
 	    cout << "##### Voxelsize \t\t: " << o.getVoxelsize() << endl;
 	}
-	cout << "##### Input file \t\t: "         << o.getInputFileName() << endl;
-	cout << "##### Number of threads \t: "    << o.getNumThreads()    << endl;
-	cout << "##### Point cloud manager: \t: " << o.getPCM()           << endl;
-	cout << "##### k_n \t\t\t: "              << o.getKn()            << endl;
-	cout << "##### k_i \t\t\t: "              << o.getKi()            << endl;
-	cout << "##### k_d \t\t\t: "              << o.getKd()            << endl;
+	cout << "##### Input file \t\t: "         << o.getInputFileName()   << endl;
+	cout << "##### Number of threads \t: "    << o.getNumThreads()      << endl;
+	cout << "##### Point cloud manager: \t: " << o.getPCM()             << endl;
+	cout << "##### Voxel decomposition: \t: " << o.getDecomposition()   << endl;
+	cout << "##### k_n \t\t\t: "              << o.getKn()              << endl;
+	cout << "##### k_i \t\t\t: "              << o.getKi()              << endl;
+	cout << "##### k_d \t\t\t: "              << o.getKd()              << endl;
 	if(o.retesselate())
 	{
-		cout << "##### retesselate:\t\t: YES"     << endl;
+		cout << "##### Retesselate:\t\t: YES"     << endl;
 	}
 	if(o.saveFaceNormals())
 	{
