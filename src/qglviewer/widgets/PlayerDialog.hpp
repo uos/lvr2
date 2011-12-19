721e1d40b863165b7cbefcaa573a1707da49d63f
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2011-12-19 16:08:10 +0100

    Created dialog layout for animation player

---
+++ src/qglviewer/widgets/PlayerDialog.hpp
@@ -0,0 +1,40 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+/**
+ * PlayerDialog.h
+ *
+ *  @date 19.12.2011
+ *  @author Thomas Wiemann
+ */
+
+#ifndef PLAYERDIALOG_H_
+#define PLAYERDIALOG_H_
+
+namespace lssr
+{
+
+class PlayerDialog
+{
+public:
+    PlayerDialog();
+    virtual ~PlayerDialog();
+};
+
+} /* namespace lssr */
+#endif /* PLAYERDIALOG_H_ */
