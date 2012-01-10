574e8e9aea5ed1a2caf361d1f6a543b0901d36c0
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2012-01-10 13:16:22 +0100

    Hacked grid into viewer -> not displaying correctly

--- src/lib/display/Grid.cpp
+++ src/lib/display/Grid.cpp
@@ -79,6 +79,8 @@ Grid::Grid(floatArr vertices, uintArr boxes, uint numPoints, uint numBoxes)
             x[j] = m_vertices[vertex_pos];
             y[j] = m_vertices[vertex_pos + 1];
             z[j] = m_vertices[vertex_pos + 2];
+
+            m_boundingBox->expand(x[j], y[j], z[j]);
         }
 
         // Render quads
