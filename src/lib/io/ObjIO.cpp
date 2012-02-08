c05133d5a4044e1c9b547b53fe41a2d3daa91f53
Denis Meyer <denmeyer@uos.de>
2012-02-08 14:51:49 +0100

    Basic obj read functionality implemented - no mtl support, yet

--- src/lib/io/ObjIO.cpp
+++ src/lib/io/ObjIO.cpp
@@ -40,118 +40,136 @@
 
 namespace lssr
 {
-using namespace std; // Bitte vergebt mir....
-// Meinst du wirklich, dass ich dir so etwas durchgehen lassen kann?
+  using namespace std; // Bitte vergebt mir....
+  // Meinst du wirklich, dass ich dir so etwas durchgehen lassen kann?
 
-ModelPtr ObjIO::read( string filename ) // TODO: Format correctly
-{
-	/*
-	cout << timestamp << "ObjIO::read" << endl; // TODO: Remove
-
-	ifstream f (filename.c_str());
-	if (!f.is_open())
-	{
-		cerr << timestamp << "File '" << filename << "' could not be opened." << endl;
-		return ModelPtr( new Model );
-  	}
-    	f.close();
-
-	char *fn = const_cast<char *>(filename.c_str());
-	objLoader *objData = new objLoader();
-	objData->load(fn);
-
-	// Buffer count variables
-	size_t numVertices              = objData->vertexCount;
-	size_t numVertexNormals         = objData->normalCount;
-	size_t numFaces                 = objData->faceCount;
-	size_t numTextures		= objData->textureCount;
-	size_t numSpheres		= objData->sphereCount;
-	size_t numPlanes		= objData->planeCount;
-	size_t numPointLights		= objData->lightPointCount;
-	size_t numDiscLights		= objData->lightDiscCount;
-	size_t numQuadLights		= objData->lightQuadCount;
-	size_t numMaterials		= objData->materialCount;
-
-	size_t numVertexColors		= 0;
-	size_t numVertexIntensities	= 0;
-	size_t numVertexConfidences	= 0;
-
-	// Some output
-	cout << timestamp << "Number of vertices: " 		<< numVertices 		<< endl;
-	cout << timestamp << "Number of vertex normals: " 	<< numVertexNormals 	<< endl;
-	cout << timestamp << "Number of faces: " 		<< numFaces 		<< endl;
-	cout << timestamp << "Number of texture coordinates: " 	<< numTextures 		<< endl;
-	cout << timestamp << "Number of spheres: " 		<< numSpheres 		<< endl;
-	cout << timestamp << "Number of planes: " 		<< numPlanes 		<< endl;
-	cout << timestamp << "Number of point lights: " 	<< numPointLights 	<< endl;
-	cout << timestamp << "Number of disc lights: " 		<< numDiscLights 	<< endl;
-	cout << timestamp << "Number of quad lights: " 		<< numQuadLights 	<< endl;
-	cout << timestamp << "Number of materials: " 		<< numMaterials 	<< endl;
-	if(objData->camera != NULL)
-	{
-		cout << timestamp << "Found a camera" << endl;
-	}
-	cout << endl;
-
-	// Buffers
-	floatArr vertices;
-	floatArr vertexConfidence;
-	floatArr vertexIntensity;
-	floatArr vertexNormals;
-	ucharArr vertexColors;
-	uintArr  faceIndices;
-
-	// Allocate memory
-	if ( numVertices )
-    	{
-        	vertices = floatArr( new float[ numVertices * 3 ] );
-    	}
-    	if ( numVertexColors )
-    	{
-    	    	vertexColors = ucharArr( new uchar[ numVertices * 3 ] );
-    	}
-    	if ( numVertexConfidences )
-    	{
-    	    	vertexConfidence = floatArr( new float[ numVertices ] );
-    	}
-    	if ( numVertexIntensities )
-	{
-   	    	vertexIntensity = floatArr( new float[ numVertices ] );
-   	}
-   	if ( numVertexNormals )
-   	{
-   	    	vertexNormals = floatArr( new float[ 3 * numVertices ] );
-   	}
-   	if ( numFaces )
-   	{
-   	    	faceIndices = uintArr( new unsigned int[ numFaces * 3 ] );
-   	}
-
-	delete(objData);
-
-    	// Save buffers in model
-    	MeshBufferPtr mesh;
-    	if(vertices)
-    	{
-        	mesh = MeshBufferPtr( new MeshBuffer );
-        	mesh->setVertexArray(           vertices,         numVertices );
-        	mesh->setVertexColorArray(      vertexColors,     numVertexColors );
-        	mesh->setVertexIntensityArray(  vertexIntensity,  numVertexIntensities );
-        	mesh->setVertexNormalArray(     vertexNormals,    numVertexNormals );
-        	mesh->setVertexConfidenceArray(	vertexConfidence, numVertexConfidences );
-        	mesh->setFaceArray(             faceIndices,      numFaces );
-    	}
-
-    	ModelPtr m( new Model( mesh ) );
-    	m_model = m;
-	return m;
-	*/
-
+  ModelPtr ObjIO::read( string filename ) // TODO: Format correctly
+  {
+    ifstream f (filename.c_str());
+    if (!f.is_open())
+      {
+	cerr << timestamp << "File '" << filename << "' could not be opened." << endl;
 	return ModelPtr( new Model );
-};
+      }
+    f.close();
 
-void ObjIO::save( string filename )
-{
+    char *fn = const_cast<char *>(filename.c_str());
+    objLoader *objData = new objLoader();
+    objData->load(fn);
+
+    // Buffer count variables
+    size_t numVertices      = objData->vertexCount;
+    size_t numVertexNormals = objData->normalCount;
+    size_t numFaces         = objData->faceCount;
+    size_t numTextures      = objData->textureCount;
+    size_t numSpheres       = objData->sphereCount;
+    size_t numPlanes        = objData->planeCount;
+    size_t numPointLights   = objData->lightPointCount;
+    size_t numDiscLights    = objData->lightDiscCount;
+    size_t numQuadLights    = objData->lightQuadCount;
+    size_t numMaterials     = objData->materialCount;
+    size_t numVertexColors  = 0; // TODO: Set
+
+    // Some output
+    /*
+    cout << timestamp << "Number of vertices: "            << numVertices      << endl;
+    cout << timestamp << "Number of vertex normals: "      << numVertexNormals << endl;
+    cout << timestamp << "Number of vertex colors: "       << numVertexColors  << endl;
+    cout << timestamp << "Number of faces: "               << numFaces         << endl;
+    cout << timestamp << "Number of texture coordinates: " << numTextures      << endl;
+    cout << timestamp << "Number of spheres: "             << numSpheres       << endl;
+    cout << timestamp << "Number of planes: "              << numPlanes        << endl;
+    cout << timestamp << "Number of point lights: "        << numPointLights   << endl;
+    cout << timestamp << "Number of disc lights: "         << numDiscLights    << endl;
+    cout << timestamp << "Number of quad lights: "         << numQuadLights    << endl;
+    cout << timestamp << "Number of materials: "           << numMaterials     << endl;
+    if(objData->camera != NULL)
+      {
+	cout << timestamp << "Found a camera" << endl;
+      }
+    cout << endl;
+    */
+
+    // Buffers
+    floatArr vertices;
+    floatArr vertexNormals;
+    ucharArr vertexColors;
+    uintArr  faceIndices;
+
+    // Allocate memory
+    if ( numVertices )
+      {
+	vertices = floatArr( new float[ numVertices * 3 ] );
+      }
+    if ( numVertexNormals )
+      {
+	vertexNormals = floatArr( new float[ numVertices * 3 ] );
+      }
+    if ( numVertexColors )
+      {
+	vertexColors = ucharArr( new uchar[ numVertexColors * 3 ] );
+      }
+    if ( numFaces )
+      {
+	faceIndices = uintArr( new unsigned int[ numFaces * 3 ] );
+      }
+
+    // vertices
+    for(int i = 0; i < numVertices; ++i)
+      {
+        obj_vector *o = objData->vertexList[i];
+	vertices[ i * 3 ]     = o->e[ 0 ];
+	vertices[ i * 3 + 1 ] = o->e[ 1 ];
+	vertices[ i * 3 + 2 ] = o->e[ 2 ];
+      }
+    
+    // normals
+    for(int i = 0; i < numVertexNormals; ++i)
+      {
+        obj_vector *o = objData->normalList[i];
+	vertexNormals[ i * 3 ]     = o->e[ 0 ];
+	vertexNormals[ i * 3 + 1 ] = o->e[ 1 ];
+	vertexNormals[ i * 3 + 2 ] = o->e[ 2 ];
+      }
+
+    // colors
+    for(int i = 0; i < numVertexColors; ++i)
+      {
+	vertexColors[ i * 3 ]    = 255;
+	vertexColors[ i * 3 + 1] = 0;
+	vertexColors[ i * 3 + 2] = 0;
+      }
+    
+    // faces
+    for(int i = 0; i < numFaces; ++i)
+      {
+	obj_face *o              = objData->faceList[ i ];
+	faceIndices[ i * 3 ]     = o->vertex_index[ 0 ];
+	faceIndices[ i * 3 + 1 ] = o->vertex_index[ 1 ];
+	faceIndices[ i * 3 + 2 ] = o->vertex_index[ 2 ];
+      }
+
+    // delete obj data object
+    delete(objData);
+
+    // Save buffers in model
+    MeshBufferPtr mesh;
+    if(vertices)
+      {
+	mesh = MeshBufferPtr( new MeshBuffer );
+	mesh->setVertexArray(           vertices,         numVertices );
+	mesh->setVertexNormalArray(     vertexNormals,    numVertexNormals );
+	mesh->setVertexColorArray(      vertexColors,     numVertexColors );
+	mesh->setFaceArray(             faceIndices,      numFaces );
+      }
+
+    ModelPtr m( new Model( mesh ) );
+    m_model = m;
+    return m;
+  };
+
+  void ObjIO::save( string filename )
+  {
     typedef Vertex<uchar> ObjColor;
 
     size_t lenVertices;
@@ -178,74 +196,74 @@ void ObjIO::save( string filename )
     ofstream mtlFile("textures.mtl");
 
     if(out.good())
-    {
+      {
         out<<"mtllib textures.mtl"<<endl;
 
         if ( !vertices )
-        {
+	  {
             cerr << "Received no vertices to store. Aborting save operation." << endl;
             return;
-        }
+	  }
         out << endl << endl << "##  Beginning of vertex definitions.\n";
 
         for( size_t i=0; i < lenVertices; ++i )
-        {
+	  {
             out << "v " << vertices[i][0] << " "
                 << vertices[i][1] << " "
                 << vertices[i][2] << endl;
-        }
+	  }
 
         out<<endl;
 
         out << endl << endl << "##  Beginning of vertex normals.\n";
         for( size_t i=0; i < lenNormals; ++i )
-        {
+	  {
             out << "vn " << normals[i][0] << " "
                 << normals[i][1] << " "
                 << normals[i][2] << endl;
-        }
+	  }
 
         out << endl << endl << "##  Beginning of vertexTextureCoordinates.\n";
 
         for( size_t i=0; i < lenTextureCoordinates; ++i )
-        {
+	  {
             out << "vt " << textureCoordinates[i][0] << " "
                 << textureCoordinates[i][1] << " "
                 << textureCoordinates[i][2] << endl;
-        }
+	  }
 
         unsigned int prevTextureIndex = UINT_MAX;
         out << endl << endl << "##  Beginning of faces.\n";
         // format of a face: f v/vt/vn
         for( size_t i = 0; i < lenFaces; ++i )
-        {
+	  {
             if( lenFaceIndices > 0 && faceTextureIndices[ i * 3 ] != UINT_MAX )
-            {
+	      {
                 if( faceTextureIndices[ i * 3 ] != prevTextureIndex )
-                {
+		  {
                     out << "usemtl texture_"<<faceTextureIndices[ i * 3 ]<<endl;
                     prevTextureIndex = faceTextureIndices[ i * 3 ];
                     textureIndexSet.insert(faceTextureIndices[ i * 3 ]);
-                }
-            }
+		  }
+	      }
             else if( lenFaceColors > 0 )
-            {
+	      {
                 ObjColor color(
-                        faceColors[ i * 3 ],
-                        faceColors[ i * 3 + 1 ],
-                        faceColors[ i * 3 + 2 ] );
+			       faceColors[ i * 3 ],
+			       faceColors[ i * 3 + 1 ],
+			       faceColors[ i * 3 + 2 ] );
                 pair<std::map<Vertex<uchar>, unsigned int>::iterator, bool> pommes 
-                    = colorMap.insert( make_pair<ObjColor, unsigned int>( color, i ) );
+		  = colorMap.insert( make_pair<ObjColor, unsigned int>( color, i ) );
                 if( pommes.second == false )
-                {
+		  {
                     out << "usemtl color_" << colorMap[color] << endl;
-                }
+		  }
                 else
-                {
+		  {
                     out << "usemtl color_" << i << endl;
-                }
+		  }
 
-            }
+	      }
             //unsigned int* faceTextureIndices
             //float**       textureCoordinates
             //usemtl....
@@ -260,49 +278,49 @@ void ObjIO::save( string filename )
                 << faceIndices[i * 3 + 2] + 1 << "/" 
                 << faceIndices[i * 3 + 2] + 1 << "/" 
                 << faceIndices[i * 3 + 2] + 1 << endl;
-        }
+	  }
 
         out<<endl;
         out.close();
-    }
+      }
     else
-    {
+      {
         cerr << "no good. file! \n";
-    }
+      }
 
 
 
     if( mtlFile.good() )
-    {
+      {
         if( textureIndexSet.size() > 0 )
-        {
+	  {
             std::set<unsigned int>::iterator index;
             for ( index = textureIndexSet.begin(); index != textureIndexSet.end(); index++ )
-            {
+	      {
                 mtlFile << "newmtl texture_"      << *index << endl;
                 mtlFile << "Ka 1.000 1.000 1.000" << endl;
                 mtlFile << "Kd 1.000 1.000 1.000" << endl;
                 mtlFile << "map_Kd texture_"      << *index << ".ppm" << endl << endl;
-            }
-        }
+	      }
+	  }
         if( colorMap.size() > 0 )
-        {
+	  {
             std::map<ObjColor, unsigned int>::iterator mapIter = colorMap.begin();
             for( ; mapIter != colorMap.end(); mapIter++ )
-            {
+	      {
                 mtlFile << "newmtl color_" << mapIter->second << endl;
                 mtlFile << "Ka " 
-                    << mapIter->first[0] / 255.0f << " " 
-                    << mapIter->first[1] / 255.0f << " "
-                    << mapIter->first[2] / 255.0f << endl;
+			<< mapIter->first[0] / 255.0f << " " 
+			<< mapIter->first[1] / 255.0f << " "
+			<< mapIter->first[2] / 255.0f << endl;
                 mtlFile << "Kd " 
-                    << mapIter->first[0] / 255.0f << " "
-                    << mapIter->first[1] / 255.0f << " "
-                    << mapIter->first[2] / 255.0f << endl;
-            }
-        }
-    }
+			<< mapIter->first[0] / 255.0f << " "
+			<< mapIter->first[1] / 255.0f << " "
+			<< mapIter->first[2] / 255.0f << endl;
+	      }
+	  }
+      }
     mtlFile.close();
-}
+  }
 
 } // Namespace lssr
