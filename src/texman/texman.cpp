eed5822e3777cb2587c13f2304177f03e7f081fa
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-06-13 16:33:12 +0200

    Moved to new calcSURF interface

--- src/texman/texman.cpp
+++ src/texman/texman.cpp
@@ -57,13 +57,14 @@ void a(lssr::TextureIO* tio)
 		unsigned short int tc = 0;
 		cin>>tc;
 		unsigned char depth = img.depth() == CV_8U ? 1 : 2;
-		// calculate features
-		unsigned short numFeatures = 0;
-		unsigned char numComps = 0;
-		float* features = lssr::ImageProcessor::calcSURF(img, numFeatures, numComps);
+
 		//create Texture
-		lssr::Texture* t = new lssr::Texture(img.size().width, img.size().height, img.channels(), depth, tc, numFeatures, numComps, features);
+		lssr::Texture* t = new lssr::Texture(img.size().width, img.size().height, img.channels(), depth, tc, 0, 0 ,0);
 		memcpy(t->m_data, img.data, img.size().width * img.size().height * img.channels() * depth);
+
+		// calculate features
+		lssr::ImageProcessor::calcSURF(t);
+
 		cout<<"\t(a)dded new texture."<<endl;
 		tio->add(t);
 	}
@@ -198,13 +199,14 @@ void u(lssr::TextureIO* tio, int &sel)
 				unsigned short int tc = 0;
 				cin>>tc;
 				unsigned char depth = img.depth() == CV_8U ? 1 : 2;
-				// calculate features
-				unsigned short numFeatures = 0;
-				unsigned char numComps = 0;
-				float* features = lssr::ImageProcessor::calcSURF(img, numFeatures, numComps);
+
 				//create Texture
-				lssr::Texture* t = new lssr::Texture(img.size().width, img.size().height, img.channels(), depth, tc, numFeatures, numComps, features);
+				lssr::Texture* t = new lssr::Texture(img.size().width, img.size().height, img.channels(), depth, tc, 0, 0 ,0);
 				memcpy(t->m_data, img.data, img.size().width * img.size().height * img.channels() * depth);
+
+				// calculate features
+				lssr::ImageProcessor::calcSURF(t);
+		
 				tio->update(sel, t);
 				cout<<"\t(u)dated texture #"<<sel<<"."<<endl; 
 			}
