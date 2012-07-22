6d88951eda567626cef431ce13de516aad82a784
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-07-22 11:19:24 +0200

    work in progress...

--- src/statstrain/statstrain.cpp
+++ src/statstrain/statstrain.cpp
@@ -36,6 +36,8 @@
 #include <cstdlib>
 #include <iomanip>
 #include <sstream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 using namespace std;
 
@@ -56,14 +58,39 @@ int main( int argc, char ** argv )
 	lssr::TextureIO* tio = new lssr::TextureIO(argv[1]);
 
 	//Generate training data randomly
-	vector<Texture*> tData;
+	vector<lssr::Texture*> tData;
 	for (int i = 0; i < tio->m_textures.size(); i++)
 	{
-		Texture* t = 0; //TODO
-	
+		//choose random size for subrect
+		int width  = rand() % tio->m_textures[i]->m_width  + 20;
+		int height = rand() % tio->m_textures[i]->m_height + 20;
+
+		//choose a random center for the subrect
+		int cx = rand() % (tio->m_textures[i]->m_width  - 10) + 10;
+		int cy = rand() % (tio->m_textures[i]->m_height - 10) + 10;
+
+		//extract the subrect
+		cv::Mat img(cv::Size(tio->m_textures[i]->m_width, tio->m_textures[i]->m_height), CV_MAKETYPE(tio->m_textures[i]->m_numBytesPerChan * 8, tio->m_textures[i]->m_numChannels), tio->m_textures[i]->m_data);
+		cv::Mat dst;
+		cv::Size size(width, height);
+		cv::Point2f center(cx, cy);
+		cv::getRectSubPix(img, size, center, dst);
+		unsigned char depth = dst.depth() == CV_8U ? 1 : 2;
+		lssr::Texture* t = new lssr::Texture(dst.size().width, dst.size().height, dst.channels(), depth, 0, 0, 0 ,0, 0, true, 0, 0);
+		memcpy(t->m_data, dst.data, dst.size().width * dst.size().height * dst.channels() * depth);
 		//calculate stats for training data
-		ImageProcessor::calcStats(t, 16); //TODO: Param oder member
+		lssr::ImageProcessor::calcStats(t, 16); //TODO: Param oder member
 		tData.push_back(t);
+
+/*		cv::startWindowThread();
+		cv::Mat img2(cv::Size(t->m_width, t->m_height),
+			    CV_MAKETYPE(t->m_numBytesPerChan * 8,
+			    t->m_numChannels), t->m_data);
+		cv::namedWindow("MyWindow", CV_WINDOW_AUTOSIZE);
+		cv::imshow("MyWindow", img2);
+		cv::waitKey();
+		cv::destroyAllWindows();
+*/
 	}	
 
 	//calculate stat diffs
@@ -74,9 +101,9 @@ int main( int argc, char ** argv )
 		for (int j = 0; j < tio->m_textures.size(); j++)
 		{
 			x[i][j] = new float[14];
-			for (int k = 0; k < 14; ++)
+			for (int k = 0; k < 14; k++)
 			{
-				x[j][j][k] = fabs(tio->m_textures[i]->m_stats[k] - tData[j]->m_stats[k]);
+				x[i][j][k] = fabs(tio->m_textures[i]->m_stats[k] - tData[j]->m_stats[k]);
 			}
 		}
 	}
@@ -84,9 +111,9 @@ int main( int argc, char ** argv )
 
 	float coeffs[14];
 	//Write zimpl file
-
+//TODO
 	//Solve LP
-
+//TODO
 	//write sc.co
 	ofstream out("sc.co");
 	for (int i = 0; i < 14; i++)
@@ -94,7 +121,8 @@ int main( int argc, char ** argv )
 		out<<coeffs[i];
 		if (i < 13) 
 		{
-			<<" ";
+			out<<" ";
+
 		}
 	}
 	out.close();
