ddc1a482516bb4935fb7d01b3a7a3a8a4683b31f
dofeldsc <dofeldsc@kbs8.kbs>
2012-12-18 15:51:31 +0100

    Meine ersten Versuchen mit MPI ins Repository eingecheckt

---
+++ MPI_Versuche/build/CMakeFiles/MPI_start.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/MPI_start.dir/src/MPI_start.cpp.o"
+  "MPI_start.pdb"
+  "MPI_start"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/MPI_start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
