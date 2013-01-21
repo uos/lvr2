89079d2652ef92a36aa96973267ab2afd511e630
dofeldsc <dofeldsc@kbs11.kbs>
2013-01-21 12:14:59 +0100

    Ordner Mpi angefuegt aber noch nicht lauffähig

---
+++ src/lib/mpi/tmpmain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <vector>
+#include "KdTree.h"
+/**
+ * @brief   Main entry point for the LSSR surface executable
+ */
+int main(int argc, char** argv)
+{
+	std::cout << "!!!!!!!!!!!!!!Eigene Main gestartet!!!!!!!!!!!!!!!!!!!!" << endl;
+	lssr::KdTree<std::vector> tmp();
+
+	//lssr::KdTree<ColorVertex<float, unsigned char>> tmp();
+
+	std::cout << "!!!!!!!!!!!!!!Programm ist durchgelaufen, scan1 bis n müssten zur Verfuegung stehen!!!!!!!!!!!!!!!!!!!!" << endl;
+}
