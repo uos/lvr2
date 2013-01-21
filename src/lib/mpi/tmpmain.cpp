fa91b37df43444606121b1383f7a5cfe67328d11
dofeldsc <dofeldsc@kbs11.kbs>
2013-01-21 14:19:45 +0100

    Änderung in lib/mpi/KdTree.tcc , jedoch noch nicht getestet!

--- src/lib/mpi/tmpmain.cpp
+++ src/lib/mpi/tmpmain.cpp
@@ -4,12 +4,18 @@
 /**
  * @brief   Main entry point for the LSSR surface executable
  */
+using namespace lssr;
+
+#include "geometry/ColorVertex.hpp"
+
+typedef ColorVertex<float, unsigned char>      cVertex;
+typedef KdTree<cVertex>                        kd;
 int main(int argc, char** argv)
 {
 	std::cout << "!!!!!!!!!!!!!!Eigene Main gestartet!!!!!!!!!!!!!!!!!!!!" << endl;
-	lssr::KdTree<std::vector> tmp();
+	//lssr::KdTree<std::vector> tmp();
 
-	//lssr::KdTree<ColorVertex<float, unsigned char>> tmp();
+	kd tmp();
 
 	std::cout << "!!!!!!!!!!!!!!Programm ist durchgelaufen, scan1 bis n müssten zur Verfuegung stehen!!!!!!!!!!!!!!!!!!!!" << endl;
 }
