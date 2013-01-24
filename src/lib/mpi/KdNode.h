2846fe56569bae7515ed21c483a2378818d80ff0
dofeldsc <dofeldsc@kbs11.kbs>
2013-01-24 10:29:52 +0100

    "Aufteilung der Punktwolke funktioniert, das Abspeichern der Dateien jeddoch nicht"

--- src/lib/mpi/KdNode.h
+++ src/lib/mpi/KdNode.h
@@ -14,21 +14,39 @@
 #include "geometry/Vertex.hpp"
 
 
+
 namespace lssr{
 template<typename VertexT>
 class KdNode {
 public:
+	/**
+	 * @brief Der Konstruktor setzt die initialen Werte.
+	 *
+	 * @param points IndexBuffer der Punktwolke bzw- der Teilpunktwolke
+	 * @param min Dieser Vektor repräsentiert den "untersten" Punkt der Szene ( minimaler x,y und z Wert)
+	 * @param max Dieser Vektor repräsentiert den "obersten" Punkt der Szene (maximaler x, y, und z Wert)
+	 */
 	KdNode(coord3fArr points, VertexT min, VertexT max);
 	virtual ~KdNode();
 
+	/**
+	 * @brief Gibt die Anzahl der enthaltenen Punkte des Nodes zurueck.
+	 */
 	double getnumpoints();
 
+	/**
+	 * @brief Setzt die Anzahl der enthaltenen Punkte des Nodes.
+	 *
+	 * @param num Zu setzender Wert.
+	 */
 	void setnumpoints(double num);
 
+	/* Index Buffer fuer die Punkte */
 	coord3fArr node_points;
 
 	// Anzahl Punkte im Node
 	double m_numpoints;
+
 
 	/* The Bounce of the BoundingBox */
 	VertexT m_maxvertex;
