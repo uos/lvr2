a5b5c21d344f3f1b7ec46f20fe120c8582cd4dc5
Thomas Wiemann <twiemann@uos.de>
2013-08-09 10:30:11 +0200

    Started writing leica_convert

---
+++ build/src/tools/leica_converter/CMakeFiles/leica_convert.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/SSD/meshing.twiemann/src/tools/leica_converter/Main.cpp" "/SSD/meshing.twiemann/build/src/tools/leica_converter/CMakeFiles/leica_convert.dir/Main.cpp.o"
+  "/SSD/meshing.twiemann/src/tools/leica_converter/Options.cpp" "/SSD/meshing.twiemann/build/src/tools/leica_converter/CMakeFiles/leica_convert.dir/Options.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "_USE_PCL_"
+  "_PCL_VERSION_GT_12_"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/SSD/meshing.twiemann/build/src/liblvr/CMakeFiles/staticlvr.dir/DependInfo.cmake"
+  "/SSD/meshing.twiemann/build/ext/laslib/CMakeFiles/laslib_static.dir/DependInfo.cmake"
+  "/SSD/meshing.twiemann/build/ext/obj/CMakeFiles/obj.dir/DependInfo.cmake"
+  "/SSD/meshing.twiemann/build/ext/slam6d/CMakeFiles/slam6d_static.dir/DependInfo.cmake"
+  "/SSD/meshing.twiemann/build/ext/rply/CMakeFiles/rply_local_static.dir/DependInfo.cmake"
+  )
