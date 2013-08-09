b5dc3f3c8621a92c3a49ad7571cdc30d1531034c
Thomas Wiemann <twiemann@uos.de>
2013-08-09 10:31:06 +0200

    Started writing leica_convert

---
+++ src/tools/leica_converter/Main.cpp
@@ -0,0 +1,111 @@
+/* Copyright (C) 2013 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *
+ * Main.cpp
+ *
+ *  Created on: Aug 9, 2013
+ *      Author: Thomas Wiemann
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+#include <boost/filesystem.hpp>
+
+#include "Options.hpp"
+#include "io/Timestamp.hpp"
+#include "io/ModelFactory.hpp"
+
+using namespace lvr;
+
+int main(int argc, char** argv)
+{
+	// Parse command line arguments
+	leica_convert::Options options(argc, argv);
+
+	boost::filesystem::path inputDir(options.getInputDir());
+	boost::filesystem::path outputDir(options.getOutputDir());
+
+	// Check input directory
+	if(!boost::filesystem::exists(inputDir))
+	{
+		cout << timestamp << "Error: Directory " << options.getInputDir() << " does not exist" << endl;
+		exit(-1);
+	}
+
+	// Check if output dir exists
+	if(!boost::filesystem::exists(outputDir))
+	{
+		cout << timestamp << "Creating directory " << options.getOutputDir() << endl;
+		if(!boost::filesystem::create_directory(outputDir))
+		{
+			cout << timestamp << "Error: Unable to create " << options.getOutputDir() << endl;
+			exit(-1);
+		}
+	}
+
+	// Create director iterator and parse supported file formats
+	boost::filesystem::directory_iterator end;
+	vector<boost::filesystem::path> v;
+	for(boost::filesystem::directory_iterator it(inputDir); it != end; ++it)
+	{
+		string extension = "";
+		if(options.getInputFormat() == "PLY")
+		{
+			extension = ".ply";
+		}
+		else if(options.getInputFormat() == "DAT")
+		{
+			extension = ".dat";
+		}
+		else if(options.getInputFormat() == "ALL")
+		{
+
+		}
+
+		if(it->path().extension() == extension)
+		{
+			v.push_back(it->path());
+		}
+	}
+
+	// Sort entries
+	sort(v.begin(), v.end());
+
+	int c = 0;
+	for(vector<boost::filesystem::path>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		if(options.getOutputFormat() == "SLAM")
+		{
+			cout << timestamp << "Reading point cloud data from " << it->c_str() << "." << endl;
+			ModelPtr model = ModelFactory::readModel(string(it->c_str()));
+			if(model)
+			{
+				char name[1024];
+				sprintf(name, "%s/scan%03d.3d", c);
+				cout << name << endl;
+			}
+		}
+	}
+
+	return 0;
+}
+
+
---
+++ src/tools/leica_converter/Options.hpp
@@ -0,0 +1,110 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+
+ /*
+ * Options.h
+ *
+ *  Created on: Nov 21, 2010
+ *      Author: Thomas Wiemann
+ */
+
+#ifndef OPTIONS_H_
+#define OPTIONS_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/program_options.hpp>
+
+using std::ostream;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+namespace leica_convert
+{
+
+using namespace boost::program_options;
+
+/**
+ * @brief A class to parse the program options for the reconstruction
+ * 		  executable.
+ */
+class Options {
+public:
+
+	/**
+	 * @brief 	Ctor. Parses the command parameters given to the main
+	 * 		  	function of the program
+	 */
+	Options(int argc, char** argv);
+	virtual ~Options();
+
+	string	getInputDir() const;
+	string 	getOutputDir() const;
+	string 	getOutputFormat() const;
+	string 	getInputFormat() const;
+	bool	filter() const;
+	int		getK() const;
+	float	getSigma() const;
+	int		getTargetSize() const;
+
+private:
+
+	/// Output file name
+	string m_outputFile;
+
+    /// The internally used variable map
+    variables_map                   m_variables;
+
+    /// The internally used option description
+    options_description             m_descr;
+
+    /// The internally used positional option desription
+    positional_options_description  m_pdescr;
+
+};
+
+
+/// Overlaoeded outpur operator
+inline ostream& operator<<(ostream& os, const Options &o)
+{
+	cout << "##### Program options: " 	<< endl;
+	cout << "##### Input dir \t\t: "  << o.getInputDir() << endl;
+	cout << "##### Output dir \t\t: " 	<< o.getOutputDir() << endl;
+	if(o.filter())
+	{
+		cout << "##### Filter  \t\t\t: YES" << endl;
+		cout << "##### K  \t\t\t\t: " << o.getK() << endl;
+		cout << "##### Sigma \t\t\t: " << o.getSigma() << endl;
+	}
+	else
+	{
+		cout << "##### Filter  \t\t\t: NO" << endl;
+	}
+	cout << "##### Target Size \t: " << o.getTargetSize() << endl;
+	return os;
+}
+
+} // namespace reconstruct
+
+
+#endif /* OPTIONS_H_ */
