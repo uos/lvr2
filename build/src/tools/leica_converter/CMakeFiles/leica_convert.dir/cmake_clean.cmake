2b07aae5a526d394850ffab1b9e5b9c8534c6275
Thomas Wiemann <twiemann@uos.de>
2013-08-09 10:30:11 +0200

    Started writing leica_convert

---
+++ build/src/tools/leica_converter/CMakeFiles/leica_convert.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/leica_convert.dir/Options.cpp.o"
+  "CMakeFiles/leica_convert.dir/Main.cpp.o"
+  "../../../../bin/leica_convert.pdb"
+  "../../../../bin/leica_convert"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/leica_convert.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
