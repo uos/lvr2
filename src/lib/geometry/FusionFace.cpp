8275a8313e55b06789752d4c7a2b0265417a4bb6
Henning Deeken <hdeeken@uos.de>
2013-09-06 01:01:17 +0200

    made fusion functionality compatible with working branch

---
+++ src/lib/geometry/FusionFace.cpp
@@ -0,0 +1,59 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+
+/*
+ * FusionFace.cpp
+ *
+ *  @date 03.12.2008
+ *  @author Henning Deeken (hdeeken@uos.de)
+ *  @author Ann-Katrin Häuser (ahaeuser@uos.de)
+ */
+
+#include "FusionFace.hpp"
+#include "FusionVertex.hpp"
+
+// Forward declaration
+template<typename A, typename B> class FusionVertex;
+
+namespace lssr
+{
+
+template<typename VertexT, typename NormalT> FusionFace<VertexT, NormalT>::FusionFace() 
+{
+ r = 0;
+ g = 255;
+ b = 0;	
+}
+
+template<typename VertexT, typename NormalT> FusionFace<VertexT, NormalT>::~FusionFace() {}
+
+/*
+template<typename VertexT, typename NormalT> FusionFace<VertexT, NormalT>::FusionFace(const FusionFace<VertexT, NormalT> &o)
+{
+	vertices = o.vertices;
+	r = o.r;
+	g = o.g;
+	b = o.b;
+	m_index = o.m_index;
+    m_normal =o.m_normal;
+    m_is_border_face = o.m_is_border_face;
+}
+* */
+
+} // namespace lssr
