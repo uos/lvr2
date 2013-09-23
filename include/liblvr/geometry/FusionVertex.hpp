55a2e16c9e8f7188482c7737d2bdadd624478fcb
Ann-Katrin Haeuser <ahaeuser@uos.de>
2013-09-23 12:13:45 +0200

    changed intersectIntegrate, to cut off overlapping corners
    need test cases, to check

--- include/liblvr/geometry/FusionVertex.hpp
+++ include/liblvr/geometry/FusionVertex.hpp
@@ -89,7 +89,7 @@ public:
 	size_t 				m_self_index;
 	
 	/// The vertex faces index in the mesh
-	vector<int> 		m_face_indices;
+	//vector<int> 		m_face_indices;
 
 	/// Indicator if vertex is part of the mesh boundary
 	bool is_border_vertex;
