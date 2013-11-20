7cddbf8a54c409feaa26e35ef0d17f82706b9152
Simon Herkenhoff <sherkenh@gmail.com>
2013-11-20 21:33:36 +0100

    prepared the classifier executable

--- src/tools/classifier/Options.cpp
+++ src/tools/classifier/Options.cpp
@@ -17,38 +17,76 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  */
 
+
 /**
- * @file Options.cpp
- * @author Simon Herkenhoff <sherkenh@uni-osnabrueck.de>
- */
+* @file Options.cpp
+* @author Simon Herkenhoff <sherkenh@uni-osnabrueck.de>
+*/
 
 #include "Options.hpp"
 #include <omp.h>
 #include <fstream>
 
-namespace reconstruct{
+namespace classifier
+{
 
-	Options::Options(int argc, char** argv) : m_descr("Supported options")
+Options::Options(int argc, char** argv) : m_descr("Supported options")
+{
+
+	// Create option descriptions
+
+	m_descr.add_options()
+	("help", "Produce help message")
+	("threads", value<int>(&m_numThreads)->default_value( omp_get_num_procs() ), "Number of threads")
+	("inputFile", value< vector<string> >(), "Input file name. Supported formats are ASCII (.pts, .xyz) and .ply")
+	;
+
+	m_pdescr.add("inputFile", -1);
+
+	// Parse command line and generate variables map
+	store(command_line_parser(argc, argv).options(m_descr).positional(m_pdescr).run(), m_variables);
+	notify(m_variables);
+
+	if(m_variables.count("help"))
 	{
-
-		// Create option descriptions
-		m_descr.add_options()
-			("help", "Produce help message")
-			;
-
-		// Parse command line and generate variables map
-		store(command_line_parser(argc, argv).options(m_descr).positional(m_pdescr).run(), m_variables);
-		notify(m_variables);
-
-		if(m_variables.count("help")) {
-			::std::cout<< m_descr << ::std::endl;
-		}
-
+		::std::cout << m_descr << ::std::endl;
 	}
 
-	Options::~Options() {
-		// TODO Auto-generated destructor stub
+}
+
+string Options::getInputFileName() const
+{
+	return (m_variables["inputFile"].as< vector<string> >())[0];
+}
+
+int Options::getNumThreads() const
+{
+	return m_variables["threads"].as<int>();
+}
+
+bool Options::printUsage() const
+{
+	if(!m_variables.count("inputFile"))
+	{
+		cout << "Error: You must specify an input file." << endl;
+		cout << endl;
+		cout << m_descr << endl;
+		return true;
 	}
+
+	if(m_variables.count("help"))
+	{
+		cout << endl;
+		cout << m_descr << endl;
+		return true;
+	}
+	return false;
+}
+
+Options::~Options()
+{
+	// TODO Auto-generated destructor stub
+}
 
 }
 
