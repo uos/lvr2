5d0523f7a05fb99ea71b45ec9fe79009d8ab9e7d
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2014-02-06 15:24:43 +0100

    New viewer app started. Renders point clouds and meshes via vtk

---
+++ src/tools/viewer/vtkBridge/LVRPointBufferBridge.cpp
@@ -0,0 +1,89 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+/**
+ * LVRPointBufferBridge.cpp
+ *
+ *  @date Feb 6, 2014
+ *  @author Thomas Wiemann
+ */
+#include "LVRPointBufferBridge.hpp"
+
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkCellArray.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkPoints.h>
+#include <vtkActor.h>
+
+namespace lvr
+{
+
+LVRPointBufferBridge::LVRPointBufferBridge(PointBufferPtr pointCloud) :
+    m_pointCloud(pointCloud)
+{
+    computePointCloudActor();
+}
+
+
+
+LVRPointBufferBridge::~LVRPointBufferBridge()
+{
+}
+
+void LVRPointBufferBridge::computePointCloudActor()
+{
+    if(m_pointCloud)
+    {
+        m_pointCloudActor= vtkSmartPointer<vtkActor>::New();
+
+        // Setup a poly data object
+        vtkSmartPointer<vtkPolyData>    vtk_polyData = vtkSmartPointer<vtkPolyData>::New();
+        vtkSmartPointer<vtkPoints>      vtk_points = vtkSmartPointer<vtkPoints>::New();
+        vtkSmartPointer<vtkCellArray>   vtk_cells = vtkSmartPointer<vtkCellArray>::New();
+
+        double point[3];
+        size_t n;
+        floatArr points = m_pointCloud->getPointArray(n);
+        for(vtkIdType i = 0; i < n; i++)
+        {
+            point[0] = points[3 * i    ];
+            point[1] = points[3 * i + 1];
+            point[2] = points[3 * i + 2];
+            vtk_points->InsertNextPoint(point);
+            vtk_cells->InsertNextCell(1, &i);
+        }
+
+        vtk_polyData->SetPoints(vtk_points);
+        vtk_polyData->SetVerts(vtk_cells);
+
+        // Create poly data mapper and generate actor
+        vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+        mapper->SetInput(vtk_polyData);
+
+        m_pointCloudActor->SetMapper(mapper);
+    }
+}
+
+vtkSmartPointer<vtkActor> LVRPointBufferBridge::getPointCloudActor()
+{
+    return m_pointCloudActor;
+}
+
+
+} /* namespace lvr */
