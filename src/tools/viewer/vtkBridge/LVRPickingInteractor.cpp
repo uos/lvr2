6ff69250e0937a597912b38cb15450e2e7d502fa
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2014-02-19 15:28:30 +0100

    First version of picking interactor integrated

---
+++ src/tools/viewer/vtkBridge/LVRPickingInteractor.cpp
@@ -0,0 +1,124 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+/**
+ * LVRPickingInteractor.cpp
+ *
+ *  @date Feb 19, 2014
+ *  @author Thomas Wiemann
+ */
+#include "LVRPickingInteractor.hpp"
+
+#include <vtkObjectFactory.h>
+#include <vtkTextProperty.h>
+#include <vtkRenderWindowInteractor.h>
+
+namespace lvr
+{
+
+LVRPickingInteractor::LVRPickingInteractor(vtkSmartPointer<vtkRenderer> renderer) :
+        m_renderer(renderer)
+{
+    m_pickMode = None;
+    m_correspondenceMode = false;
+    vtkSmartPointer<vtkTextProperty> p = vtkSmartPointer<vtkTextProperty>::New();
+    p->SetColor(1.0, 1.0, 0.0);
+    p->SetBold(1);
+    p->SetShadow(0);
+
+    m_textActor = vtkSmartPointer<vtkTextActor>::New();
+    m_textActor->SetDisplayPosition(100, 10);
+    m_textActor->SetTextProperty(p);
+    m_textActor->SetInput("Pick a point...");
+    m_textActor->VisibilityOff();
+    m_renderer->AddActor(m_textActor);
+
+}
+
+LVRPickingInteractor::~LVRPickingInteractor()
+{
+    // TODO Auto-generated destructor stub
+}
+
+
+void LVRPickingInteractor::correspondenceSearchOn()
+{
+    vtkRenderWindowInteractor *rwi = this->Interactor;
+    m_correspondenceMode = true;
+    m_textActor->SetInput("Pick first correspondence point...");
+    m_textActor->VisibilityOn();
+    rwi->Render();
+    m_pickMode = PickFirst;
+}
+
+void LVRPickingInteractor::correspondenceSearchOff()
+{
+    vtkRenderWindowInteractor *rwi = this->Interactor;
+    m_correspondenceMode = false;
+    m_pickMode = None;
+    m_textActor->VisibilityOff();
+    rwi->Render();
+}
+
+void LVRPickingInteractor::OnLeftButtonDown()
+{
+    vtkInteractorStyleTrackballCamera::OnLeftButtonDown();
+}
+
+
+void LVRPickingInteractor::OnKeyPress()
+{
+
+}
+
+void LVRPickingInteractor::OnKeyDown()
+{
+    vtkRenderWindowInteractor *rwi = this->Interactor;
+    std::string key = rwi->GetKeySym();
+
+    if(key == "a" && m_correspondenceMode)
+    {
+        m_textActor->SetInput("Pick first correspondence point...");
+        m_textActor->VisibilityOn();
+        rwi->Render();
+        m_pickMode = PickFirst;
+    }
+
+    if(key == "y" && m_correspondenceMode)
+    {
+        m_textActor->SetInput("Pick second correspondence point...");
+        m_textActor->VisibilityOn();
+        rwi->Render();
+        m_pickMode = PickSecond;
+    }
+
+    if(key == "q")
+    {
+        m_textActor->VisibilityOff();
+        m_pickMode = None;
+        rwi->Render();
+    }
+
+}
+
+void LVRPickingInteractor::OnKeyRelease()
+{
+
+}
+
+} /* namespace lvr */
