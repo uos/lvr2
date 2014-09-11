0299fc0a21d6fb3ff4bbfe3c140540133e5ef07c
Jan Toennemann <jtoenne@kbs2.kbs>
2014-09-11 14:13:24 +0200

    changed normal estimation code; requires Matrix4 to add support for lvr::Pose

--- src/tools/viewer/widgets/LVRReconstructionEstimateNormalsDialog.cpp
+++ src/tools/viewer/widgets/LVRReconstructionEstimateNormalsDialog.cpp
@@ -36,43 +36,43 @@ void LVREstimateNormalsDialog::estimateNormals()
 
     PointBufferPtr pc = m_pc->getPointBuffer();
     size_t numPoints = m_pc->getNumPoints();
-    floatArr points = pc->getPointArray(numPoints);
-    floatArr normals = pc->getPointNormalArray(numPoints);
 
-    typename SearchTree<Vertex<float> >::Ptr       tree;
-    #ifdef _USE_PCL_
-            tree = SearchTree<Vertex<float> >::Ptr( new SearchTreeFlann<Vertex<float> >(pc, numPoints, ki, ki, ki) );
-    #else
-            cout << timestamp << "Warning: PCL is not installed. Using STANN search tree in AdaptiveKSearchSurface." << endl;
-            tree = SearchTree<Vertex<float> >::Ptr( new SearchTreeStann<Vertex<float> >(pc, numPoints, ki, ki, ki) );
-    #endif
+    // Create buffer arrays
+    floatArr points(new float[3 * numPoints]);
+    floatArr normals(new float[3 * numPoints]);
 
-    #pragma omp parallel for schedule(static)
-    for(size_t i = 0; i < numPoints; i++)
+    // Get transformation from frames or pose files if possible
+    Matrix4<float> transform;
+    //transform.toPostionAngle(m_parent->getPose());
+    // Matrix4 unterstützt lvr::Pose noch nicht
+
+    float x, y, z, nx, ny, nz;
+    size_t pointsRead = 0;
+
+    do
     {
-        // Create search tree
-        vector< ulong > indices;
-        vector< double > distances;
+        // Transform normal according to pose
+        Normal<float> normal(nx, ny, nz);
+        Vertex<float> point(x, y, z);
+        normal = transform * normal;
+        point = transform * point;
 
-        Vertex<float> vertex(points[3 * i], points[3 * i + 1], points[3 * i + 2]);
-        tree->kSearch(vertex, ki, indices, distances);
+        // Write data into buffer
+        points[pointsRead * 3]     = point.x;
+        points[pointsRead * 3 + 1] = point.y;
+        points[pointsRead * 3 + 2] = point.z;
 
-        // Do interpolation
-        Normal<float> normal(normals[3 * i], normals[3 * i + 1], normals[3 * i + 2]);
-        for(int j = 0; j < indices.size(); j++)
-        {
-            normal += Normal<float>(normals[3 * indices[j]], normals[3 * indices[j] + 1], normals[3 * indices[j] + 2]);
-        }
-        normal.normalize();
+        normals[pointsRead * 3]     = normal.x;
+        normals[pointsRead * 3 + 1] = normal.y;
+        normals[pointsRead * 3 + 2] = normal.z;
+        pointsRead++;
+    } while(pointsRead < numPoints);
 
-        // Save results in buffer (I know i should use a seperate buffer, but for testing it
-        // should be OK to save the interpolated values directly into the input buffer)
-        normals[3 * i]      = normal.x;
-        normals[3 * i + 1]  = normal.y;
-        normals[3 * i + 2]  = normal.z;
-    }
+    PointBufferPtr new_pc = PointBufferPtr( new PointBuffer );
+    new_pc->setPointArray(points, numPoints);
+    new_pc->setPointNormalArray(normals, numPoints);
 
-    ModelPtr model(new Model(pc));
+    ModelPtr model(new Model(new_pc));
 
     ModelBridgePtr bridge(new LVRModelBridge(model));
     vtkSmartPointer<vtkRenderer> renderer = m_renderWindow->GetRenderers()->GetFirstRenderer();
