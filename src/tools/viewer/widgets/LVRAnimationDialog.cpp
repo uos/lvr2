91f02ecd64b2315f69f7e8f67687855e8107f577
Jan Toennemann <jtoenne@kbs2.kbs>
2014-09-17 11:48:34 +0200

    added frame deletion, changed frame counter to produce unique numbers

--- src/tools/viewer/widgets/LVRAnimationDialog.cpp
+++ src/tools/viewer/widgets/LVRAnimationDialog.cpp
@@ -29,6 +29,7 @@ LVRAnimationDialog::~LVRAnimationDialog()
 void LVRAnimationDialog::connectSignalsAndSlots()
 {
     QObject::connect(m_dialog->addFrame_button, SIGNAL(pressed()), this, SLOT(addFrame()));
+    QObject::connect(m_dialog->removeFrame_button, SIGNAL(pressed()), this, SLOT(removeFrame()));
     QObject::connect(m_dialog->clearFrames_button, SIGNAL(pressed()), this, SLOT(clearFrames()));
     QObject::connect(m_dialog->play_button, SIGNAL(pressed()), this, SLOT(play()));
 }
@@ -36,14 +37,19 @@ void LVRAnimationDialog::connectSignalsAndSlots()
 void LVRAnimationDialog::addFrame()
 {
     cout << "Frame added." << endl;
-    QString frameCount = QString("Frame no. %1").arg(m_dialog->timeline_list->count() + 1);
+    QString frameCount = QString("Frame no. %1").arg(++m_frameCounter);
     QListWidgetItem* currentFrame = new LVRRecordedFrameItem(m_pathCamera, frameCount);
     m_dialog->timeline_list->addItem(currentFrame);
 }
 
 void LVRAnimationDialog::removeFrame()
 {
-
+    QListWidgetItem* currentItem = m_dialog->timeline_list->currentItem();
+    if(currentItem)
+    {
+        cout << "Deleting " << currentItem->text().toStdString() << "..." << endl;
+        delete currentItem;
+    }
 }
 
 void LVRAnimationDialog::clearFrames()
