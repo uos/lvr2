931c4ccdfff5bf56cf35c4a07e5c909c3dc27ed2
Jan Toennemann <jtoenne@kbs2.kbs>
2014-09-19 11:02:32 +0200

    added debug output to picking interactor

--- src/tools/viewer/vtkBridge/LVRPickingInteractor.hpp
+++ src/tools/viewer/vtkBridge/LVRPickingInteractor.hpp
@@ -61,6 +61,10 @@ public:
 
     virtual void OnKeyDown();
 
+    virtual void OnTimer();
+    virtual void OnExpose();
+    virtual void OnConfigure();
+
     /**
      * @brief   returns the text-actor, needed to readd-it after clearing the render window
      */
