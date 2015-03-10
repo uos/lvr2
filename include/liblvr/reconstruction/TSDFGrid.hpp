71d5232753180d52a6f49f06438ec1ad0e39c52d
Tristan Igelbrink <tigelbri@uos.de>
2015-03-10 09:51:38 +0100

    added slice fusion with fusion cell copieng

--- include/liblvr/reconstruction/TSDFGrid.hpp
+++ include/liblvr/reconstruction/TSDFGrid.hpp
@@ -19,18 +19,35 @@ class TsdfGrid: public HashGrid<VertexT, BoxT>
 public:
 
 	typedef unordered_map<size_t, BoxT*> box_map;
+	typedef unordered_map<size_t, size_t> qp_map;
 	
+	typedef unordered_map<size_t, size_t>::iterator qp_map_iterator;
 	/// Typedef to alias iterators for box maps
-	typedef typename unordered_map<size_t, BoxT*>::iterator  box_map_it;
+	typedef typename unordered_map<size_t, BoxT*>::iterator  box_map_it;;
+
+	/// Typedef to alias iterators to query points
+	typedef typename vector<QueryPoint<VertexT> >::iterator	query_point_it;
 	
-	TsdfGrid(float cellSize,  BoundingBox<VertexT> bb, TsdfT* tsdf, size_t size, bool isVoxelsize = true);
+	TsdfGrid(float cellSize,  BoundingBox<VertexT> bb, TsdfT* tsdf, size_t size,
+			int shiftX, int shiftY, int shiftZ,
+			TsdfGrid<VertexT, BoxT, TsdfT>* lastGrid, bool isVoxelsize = true);
 	virtual ~TsdfGrid();
 	void addTSDFLatticePoint(int index_x, int index_y, int index_z, float distance);
+	box_map getFusionCells() { return m_fusion_cells;}
+	qp_map getFusionIndices() { return m_fusion_qpIndices;}
+	vector<QueryPoint<VertexT> > getFusionPoints() {return m_fusionPoints;}
 	inline int calcIndex(float f)
     {
         return f < 0 ? f-.5:f+.5;
     }
-
+    box_map m_fusion_cells;
+    qp_map	m_fusion_qpIndices;
+    qp_map  m_fusion_oldIndices;
+    vector<QueryPoint<VertexT> > m_fusionPoints;
+    int m_fusionIndex_x;
+    int m_fusionIndex_y;
+    int m_fusionIndex_z;
+    int m_fusionIndex;
 };
 
 } /* namespace lvr */
