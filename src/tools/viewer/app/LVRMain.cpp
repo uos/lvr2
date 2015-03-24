5e74a0e2af67e7b51697db5c2ee74762914ba9cf
Thomas Wiemann <twiemann@uos.de>
2015-03-24 11:01:48 +0100

    Integradted QProgressDialog

--- src/tools/viewer/app/LVRMain.cpp
+++ src/tools/viewer/app/LVRMain.cpp
@@ -9,8 +9,6 @@ int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
-    lvr::ProgressBar::enableDialog();
-
     LVRMainWindow mainWindow;
     mainWindow.parseCommandLine(argc, argv);
     mainWindow.show();
