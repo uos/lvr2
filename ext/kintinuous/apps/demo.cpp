70f363c7ae5fba49cbb26e38bc3dbe9f71b1b2a1
Tristan Igelbrink <tigelbri@uos.de>
2015-07-23 22:28:30 +0200

    vertex fusion in half edge mesh implemented

--- ext/kintinuous/apps/demo.cpp
+++ ext/kintinuous/apps/demo.cpp
@@ -209,7 +209,7 @@ struct KinFuApp
 
             viz.spinOnce(3, true);
 			exit_ = exit_ || ( kinfu.hasShifted() && kinfu.isLastScan() );
-			if(kinfu.cyclical().getSliceCount() == 1)
+			if(kinfu.cyclical().getSliceCount() == 4)
 				take_cloud(kinfu);
         }
         return true;
