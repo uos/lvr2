7fc35f360602df39fda8d2fe8b5f6bbcfa07fad4
Tristan Igelbrink <tigelbri@uos.de>
2015-08-21 11:08:58 +0200

    added online mesh vizualisation

--- ext/kintinuous/kfusion/src/OptimizeStage.cpp
+++ ext/kintinuous/kfusion/src/OptimizeStage.cpp
@@ -1,9 +1,9 @@
 #include <kfusion/OptimizeStage.hpp>
 
 // default constructor
-OptimizeStage::OptimizeStage() : AbstractStage()
+OptimizeStage::OptimizeStage(double camera_target_distance, double voxel_size) : AbstractStage()
+	,mesh_count_(0), camera_target_distance_(camera_target_distance), voxel_size_(voxel_size)
 {
-	mesh_count_ = 0;
 }
 
 void OptimizeStage::firstStep() { /* skip */ };
@@ -18,9 +18,30 @@ void OptimizeStage::step()
 	MeshPtr tmp_pointer = act_mesh->retesselateInHalfEdge();
 	std::cout << "            ####     3 Finished optimisation number: " << mesh_count_ << "   ####" << std::endl;
 	mesh_count_++;
+	transformMeshBack(tmp_pointer);
 	getOutQueue()->Add(pair<MeshPtr, bool>(tmp_pointer, last_shift));
 	delete act_mesh;
 	if(last_shift)
 		done(true);
 }
 void OptimizeStage::lastStep()	{ /* skip */ };
+
+void OptimizeStage::transformMeshBack(MeshPtr mesh)
+{
+	for(auto vert : mesh->getVertices())
+	{
+		// calc in voxel
+		vert->m_position.x 	*= voxel_size_;				
+		vert->m_position.y 	*= voxel_size_;				
+		vert->m_position.z 	*= voxel_size_;			
+		//offset for cube coord to center coord
+		vert->m_position.x 	-= 1.5;				
+		vert->m_position.y 	-= 1.5;				
+		vert->m_position.z 	-= 1.5 - camera_target_distance_;				
+		
+		//offset for cube coord to center coord
+		vert->m_position.x 	-= 150;				
+		vert->m_position.y 	-= 150;				
+		vert->m_position.z 	-= 150;
+	}
+}
