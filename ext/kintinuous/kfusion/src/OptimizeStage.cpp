78e8dc12311b3a7a110455554557a5117758d75c
Tristan Igelbrink <tigelbri@uos.de>
2015-09-09 15:55:08 +0200

    implemented region fusion

--- ext/kintinuous/kfusion/src/OptimizeStage.cpp
+++ ext/kintinuous/kfusion/src/OptimizeStage.cpp
@@ -4,39 +4,38 @@
 OptimizeStage::OptimizeStage(double camera_target_distance, double voxel_size) : AbstractStage()
 	,mesh_count_(0), camera_target_distance_(camera_target_distance), voxel_size_(voxel_size)
 {
+	
 }
 
-void OptimizeStage::firstStep() { optiMesh_ = NULL; };
+void OptimizeStage::firstStep() { optiMesh_ = NULL; }
 
 void OptimizeStage::step()
 {
 	auto mesh_work = boost::any_cast<pair<MeshPtr, bool> >(getInQueue()->Take());
 	bool last_shift = mesh_work.second;
 	MeshPtr act_mesh = mesh_work.first;
+	string mesh_notice = ("#### C:            Mesh Optimization " +  to_string(mesh_count_) + "    ####");
+	ScopeTime* opti_time = new ScopeTime(mesh_notice.c_str());
 	transformMeshBack(act_mesh);
 	if(optiMesh_ == NULL)
 		optiMesh_ = act_mesh;
 	else
 		optiMesh_->addMesh(act_mesh);
+	timestamp.setQuiet(true);
+	optiMesh_->optimizePlanes(3, 0.8, 7, 0);
+	//optiMesh_->optimizePlaneIntersections();
 	
-	optiMesh_->optimizePlanes(1, 0.83, 7, 0);
-	//act_mesh->optimizePlaneIntersections();
 	MeshPtr tmp_pointer = optiMesh_->retesselateInHalfEdge();
-	std::cout << "            ####     3 Finished optimisation number: " << mesh_count_ << "   ####" << std::endl;
+	delete opti_time;
 	mesh_count_++;
-	//getOutQueue()->Add(pair<MeshPtr, bool>(act_mesh, last_shift));
 	getOutQueue()->Add(pair<MeshPtr, bool>(tmp_pointer, last_shift));
-	//delete act_mesh;
 	if(last_shift)
 		done(true);
 }
 void OptimizeStage::lastStep()	
-{ 
-	optiMesh_->finalize();
-	ModelPtr m( new Model( optiMesh_->meshBuffer() ) );
-	ModelFactory::saveModel( m, "./normalMesh_" + to_string(mesh_count_) + ".ply");	 
+{  
 	 
-};
+}
 
 void OptimizeStage::transformMeshBack(MeshPtr mesh)
 {
