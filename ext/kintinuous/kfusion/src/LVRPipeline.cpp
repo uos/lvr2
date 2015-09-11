4d9705a8767a87896f1237405e1c7edbdff49549
Tristan Igelbrink <tigelbri@uos.de>
2015-09-11 08:23:04 +0200

    fixed cam distance

--- ext/kintinuous/kfusion/src/LVRPipeline.cpp
+++ ext/kintinuous/kfusion/src/LVRPipeline.cpp
@@ -7,13 +7,12 @@ namespace kfusion
 	LVRPipeline::LVRPipeline(double camera_target_distance, double voxel_size, bool optimize, string mesh_name) : slice_count_(0)
 	{
 		meshPtr_ = new HMesh();
-		cout << "cam target dist " << camera_target_distance << endl;
 		omp_set_num_threads(omp_get_num_procs());
 		pl_.AddStage(
 			boost::shared_ptr<GridStage>(new GridStage(voxel_size))
 			);
 		pl_.AddStage(
-			boost::shared_ptr<MeshStage>(new MeshStage(camera_target_distance_, voxel_size))
+			boost::shared_ptr<MeshStage>(new MeshStage(camera_target_distance, voxel_size))
 			);
 		if(optimize)
 		{
