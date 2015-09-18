3a24842b1fd4a153e5c9b9f3caaa88c9dd4ee21e
Michael Görner <mgoerner@uos.de>
2015-09-18 16:16:57 +0200

    support VTK 5 as well as 6

--- CMakeModules/FindQVTK.cmake
+++ CMakeModules/FindQVTK.cmake
@@ -11,17 +11,27 @@
 # QVTK_LIBRARY is appended to QVTK_LIBRARY_DIR.
 #
 
-find_library (QVTK_LIBRARY QVTK HINTS ${VTK_DIR} ${VTK_DIR}/bin
-  PATH_SUFFIXES Release Debug)
-find_path (QVTK_INCLUDE_DIR QVTKWidget.h HINT ${VTK_INCLUDE_DIRS})
-find_package_handle_standard_args(QVTK DEFAULT_MSG
-  QVTK_LIBRARY QVTK_INCLUDE_DIR)
+find_package(VTK)
 
-if(NOT QVTK_FOUND)
-  set (VTK_USE_QVTK OFF)
-else(NOT QVTK_FOUND)
-  get_filename_component (QVTK_LIBRARY_DIR ${QVTK_LIBRARY} PATH)
-  set (VTK_LIBRARY_DIRS ${VTK_LIBRARY_DIRS} ${QVTK_LIBRARY_DIR})
+find_path (QVTK_INCLUDE_DIR QVTKWidget.h HINT ${VTK_INCLUDE_DIRS})
+
+if(VTK_MAJOR_VERSION VERSION_LESS "6")
+  find_library (QVTK_LIBRARY QVTK HINTS ${VTK_DIR} ${VTK_DIR}/bin
+      PATH_SUFFIXES Release Debug)
+
+  find_package_handle_standard_args(QVTK DEFAULT_MSG
+    QVTK_LIBRARY QVTK_INCLUDE_DIR)
+else()
+  # VTK 6 doesn't have a dedicated QVTK lib anymore
+  find_package_handle_standard_args(QVTK DEFAULT_MSG
+    QVTK_INCLUDE_DIR)
+  set(QVTK_LIBRARY "" CACHE PATH "stubbed QVTK library [obsolete]")
+endif()
+
+if(QVTK_FOUND)
+  if(VTK_MAJOR_VERSION VERSION_LESS "6")
+    get_filename_component (QVTK_LIBRARY_DIR ${QVTK_LIBRARY} PATH)
+    set (VTK_LIBRARY_DIRS ${VTK_LIBRARY_DIRS} ${QVTK_LIBRARY_DIR})
+  endif()
   set (VTK_INCLUDE_DIRS ${VTK_INCLUDE_DIRS} ${QVTK_INCLUDE_DIR})
-  set (VTK_USE_QVTK ON)
-endif(NOT QVTK_FOUND)
+endif(QVTK_FOUND)
