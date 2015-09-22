6aaaf7386d732d25dc7d35ce111522e61109a85b
Michael Görner <mgoerner@uos.de>
2015-09-23 01:05:47 +0200

    liblvr sources now include headers via <lvr/...>
    it doesn't make sense to include them via "..."
    in these sources, if they internally include other
    lvr headers via <lvr/...>.

--- src/liblvr/texture/CCV.cpp
+++ src/liblvr/texture/CCV.cpp
@@ -24,7 +24,7 @@
  *  @author Kim Rinnewitz (krinnewitz@uos.de)
  */
 
-#include "texture/CCV.hpp"
+#include <lvr/texture/CCV.hpp>
 //#include <opencv2/highgui/highgui.hpp>
 #include <opencv/cv.h>
 
