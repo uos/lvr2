a64275b1182d495436c9f77bcc85aed282d34e61
Tristan Igelbrink <tigelbri@uos.de>
2015-11-04 13:43:50 +0100

    use color information from planes

--- ext/kintinuous/kfusion/src/FusionStage.cpp
+++ ext/kintinuous/kfusion/src/FusionStage.cpp
@@ -36,8 +36,7 @@ void FusionStage::lastStep()
 	ModelPtr m( new Model( mesh_->meshBuffer() ) );
 	if(!options_->textures())
 		ModelFactory::saveModel( m, string(options_->getOutput() + ".ply"));
-	else{
+	else
 		ModelFactory::saveModel( m, string(options_->getOutput() + ".obj"));
-		std::cout << "Finished saving obj " << std::endl; 
-	}
+	std::cout << "Finished saving" << std::endl;
 }
