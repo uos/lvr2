765bbd31196d3373e96b3e30f79f9e9f780a2291
amock <amock@uos.de>
2015-11-20 00:40:24 +0100

    schnelle schatten loesung. nicht perfekt

--- ext/kintinuous/kfusion/include/kfusion/OptimizeStage.hpp
+++ ext/kintinuous/kfusion/include/kfusion/OptimizeStage.hpp
@@ -14,7 +14,6 @@
 #include <geometry/HalfEdgeMesh.hpp>
 #include <geometry/BoundingBox.hpp>
 #include <kfusion/types.hpp>
-#include <geometry/TexturedMesh.hpp>
 
 
 using namespace lvr;
@@ -26,9 +25,7 @@ typedef ColorVertex<float, unsigned char> cVertex;
 typedef FastBox<ColorVertex<float, unsigned char>, lvr::Normal<float> > cFastBox;
 typedef TsdfGrid<cVertex, cFastBox, kfusion::Point> TGrid;
 typedef FastReconstruction<ColorVertex<float, unsigned char>, lvr::Normal<float>, cFastBox > cFastReconstruction;
-typedef TexturedMesh<cVertex, lvr::Normal<float> > TMesh;
 typedef HalfEdgeMesh<cVertex, lvr::Normal<float> > HMesh;
-typedef TMesh* TMeshPtr;
 typedef HMesh* MeshPtr;
 
 class OptimizeStage : public AbstractStage
