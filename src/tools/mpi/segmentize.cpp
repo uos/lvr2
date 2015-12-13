aee368ce376f80a3da7a975ca170aee73ea6e05e
imitschke <imitschke@uos.de>
2015-12-13 23:43:55 +0100

    cleanup

--- src/tools/mpi/segmentize.cpp
+++ src/tools/mpi/segmentize.cpp
@@ -2,9 +2,9 @@
 // Created by eiseck on 08.12.15.
 //
 #include <fstream>
-#include "NodeData.h"
+#include "NodeData.hpp"
 #include <algorithm>
-#include "LargeScaldeOctreeNode.h"
+#include "LargeScaleOctree.hpp"
 #include <string>
 #include <sstream>
 
@@ -17,38 +17,12 @@ void printTest(Vertexf v)
 
 int main(int argc, char** argv)
 {
-/*
-    string filepath(argv[1]);
-    ifstream inputData(filepath);
-    string s;
-    int j = 1;
-    NodeData nd;
-    while( getline( inputData, s ) )
-    {
-        stringstream ss;
-        ss.str(s);
-        Vertexf v;
-        ss >> v.x >> v.y >> v.z;
-        nd.add(v);
-
-    }
-    for(Vertexf vv : nd)
-    {
-        cout << vv;
-    }
-
-    */
-
-
-
-    string filepath(argv[1]);
-    ifstream inputData(filepath);
+    ifstream inputData(argv[1]);
     string s;
     BoundingBox<Vertexf> box;
     int j = 1;
     while( getline( inputData, s ) )
     {
-        //cout << "j: " << j++ << endl;
         stringstream ss;
         ss.str(s);
         Vertexf v;
@@ -62,22 +36,18 @@ int main(int argc, char** argv)
     center/=2;
     cout << box << endl << "--------" << endl;
 
-    LargeScaldeOctreeNode octree(center, size);
+    LargeScaleOctree octree(center, size);
 
     inputData.close();
     inputData.clear();
-    ifstream inputData2(filepath);
-    //inputData2.open(filepath);
-    //inputData.seekg(0, ios::beg);
-    int i = 1;
+    ifstream inputData2(argv[1]);
+
     while( getline( inputData2, s ) )
     {
-        //cout << "i: " << i++ << endl;
         stringstream ss;
         ss.str(s);
         Vertexf v;
         ss >> v.x >> v.y >> v.z;
-        //cout << "Inserting " << v;
         octree.insert(v);
 
     }
