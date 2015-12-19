2d626437485174528da155d1136429280bfe7469
imitschke <imitschke@uos.de>
2015-12-19 15:22:46 +0100

    fixed segfaults

--- src/tools/mpi/LargeScaleReconstruction.cpp
+++ src/tools/mpi/LargeScaleReconstruction.cpp
@@ -25,6 +25,7 @@
 #include <lvr/reconstruction/HashGrid.hpp>
 #include <lvr/reconstruction/FastReconstruction.hpp>
 #include <lvr/reconstruction/PointsetGrid.hpp>
+#include <boost/optional/optional_io.hpp>
 
 namespace mpi = boost::mpi;
 using namespace std;
@@ -41,11 +42,29 @@ int main(int argc, char* argv[])
     mpi::environment env;
     mpi::communicator world;
 
+/*    ifstream ifs(argv[1]);
+    ofstream ofs("schloss4.xyz");
+    string str;
+    int i =0;
+    while( getline( ifs, str ) )
+    {
+        if(i%3==0)
+        {
+            stringstream ss;
+            ss.str(str);
+            Vertexf v;
+            ss >> v.x >> v.y >> v.z;
+            ofs << v.x << v.y << v.z;
+        }
+
+    }*/
+
     //---------------------------------------------
     // MASTER NODE
     //---------------------------------------------
     if (world.rank() == 0)
     {
+        cout << lvr::timestamp << "start" << endl;
         clock_t begin = clock();
         ifstream inputData(argv[1]);
         string s;
@@ -85,98 +104,142 @@ int main(int argc, char* argv[])
         cout << "...Octree finished" << endl;
         clock_t end = clock();
         vector<LargeScaleOctree*> nodes = octree.getNodes() ;
-        vector<LargeScaleOctree*> leafs(nodes.size());
+        vector<LargeScaleOctree*> leafs;
 
-        auto it = std::copy_if (nodes.begin(), nodes.end(), leafs.begin(), [](LargeScaleOctree* oc){return oc->isLeaf();} );
-        leafs.resize(std::distance(nodes.begin(),it));  // shrink container to new size
-
-
+        //auto it = std::copy_if (nodes.begin(), nodes.end(), leafs.begin(), [](LargeScaleOctree* oc){return oc->isLeaf();} );
+        for(int i = 0 ; i< nodes.size() ; i++)
+        {
+            if(nodes[i]->isLeaf() && nodes[i]->getSize()>=100 ) leafs.push_back(nodes[i]);
+        }
+        //leafs.resize(std::distance(nodes.begin(),it));  // shrink container to new size
+        cout << "...Octree finished" << endl;
+        cout << "...got leafs1" << endl;
+        stack<char> waitingfor;
         for(int i = 1 ; i< world.size() && !leafs.empty() ; i++)
         {
+            cout << "...sending to " << i << " do:  " << leafs.back()->getFilePath() << endl;
             world.send(i, DATA, leafs.back()->getFilePath());
+            waitingfor.push(1);
+            cout << "...poping to " << i << endl;
             leafs.pop_back();
         }
+        cout << "...got leafs" << endl;
         //WHILE DATA to send
-        while(! leafs.empty())
+
+        while(! leafs.empty() || waitingfor.size()>0)
         {
             cout << "leafs left: " << leafs.size() << endl;
             string msg;
-            mpi::request requests = world.irecv(boost::mpi::any_source, FINISHED, msg);
-            mpi::wait_all(&requests, &requests);
-            int rid = requests.test()->source();
+            mpi::status requests = world.recv(boost::mpi::any_source, FINISHED, msg);
+            waitingfor.pop();
+            cout << "######## rank 0 got message" << endl;
+
+            //cout << "######## testing " << requests.test()<< endl;
+            int rid = requests.source();
+            cout << "from" << requests.source() << endl;
+            cout << "QQQQQQQQQQQQQQQQQQQQQQQQ " << waitingfor.size() << "|" << leafs.size() << endl;
             if(! leafs.empty())
             {
                 world.send(rid, DATA, leafs.back()->getFilePath());
                 leafs.pop_back();
+                waitingfor.push(1);
+            }
+            else if(waitingfor.size()==0)
+            {
+                for(int i = 1 ; i< world.size() && !leafs.empty() ; i++)
+                {
+                    cout << "...sending to " << i << " finished"  << endl;
+                    world.send(i, DATA, "ready");
+
+                }
+                break;
             }
 
         }
-        double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-        cout << "FINESHED in " << elapsed_secs << " Seconds." << endl;
 
+        cout << "FINESHED in " << lvr::timestamp  << endl;
+        world.abort(0);
     }
         //---------------------------------------------
         // SLAVE NODE
         //---------------------------------------------
     else
     {
-        std::string filePath;
-        world.recv(0, DATA, filePath);
-        std::cout << "NODE: " << world.rank() << " will use file: " << filePath << endl;
-        ModelPtr model = ModelFactory::readModel( filePath );
-        PointBufferPtr p_loader;
-        if ( !model )
+        bool ready = false;
+        while(! ready)
         {
-            cout << timestamp << "IO Error: Unable to parse " << filePath << endl;
-            exit(-1);
+            std::string filePath;
+            world.recv(0, DATA, filePath);
+            if(filePath=="ready") break;
+            std::cout << "NODE: " << world.rank() << " will use file: " << filePath << endl;
+            ModelPtr model = ModelFactory::readModel( filePath );
+            PointBufferPtr p_loader;
+            if ( !model )
+            {
+                cout << timestamp << "IO Error: Unable to parse " << filePath << endl;
+                exit(-1);
+            }
+            p_loader = model->m_pointCloud;
+
+            psSurface::Ptr surface;
+            akSurface* aks = new akSurface(
+                    p_loader, "STANN",
+                    20,
+                    60,
+                    60,
+                    true,
+                    ""
+            );
+
+            surface = psSurface::Ptr(aks);
+            aks->useRansac(true);
+            surface->setKd(20);
+            surface->setKi(60);
+            surface->setKn(60);
+            surface->calculateSurfaceNormals();
+            HalfEdgeMesh<ColorVertex<float, unsigned char> , Normal<float> > mesh( surface );
+            float resolution;
+            bool useVoxelsize;
+            resolution = 0.05;
+            useVoxelsize = true;
+            string decomposition = "PMC";
+            GridBase* grid;
+            FastReconstructionBase<ColorVertex<float, unsigned char>, Normal<float> >* reconstruction;
+            grid = new PointsetGrid<ColorVertex<float, unsigned char>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> > >(resolution, surface, surface->getBoundingBox(), useVoxelsize);
+            grid->setExtrusion(true);
+            BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> >::m_surface = surface;
+            PointsetGrid<ColorVertex<float, unsigned char>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> > >* ps_grid = static_cast<PointsetGrid<ColorVertex<float, unsigned char>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> > > *>(grid);
+            ps_grid->calcDistanceValues();
+
+            reconstruction = new FastReconstruction<ColorVertex<float, unsigned char> , Normal<float>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> >  >(ps_grid);
+
+            reconstruction->getMesh(mesh);
+            //mesh.cleanContours(2);
+            mesh.setClassifier("PlaneSimpsons");
+            mesh.getClassifier().setMinRegionSize(0.1);
+
+            mesh.optimizePlanes(3,
+                                0.85,
+                                7,
+                                0,
+                                true);
+
+            mesh.fillHoles(1);
+            mesh.optimizePlaneIntersections();
+            mesh.restorePlanes(1);
+
+            mesh.finalize();
+            ModelPtr m( new Model( mesh.meshBuffer() ) );
+            cout << timestamp << "Saving mesh." << endl;
+            string output = filePath;
+            output.pop_back();
+            output.pop_back();
+            output.pop_back();
+            output.append("ply");
+            ModelFactory::saveModel( m, output);
+            world.send(0, FINISHED, std::string("world"));
         }
-        p_loader = model->m_pointCloud;
 
-        psSurface::Ptr surface;
-        akSurface* aks = new akSurface(
-                p_loader, "STANN",
-                10,
-                10,
-                5,
-                false,
-                ""
-        );
-
-        surface = psSurface::Ptr(aks);
-        surface->setKd(10);
-        surface->setKi(10);
-        surface->setKn(5);
-        surface->calculateSurfaceNormals();
-        HalfEdgeMesh<ColorVertex<float, unsigned char> , Normal<float> > mesh( surface );
-        float resolution;
-        bool useVoxelsize;
-        resolution = 10;
-        useVoxelsize = true;
-        string decomposition = "PMC";
-        GridBase* grid;
-        FastReconstructionBase<ColorVertex<float, unsigned char>, Normal<float> >* reconstruction;
-        grid = new PointsetGrid<ColorVertex<float, unsigned char>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> > >(resolution, surface, surface->getBoundingBox(), useVoxelsize);
-        grid->setExtrusion(true);
-        BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> >::m_surface = surface;
-        PointsetGrid<ColorVertex<float, unsigned char>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> > >* ps_grid = static_cast<PointsetGrid<ColorVertex<float, unsigned char>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> > > *>(grid);
-        ps_grid->calcDistanceValues();
-
-        reconstruction = new FastReconstruction<ColorVertex<float, unsigned char> , Normal<float>, BilinearFastBox<ColorVertex<float, unsigned char>, Normal<float> >  >(ps_grid);
-
-        reconstruction->getMesh(mesh);
-        mesh.cleanContours(0);
-        mesh.setClassifier("PlaneSimpsons");
-        mesh.getClassifier().setMinRegionSize(10);
-        mesh.finalize();
-        ModelPtr m( new Model( mesh.meshBuffer() ) );
-        cout << timestamp << "Saving mesh." << endl;
-        string output = filePath;
-        output.pop_back();
-        output.pop_back();
-        output.pop_back();
-        output.append(".ply");
-        ModelFactory::saveModel( m, output);
-        world.send(0, 1, std::string("world"));
     }
 
     return 0;
