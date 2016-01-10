d03b6362886e122eb39e776121c19c4f37f1c003
imitschke <imitschke@uos.de>
2016-01-10 23:55:05 +0100

    removed build filder

--- build/ext/laslib/CMakeFiles/lvrlas_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/liblvrlas_static.a"
-)
--- build/src/liblvr/CMakeFiles/lvr.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/liblvr.pdb"
-  "../../lib/liblvr.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/lvr.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
