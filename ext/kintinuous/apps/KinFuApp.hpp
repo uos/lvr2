c85d1e43fde2f4a5c24827d8fc343b9ad3e27216
Thomas Wiemann <twiemann@uos.de>
2016-01-27 11:24:15 +0100

    Started Qt App

--- ext/kintinuous/apps/KinFuApp.hpp
+++ ext/kintinuous/apps/KinFuApp.hpp
@@ -47,8 +47,7 @@ struct KinFuApp
         }
 
         if(event.code == '+')
-        {
-           kinfu.kinfu_->params().distance_camera_target += 0.1;
+        {           kinfu.kinfu_->params().distance_camera_target += 0.1;
            kinfu.kinfu_->performShift();
         }
         if(event.code == '-')
