7a5e062e1c79f44781f9db06b2b52bb387858852
Thomas Wiemann <twiemann@uos.de>
2016-01-27 11:53:14 +0100

    Fiexed segfaults from empty options

--- ext/kintinuous/qt_app/MainWindow.cpp
+++ ext/kintinuous/qt_app/MainWindow.cpp
@@ -12,10 +12,34 @@
 MainWindow::MainWindow(QMainWindow* parent) : QMainWindow(parent)
 {
 	setupUi(this);
+
+	// Create Kinfu object
 	KinFuParams params = KinFuParams::default_params();
 	m_kinfu = KinFu::Ptr( new KinFu(params) );
+
+	// Setup OpenNI Device
 	m_openNISource = new OpenNISource;
+	string device = "0";
+	if(device.find_first_not_of("0123456789") == std::string::npos)
+	{
+		cuda::setDevice (atoi(device.c_str()));
+		cuda::printShortCudaDeviceInfo (atoi(device.c_str()));
+
+		if(cuda::checkIfPreFermiGPU(atoi(device.c_str())))
+		{
+			std::cout << std::endl << "Kinfu does not support pre-Fermi GPU architectures, and is not built for them by default. Exiting..." << std::endl;
+		}
+		m_openNISource->open(atoi(device.c_str()));
+	}
+	else
+	{
+		m_openNISource->open(device);
+		m_openNISource->triggerPause();
+	}
 	m_openNISource->setRegistration(true);
+
+
+
 }
 
 MainWindow::~MainWindow()
