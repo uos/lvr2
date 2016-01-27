b0dca2707719be502baef86cd93e01b63fcbc008
Thomas Wiemann <twiemann@uos.de>
2016-01-27 13:23:18 +0100

    First working raycast rendering

--- ext/kintinuous/qt_app/MainWindow.cpp
+++ ext/kintinuous/qt_app/MainWindow.cpp
@@ -38,12 +38,73 @@ MainWindow::MainWindow(QMainWindow* parent) : QMainWindow(parent)
 	}
 	m_openNISource->setRegistration(true);
 
+	// Generate timer for GPU polling
+	m_timer = new QTimer(this);
+	connect(m_timer, SIGNAL(timeout()), this, SLOT(pollGPUData()));
+	m_timer->setInterval(0);
+	m_timer->start();
 
+}
 
+void MainWindow::pollGPUData()
+{
+	KinFu& kinfu = *m_kinfu;
+	cv::Mat depth, image, image_copy;
+	double time_ms = 0;
+	int has_image = 0;
+
+	std::vector<Affine3f> posen;
+	std::vector<cv::Mat> rvecs;
+
+	Affine3f best_pose;
+	cv::Mat best_rvec,best_image;
+	float best_dist=0.0;
+
+	if(!(m_kinfu->hasShifted() && m_kinfu->isLastScan()))
+	{
+		int has_frame = m_openNISource->grab(depth, image);
+		cv::flip(depth, depth, 1);
+		cv::flip(image, image, 1);
+
+		if (has_frame == 0)
+		{
+			std::cout << "Can't grab" << std::endl;
+			return;
+		}
+
+		// Check if oni file ended
+		if (has_frame == 2)
+		{
+			m_kinfu->performLastScan();
+		}
+		m_depth_device.upload(depth.data, depth.step, depth.rows, depth.cols);
+		has_image = kinfu(m_depth_device);
+	}
+
+    const int mode = 4;
+    //if (iteractive_mode_)
+    //kinfu.renderImage(view_device_, viz.getViewerPose(), mode);
+    //else
+    m_kinfu->renderImage(m_viewImage, mode);
+
+    cv::Mat view_host;
+    view_host.create(m_viewImage.rows(), m_viewImage.cols(), CV_8UC4);
+    m_viewImage.download(view_host.ptr<void>(), view_host.step);
+
+    m_displayRaycastLabel->setPixmap(QPixmap::fromImage(QImage((unsigned char*) view_host.data, view_host.cols, view_host.rows, QImage::Format_RGB32)));
+    //repaint();
 }
 
 MainWindow::~MainWindow()
 {
-	// TODO Auto-generated destructor stub
+	if(m_timer)
+	{
+		delete m_timer;
+	}
+
+	if(m_openNISource)
+	{
+		delete m_openNISource;
+	}
 }
 
