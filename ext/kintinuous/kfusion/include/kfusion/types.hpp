afabd8113f30bd4a231c53f6e2b9f5ab56852fce
Thomas Wiemann <twiemann@osnanet.de>
2016-02-05 12:27:51 +0100

    First version w. textures

--- ext/kintinuous/kfusion/include/kfusion/types.hpp
+++ ext/kintinuous/kfusion/include/kfusion/types.hpp
@@ -5,7 +5,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/contrib/contrib.hpp>
 #include <opencv2/core/affine.hpp>
-#include <opencv2/viz/vizcore.hpp>
+//#include <opencv2/viz/vizcore.hpp>
 #include <iosfwd>
 
 struct CUevent_st;
