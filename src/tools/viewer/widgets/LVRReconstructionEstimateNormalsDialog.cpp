ed02c8e841f6bc4b9676810b0e5fbe42c2923db8
Thomas Wiemann <twiemann@uos.de>
2016-02-26 15:30:44 +0100

    Fixed PCL Search tree dependency in normal estimation dialog

--- src/tools/viewer/widgets/LVRReconstructionEstimateNormalsDialog.cpp
+++ src/tools/viewer/widgets/LVRReconstructionEstimateNormalsDialog.cpp
@@ -1,12 +1,8 @@
 #include <QFileDialog>
 #include "LVRReconstructionEstimateNormalsDialog.hpp"
 
-#ifdef LVR_USE_PCL
-#include <lvr/reconstruction/SearchTreeFlannPCL.hpp>
-#endif
-#ifdef LVR_USE_STANN
-#include <lvr/reconstruction/SearchTreeStann.hpp>
-#endif
+#include <lvr/reconstruction/SearchTree.hpp>
+#include <lvr/reconstruction/SearchTreeFlann.hpp>
 
 namespace lvr
 {
@@ -107,7 +103,7 @@ void LVREstimateNormalsDialog::estimateNormals()
     if(interpolateNormals)
     {
         SearchTree<Vertex<float> >::Ptr       tree;
-	tree = SearchTree<Vertex<float> >::Ptr( new SearchTreeFlannPCL<Vertex<float> >(new_pc, numPoints, ki, ki, ki) );
+	tree = SearchTree<Vertex<float> >::Ptr( new SearchTreeFlann<Vertex<float> >(new_pc, numPoints, ki, ki, ki) );
 	    
 
         #pragma omp parallel for schedule(static)
