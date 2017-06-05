9e35315c7b836ae28835de1a2875eab3d6aa8a21
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-06-05 19:44:44 +0200

    Add first draft of HalfEdgeMesh class
    This is still super simple: it's just straight from the textbooks
    without any fance methods yet.
    
    All the new stuff is developed in the NS `lvr2` and in `include/lvr2`
    until it is production ready. This include path has been added
    to CMakeLists.txt, but maybe I missed a spot.
    
    Note: my editor removed trailing whitespace automatically in some
    files.

---
+++ include/lvr2/geometry/HalfEdgeFace.hpp
@@ -0,0 +1,53 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+
+/*
+ * HalfEdgeFace.hpp
+ *
+ *  @date 02.06.2017
+ *  @author Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
+ */
+
+#ifndef LVR2_GEOMETRY_HALFEDGEFACE_H_
+#define LVR2_GEOMETRY_HALFEDGEFACE_H_
+
+
+namespace lvr2
+{
+
+// Forward definitions
+template <typename VectorT> struct HalfEdge;
+template <typename VectorT> struct HalfEdgeVertex;
+
+template <typename VectorT>
+struct HalfEdgeFace
+{
+    using Edge = HalfEdge<VectorT>;
+    using Face = HalfEdgeFace<VectorT>;
+    using Vertex = HalfEdgeVertex<VectorT>;
+
+    /// One of the edges bounding this face.
+    Edge* edge;
+
+    // Normal<VectorT> normal;
+};
+
+} // namespace lvr
+
+#endif /* LVR2_GEOMETRY_HALFEDGEFACE_H_ */
---
+++ include/lvr2/geometry/HalfEdgeMesh.hpp
@@ -0,0 +1,56 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+
+/*
+ * HalfEdgeMesh.hpp
+ *
+ *  @date 02.06.2017
+ *  @author Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
+ */
+
+#ifndef LVR2_GEOMETRY_HALFEDGEMESH_H_
+#define LVR2_GEOMETRY_HALFEDGEMESH_H_
+
+#include "lvr2/geometry/HalfEdge.hpp"
+#include "lvr2/geometry/HalfEdgeFace.hpp"
+#include "lvr2/geometry/HalfEdgeVertex.hpp"
+
+namespace lvr2
+{
+
+/**
+ * @brief
+ */
+template<typename VectorT>
+class HalfEdgeMesh
+{
+public:
+    using Edge = HalfEdge<VectorT>;
+    using Face = HalfEdgeFace<VectorT>;
+    using Vertex = HalfEdgeVertex<VectorT>;
+
+
+};
+
+} // namespace lvr
+
+
+#include "HalfEdgeMesh.tcc"
+
+#endif /* LVR2_GEOMETRY_HALFEDGEMESH_H_ */
