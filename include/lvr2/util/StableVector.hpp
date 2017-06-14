0e692bda4ca886f3abf5119fb7ff07df39d63a42
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-06-14 19:00:29 +0200

    Fix const-correctness bugs

--- include/lvr2/util/StableVector.hpp
+++ include/lvr2/util/StableVector.hpp
@@ -63,7 +63,7 @@ private:
     /**
      * @brief Check, if the requested handle is not deleted
      */
-    void checkAccess(const HandleType& handle);
+    void checkAccess(const HandleType& handle) const;
 
 public:
     StableVector() : m_usedCount(0) {};
