f317ce55cf8774489d2a1c270b57b9096db84118
Johan M. von Behren <johan@vonbehren.eu>
2017-07-21 16:28:52 +0200

    move `ClusterPainter::Rgb8Color` to `ColorAlgorithms.hpp`

--- include/lvr2/algorithm/ClusterPainter.hpp
+++ include/lvr2/algorithm/ClusterPainter.hpp
@@ -35,6 +35,7 @@ using std::array;
 #include <lvr2/geometry/ClusterSet.hpp>
 #include <lvr2/reconstruction/PointsetSurface.hpp>
 #include <lvr2/util/VectorMap.hpp>
+#include <lvr2/algorithm/ColorAlgorithms.hpp>
 
 namespace lvr2
 {
@@ -45,7 +46,6 @@ namespace lvr2
 class ClusterPainter
 {
 public:
-    using Rgb8Color = array<uint8_t, 3>;
     ClusterPainter(const ClusterSet<FaceHandle>& clusterSet) : m_clusterSet(clusterSet) {};
 
     template<typename BaseVecT>
