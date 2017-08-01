77bef7a13404426ff1085294739adbd8ae93317c
Alexander Altemöller <aaltemoeller@uni-osnabrueck.de>
2017-08-01 11:57:45 +0200

    Add comment to the changeMap-function.

--- include/lvr2/algorithm/MeshNavAlgorithms.hpp
+++ include/lvr2/algorithm/MeshNavAlgorithms.hpp
@@ -56,6 +56,21 @@ void calcVertexLocalNeighborhood(const BaseMesh<BaseVecT>& mesh, VertexHandle vH
 template<typename BaseVecT>
 DenseVertexMap<float> calcVertexHeightDiff(const BaseMesh<BaseVecT>& mesh, double radius);
 
+/**
+ * @brief   Change the given input VertexMap to a different type output DenseVertexMap.
+ *
+ * The conversion between the input VertexMap and output DenseVertexMap is defined by the given function.
+ * The method changeMap takes every element of the input VertexMap and converts it individually via the given
+ * map-conversion function and adds the converted element to the output DenseVertexMap.
+ *
+ * @tparam  in              Templatetype for the input VertexMap.
+ * @tparam  out             Templatetype for the output DenseVertexMap.
+ * @tparam  MapF            Templatetype for the given map-conversion function.
+ * @param   map_in          VertexMap holding input-type data, which will be converted to the DenseVertexMap holding output-type data.
+ * @param   map_function    Function for converting a single element of the input-type to a single element of the output-type.
+ *
+ * @return  A DenseVertexMap holding output-type data, which is created via the input VertexMap and the map-conversion function.
+ */
 template<typename in, typename out, typename MapF>
 DenseVertexMap<out> changeMap(const VertexMap<in>& map_in, MapF map_function);
 
