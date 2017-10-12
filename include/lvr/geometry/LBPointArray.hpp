c11b0358aa20eeaa6ffd17ea76a103282496b5ea
Alexander Mock <amock@uos.de>
2017-10-12 17:17:25 +0200

    optional checks

--- include/lvr/geometry/LBPointArray.hpp
+++ include/lvr/geometry/LBPointArray.hpp
@@ -45,18 +45,26 @@ template<typename T>
 static void splitPointArray(LBPointArray<T>& I, LBPointArray<T>& I_L, LBPointArray<T>& I_R);
 
 template<typename T, typename U>
-static bool checkSortedIndices(const LBPointArray<T>& V, const LBPointArray<U>& sorted_indices, unsigned int dim, int n=0);
+static bool checkSortedIndices(const LBPointArray<T>& V,
+        const LBPointArray<U>& sorted_indices,
+        unsigned int dim,
+        int n=0);
 
 
 template<typename T, typename U>
-static void splitPointArrayWithValue(const LBPointArray<T>& V, const LBPointArray<U>& I, LBPointArray<U>& I_L, LBPointArray<U>& I_R, int current_dim, T value,
-                T& deviation_left, T& deviation_right, const unsigned int& orig_dim,
-                const std::list<U>& critical_indices_left, const std::list<U>& critical_indices_right);
+static void splitPointArrayWithValue(const LBPointArray<T>& V,
+        const LBPointArray<U>& I, LBPointArray<U>& I_L, LBPointArray<U>& I_R,
+        int current_dim, T value,
+        T& deviation_left, T& deviation_right, const unsigned int& orig_dim,
+        const std::list<U>& critical_indices_left, const std::list<U>& critical_indices_right);
 
 template<typename T, typename U>
-static void splitPointArrayWithValueSet(const LBPointArray<T>& V, const LBPointArray<U>& I, LBPointArray<U>& I_L, LBPointArray<U>& I_R, int current_dim, T value,
-                T& deviation_left, T& deviation_right, const unsigned int& orig_dim,
-                const std::unordered_set<U>& critical_indices_left, const std::unordered_set<U>& critical_indices_right);
+static void splitPointArrayWithValueSet(const LBPointArray<T>& V,
+        const LBPointArray<U>& I, LBPointArray<U>& I_L, LBPointArray<U>& I_R,
+        int current_dim, T value,
+        T& deviation_left, T& deviation_right, const unsigned int& orig_dim,
+        const std::unordered_set<U>& critical_indices_left,
+        const std::unordered_set<U>& critical_indices_right);
 
 
 template<typename T>
@@ -67,16 +75,18 @@ static unsigned int checkNumberOfSmallerEqualValues(LBPointArray<float>& V, unsi
 
 // SORT FUNCTIONS THREADED
 template<typename T, typename U>
-static void mergeHostWithIndices(T* a, U* b, unsigned int i1, unsigned int j1, unsigned int i2, unsigned int j2, int limit);
+static void mergeHostWithIndices(T* a, U* b, unsigned int i1, unsigned int j1,
+        unsigned int i2, unsigned int j2, int limit);
 
 template<typename T, typename U>
-static void naturalMergeSort(LBPointArray<T>& in, int dim, LBPointArray<U>& indices, LBPointArray<T>& m, int limit=-1) ;
+static void naturalMergeSort(LBPointArray<T>& in, int dim, LBPointArray<U>& indices, LBPointArray<T>& m, int limit=-1);
 
 template<typename T, typename U>
 static void sortByDim(LBPointArray<T>& V, int dim, LBPointArray<U>& indices, LBPointArray<T>& values);
 
 template<typename T, typename U>
-static void generateAndSort(int id, LBPointArray<T>& vertices, LBPointArray<U>* indices_sorted, LBPointArray<T>* values_sorted, int dim);
+static void generateAndSort(int id, LBPointArray<T>& vertices, LBPointArray<U>* indices_sorted,
+        LBPointArray<T>* values_sorted, int dim);
 
 } /* namespace lvr */
 
