4e53c729436b2a92ef6d2cd3baea88543f7e9ca3
imitschke <imitschke@uos.de>
2017-10-19 00:01:23 +0200

    added time measurements

--- src/tools/mpi/bigGridTest.cpp
+++ src/tools/mpi/bigGridTest.cpp
@@ -76,14 +76,17 @@ int main(int argc, char** argv)
 
 
     LargeScaleOptions::Options options(argc, argv);
-
+    double seconds=0;
     string filePath = options.getInputFileName();
     float voxelsize = options.getVoxelsize();
     float scale = options.getScaling();
     std::vector<float> flipPoint = options.getFlippoint();
     cout << lvr::timestamp << "Starting grid" << endl;
+    double start_ss = lvr::timestamp.getElapsedTimeInS();
     BigGrid bg(filePath, voxelsize, scale);
-    cout << lvr::timestamp << "grid finished " << endl;
+    double end_ss = lvr::timestamp.getElapsedTimeInS();
+    seconds+=(end_ss - start_ss);
+    cout << lvr::timestamp << "grid finished in" << (end_ss - start_ss) << "sec." << endl;
     lvr::BoundingBox<lvr::Vertexf> bb = bg.getBB();
     cout << bb << endl;
 
@@ -162,9 +165,13 @@ int main(int argc, char** argv)
         size_t numPoints;
 
         //todo: okay?
+        cout << lvr::timestamp << "loading data " << i  << endl;
+        double start_s = lvr::timestamp.getElapsedTimeInS();
         lvr::floatArr points = bg.points(partitionBoxes[i].getMin().x - voxelsize*3, partitionBoxes[i].getMin().y - voxelsize*3, partitionBoxes[i].getMin().z - voxelsize*3 ,
                                          partitionBoxes[i].getMax().x + voxelsize*3, partitionBoxes[i].getMax().y + voxelsize*3, partitionBoxes[i].getMax().z + voxelsize*3,numPoints);
-
+        double end_s = lvr::timestamp.getElapsedTimeInS();
+        seconds+=(end_s - start_s);
+        cout << lvr::timestamp << "finished loading data " << i  << " in " << (end_s - start_s)<< endl;
         //std::cout << "i: " << std::endl << bb << std::endl << "got : " << numPoints << std::endl;
         if(numPoints<=50) continue;
 
@@ -249,28 +256,30 @@ int main(int argc, char** argv)
 
         reconstruction = new FastReconstruction<ColorVertex<float, unsigned char> , Normal<float>, FastBox<ColorVertex<float, unsigned char>, Normal<float> >  >(ps_grid);
         HalfEdgeMesh<ColorVertex<float, unsigned char> , Normal<float> > mesh;
-
+        cout << lvr::timestamp << " saving data " << i << endl;
         vector<unsigned int> duplicates;
         reconstruction->getMesh(mesh, ps_grid->qp_bb, duplicates, voxelsize*5);
         mesh.finalize();
-
+        start_s = lvr::timestamp.getElapsedTimeInS();
         std::stringstream ss_mesh;
         ss_mesh << "part-" << i << "-mesh.ply";
         ModelPtr m( new Model( mesh.meshBuffer() ) );
         ModelFactory::saveModel( m, ss_mesh.str());
         delete reconstruction;
 
-        std::stringstream ss_grid;
-        ss_grid << "part-" << i << "-grid.ser";
-        ps_grid->saveCells(ss_grid.str());
-        grid_files.push_back(ss_grid.str());
+//        std::stringstream ss_grid;
+//        ss_grid << "part-" << i << "-grid.ser";
+//        ps_grid->saveCells(ss_grid.str());
+//        grid_files.push_back(ss_grid.str());
 
         std::stringstream ss_duplicates;
         ss_duplicates << "part-" << i << "-duplicates.ser";
         std::ofstream ofs(ss_duplicates.str(), std::ofstream::out | std::ofstream::trunc);
         boost::archive::text_oarchive oa(ofs);
         oa & duplicates;
-
+        end_s = lvr::timestamp.getElapsedTimeInS();
+        seconds+=(end_s - start_s);
+        cout << lvr::timestamp << "finished saving data " << i << " in " << (end_s - start_s) << endl;
         delete ps_grid;
 
 
@@ -286,6 +295,7 @@ int main(int argc, char** argv)
     {
         string duplicate_path = grid_files[i];
         string ply_path = grid_files[i];
+        double start_s = lvr::timestamp.getElapsedTimeInS();
         boost::algorithm::replace_last(duplicate_path, "-grid.ser", "-duplicates.ser");
         boost::algorithm::replace_last(ply_path, "-grid.ser", "-mesh.ply");
         std::ifstream ifs(duplicate_path);
@@ -293,6 +303,7 @@ int main(int argc, char** argv)
         std::vector<unsigned int> duplicates;
         ia & duplicates;
         LineReader lr(ply_path);
+
         size_t numPoints = lr.getNumPoints();
         offsets.push_back(numPoints+offsets[i]);
         if(numPoints==0) continue;
@@ -301,7 +312,8 @@ int main(int argc, char** argv)
         //ModelPtr modelPtr = ModelFactory::readModel(ply_path);
         size_t numVertices;
         floatArr modelVertices = modelPtr->m_mesh->getVertexArray(numVertices);
-
+        double end_s = lvr::timestamp.getElapsedTimeInS();
+        seconds+=(end_s - start_s);
 //        for (size_t j = 0; j < numVertices; j++)
 //        {
 //            duplicates.push_back(static_cast<unsigned int>(j));
@@ -320,8 +332,8 @@ int main(int argc, char** argv)
     }
     std::unordered_map<unsigned int, unsigned int> oldToNew;
     double dist_epsilon_squared = (voxelsize/10000)*(voxelsize/10000);
-    ofstream ofsd;
-    ofsd.open("duplicate_colors.pts", ios_base::app);
+//    ofstream ofsd;
+//    ofsd.open("duplicate_colors.pts", ios_base::app);
 
 //    flann::Matrix<float> flannPoints = flann::Matrix<float> (new float[3 * duplicateVertices.size()], duplicateVertices.size(), 3);
 //    for(size_t i = 0; i < duplicateVertices.size(); i++)
@@ -334,27 +346,32 @@ int main(int argc, char** argv)
 //            boost::shared_ptr<flann::Index<flann::L2_Simple<float> > >(new flann::Index<flann::L2_Simple<float> >(flannPoints, ::flann::KDTreeSingleIndexParams (10, false)));
 //    tree->buildIndex();
     cout << lvr::timestamp << "removing duplicate vertices" << endl;
-    SearchTreeFlann<Vertexf> searchTree(duplicateVertices);
-    float comp_dist = voxelsize/100;
-    for(size_t i = 0 ; i < duplicateVertices.size() ; i+=3)
+    if(duplicateVertices.size()>0)
     {
-        vector<size_t> indices;
-        vector<float> distances;
-        searchTree.kSearch(duplicateVertices[i], duplicateVertices[i*3+1], duplicateVertices[i*3+2],20, indices, distances);
-        for(int j = 0 ; j <distances.size();j++)
+        SearchTreeFlann<Vertexf> searchTree(duplicateVertices);
+        float comp_dist = std::max(voxelsize/10000, 0.00001f);
+        for(size_t i = 0 ; i < duplicateVertices.size() ; i+=3)
         {
-            if(distances[j] < comp_dist && all_duplicates[i/3] != all_duplicates[indices[j]])
+            vector<size_t> indices;
+            vector<float> distances;
+            searchTree.kSearch(duplicateVertices[i], duplicateVertices[i+1], duplicateVertices[i+2],5, indices, distances);
+            for(int j = 0 ; j <distances.size();j++)
             {
-                auto find_it = oldToNew.find(all_duplicates[i/3]);
-                if( find_it == oldToNew.end())
+                if(distances[j] < comp_dist && all_duplicates[i/3] != all_duplicates[indices[j]])
                 {
-                    oldToNew[all_duplicates[indices[j]]] = all_duplicates[i/3];
+                    if(all_duplicates[i/3] == all_duplicates[indices[j]]) continue;
+                    auto find_it = oldToNew.find(all_duplicates[i/3]);
+                    if( find_it == oldToNew.end())
+                    {
+                        oldToNew[all_duplicates[indices[j]]] = all_duplicates[i/3];
 //                    cout << "dup found! mapping " << all_duplicates[j/3] << " -> " << all_duplicates[i/3] << " dist: " << sqrt(dist_squared)<< endl;
 
+                    }
                 }
             }
         }
     }
+
 
 //    for(size_t i = 0 ; i < duplicateVertices.size() ; i+=3)
 //    {
@@ -412,6 +429,7 @@ int main(int argc, char** argv)
     cout << lvr::timestamp << "merging mesh..." << endl;
     for(size_t i = 0 ; i <grid_files.size() ; i++)
     {
+        double start_s = lvr::timestamp.getElapsedTimeInS();
 
         string ply_path = grid_files[i];
         boost::algorithm::replace_last(ply_path, "-grid.ser", "-mesh.ply");
@@ -425,6 +443,8 @@ int main(int argc, char** argv)
         size_t offset = offsets[i];
         floatArr modelVertices = modelPtr->m_mesh->getVertexArray(numVertices);
         uintArr modelFaces = modelPtr->m_mesh->getFaceArray(numFaces);
+        double end_s = lvr::timestamp.getElapsedTimeInS();
+        seconds+=(end_s - start_s);
         newNumFaces+=numFaces;
         for(size_t j = 0; j<numVertices ; j++)
         {
@@ -435,7 +455,10 @@ int main(int argc, char** argv)
             if(oldToNew.find(j+offset)==oldToNew.end())
             {
 //                ofs_vertices.write((char*)p,sizeof(float)*3);
+                start_s = lvr::timestamp.getElapsedTimeInS();
                 ofs_vertices << std::setprecision(16) << p[0] << " " << p[1] << " " << p[2] << endl;
+                end_s = lvr::timestamp.getElapsedTimeInS();
+                seconds+=(end_s - start_s);
                 newNumVertices++;
             }
             else
@@ -471,11 +494,17 @@ int main(int argc, char** argv)
                     face_idx = oldToNew[f[k]]- decr_itr->second;
 //                    
                 }
+                start_s = lvr::timestamp.getElapsedTimeInS();
                 ofs_faces << face_idx;
+                end_s = lvr::timestamp.getElapsedTimeInS();
+                seconds+=(end_s - start_s);
                 if(k!=2) ofs_faces << " ";
 
             }
+            start_s = lvr::timestamp.getElapsedTimeInS();
             ofs_faces << endl;
+            end_s = lvr::timestamp.getElapsedTimeInS();
+            seconds+=(end_s - start_s);
 //            ofs_faces.write( (char*) newface,sizeof(unsigned int)*3);
             // todo: sort
 
@@ -488,6 +517,7 @@ int main(int argc, char** argv)
     }
     cout << lvr::timestamp << "saving ply" << endl;
     cout << "Largest decrement: " << increment << endl;
+    double start_s = lvr::timestamp.getElapsedTimeInS();
 
     ofstream ofs_ply("bigMesh.ply", std::ofstream::out | std::ofstream::trunc);
     ifstream ifs_faces("largeFaces.bin");
@@ -524,14 +554,31 @@ int main(int argc, char** argv)
     {
         ofs_ply << line << endl;
     }
+    size_t c = 0;
     while(std::getline(ifs_faces,line))
     {
+
         ofs_ply << line << endl;
+        stringstream ss(line);
+        unsigned int v[3];
+        ss >> v[0];
+        ss >> v[1];
+        ss >> v[2];
+        for(int i = 0 ; i< 3; i++)
+        {
+            if(v[i]>=newNumVertices)
+            {
+                cout << "WTF: FACE " << c << " has index " << v[i] << endl;
+            }
+
+        }
+        c++;
     }
+    double end_s = lvr::timestamp.getElapsedTimeInS();
+    seconds+=(end_s - start_s);
 
 
-
-
+    cout << "IO-TIME: " << seconds << " seconds" << endl;
     cout << lvr::timestamp << "finished" << endl;
 
 //
