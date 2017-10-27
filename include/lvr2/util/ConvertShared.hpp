8455abe1f5ed760c0bdb78dc60c48e172a2cea2f
Sebastian Pütz <spuetz@uos.de>
2017-10-27 18:33:46 +0200

    ConvertShared header

---
+++ include/lvr2/util/ConvertShared.hpp
@@ -0,0 +1,61 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+/*
+ * ConvertShared.hpp
+ */
+
+#ifndef LVR2_UTIL_CONVERT_SHARED_H_
+#define LVR2_UTIL_CONVERT_SHARED_H_
+
+#include <memory>
+#include <boost/shared_ptr.hpp>
+namespace lvr2
+{
+
+namespace {
+    template<class SharedPointer> struct Holder {
+        SharedPointer p;
+
+        Holder(const SharedPointer &p) : p(p) {}
+        Holder(const Holder &other) : p(other.p) {}
+        Holder(Holder &&other) : p(std::move(other.p)) {}
+
+        void operator () (...) { p.reset(); }
+    };
+}
+
+template<class T> std::shared_ptr<T> to_std_ptr(const boost::shared_ptr<T> &p) {
+    typedef Holder<std::shared_ptr<T>> H;
+    if(H *h = boost::get_deleter<H, T>(p)) {
+        return h->p;
+    } else {
+        return std::shared_ptr<T>(p.get(), Holder<boost::shared_ptr<T>>(p));
+    }
+}
+
+template<class T> boost::shared_ptr<T> to_boost_ptr(const std::shared_ptr<T> &p){
+    typedef Holder<boost::shared_ptr<T>> H;
+    if(H * h = std::get_deleter<H, T>(p)) {
+        return h->p;
+    } else {
+        return boost::shared_ptr<T>(p.get(), Holder<std::shared_ptr<T>>(p));
+    }
+}
+}
+#endif /* LVR2_UTIL_CONVERT_SHARED_H_ */
