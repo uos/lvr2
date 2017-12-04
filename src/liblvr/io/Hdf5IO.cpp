89885d3075500e1a10807aadfb5df9f040c6bf98
Christian Swan <cswan@uos.de>
2017-12-04 14:35:10 +0100

    add saving of vertex colors in Hdf5IO

--- src/liblvr/io/Hdf5IO.cpp
+++ src/liblvr/io/Hdf5IO.cpp
@@ -33,25 +33,30 @@ void Hdf5IO::save(string filename)
     size_t numVertices = 0;
     size_t numFaceIds = 0;
     size_t numNormals = 0;
+    size_t numColors = 0;
     floatArr vertices;
     uintArr faceIndices;
     floatArr normals;
+    ucharArr colors;
 
     // TODO: check whether or not we have a mesh...
     vertices = m_model->m_mesh->getVertexArray(numVertices);
     faceIndices = m_model->m_mesh->getFaceArray(numFaceIds);
     normals = m_model->m_mesh->getVertexNormalArray(numNormals);
+    colors = m_model->m_mesh->getVertexColorArray(numColors);
 
     cout << "num vertices: " << numVertices << endl;
 
     auto verts = std::vector<float>(vertices.get(), vertices.get() + numVertices * 3);
     auto indis = std::vector<uint32_t>(faceIndices.get(), faceIndices.get() + numFaceIds * 3);
     auto normalsVector = std::vector<float>(normals.get(), normals.get() + numNormals * 3);
+    auto colorsVector = std::vector<uint8_t>(colors.get(), colors.get() + numColors * 3);
 
     cout << "verts size: " << verts.size() << endl;
 
     lvr2::PlutoMapIO pm(filename, verts, indis);
     pm.addNormals(normalsVector);
+    pm.addVertexColors(colorsVector);
 }
 
 
