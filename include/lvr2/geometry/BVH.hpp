3e139d8063c031d5a9fa87f2ba7e6ac74a3d3fce
Johan M. von Behren <johan@vonbehren.eu>
2018-01-23 21:42:26 +0100

    move structs into class to narrow scope

--- include/lvr2/geometry/BVH.hpp
+++ include/lvr2/geometry/BVH.hpp
@@ -37,62 +37,6 @@ using std::pair;
 namespace lvr2
 {
 
-// TODO: move into BVHTree -> private
-template<typename BaseVecT>
-struct Triangle {
-
-    Triangle();
-
-    // indices in vertex array
-    uint32_t idx1;
-    uint32_t idx2;
-    uint32_t idx3;
-
-    Point<BaseVecT> center;
-    Normal<BaseVecT> normal;
-
-    // intersection pre-computed cache
-    float d, d1, d2, d3;
-    Normal<BaseVecT> e1, e2, e3;
-
-    // todo: not used?
-    BoundingBox<BaseVecT> bb;
-};
-
-template<typename BaseVecT>
-struct AABB {
-    BoundingBox<BaseVecT> bb;
-    vector<size_t> triangles;
-};
-
-template<typename BaseVecT>
-struct BVHNode {
-    BoundingBox<BaseVecT> bb;
-    virtual bool isLeaf() = 0;
-};
-
-template<typename BaseVecT>
-using BVHNodePtr = unique_ptr<BVHNode<BaseVecT>>;
-
-template<typename BaseVecT>
-struct BVHInner: BVHNode<BaseVecT> {
-    unique_ptr<BVHNode<BaseVecT>> left;
-    unique_ptr<BVHNode<BaseVecT>> right;
-    virtual bool isLeaf() { return false; }
-};
-
-template<typename BaseVecT>
-using BVHInnerPtr = unique_ptr<BVHInner<BaseVecT>>;
-
-template<typename BaseVecT>
-struct BVHLeaf: BVHNode<BaseVecT> {
-    vector<size_t> triangles;
-    virtual bool isLeaf() { return true; }
-};
-
-template<typename BaseVecT>
-using BVHLeafPtr = unique_ptr<BVHLeaf<BaseVecT>>;
-
 template<typename BaseVecT>
 class BVHTree
 {
@@ -105,11 +49,55 @@ public:
     const vector<float>& getTrianglesIntersectionData() const;
 
 private:
-    using AABB_t = AABB<BaseVecT>;
+
+    struct Triangle {
+
+        Triangle();
+
+        // indices in vertex array
+        // todo: not used?
+        uint32_t idx1;
+        uint32_t idx2;
+        uint32_t idx3;
+
+        Point<BaseVecT> center;
+        Normal<BaseVecT> normal;
+
+        // intersection pre-computed cache
+        float d, d1, d2, d3;
+        Normal<BaseVecT> e1, e2, e3;
+
+        // todo: not used?
+        BoundingBox<BaseVecT> bb;
+    };
+
+    struct AABB {
+        BoundingBox<BaseVecT> bb;
+        vector<size_t> triangles;
+    };
+
+    struct BVHNode {
+        BoundingBox<BaseVecT> bb;
+        virtual bool isLeaf() = 0;
+    };
+    using BVHNodePtr = unique_ptr<BVHNode>;
+
+    struct BVHInner: BVHNode {
+        BVHNodePtr left;
+        BVHNodePtr right;
+        virtual bool isLeaf() { return false; }
+    };
+    using BVHInnerPtr = unique_ptr<BVHInner>;
+
+    struct BVHLeaf: BVHNode {
+        vector<size_t> triangles;
+        virtual bool isLeaf() { return true; }
+    };
+    using BVHLeafPtr = unique_ptr<BVHLeaf>;
 
     // working variables for tree construction
-    BVHNodePtr<BaseVecT> m_root;
-    vector<Triangle<BaseVecT>> m_triangles;
+    BVHNodePtr m_root;
+    vector<Triangle> m_triangles;
 
     // cache friendly data for the SIMD device
     vector<uint32_t> m_triIndexList;
@@ -117,11 +105,11 @@ private:
     vector<uint32_t> m_indexesOrTrilists;
     vector<float> m_trianglesIntersectionData;
 
-    BVHNodePtr<BaseVecT> buildTree(const vector<float>& vertices, const vector<uint32_t>& faces);
-    BVHNodePtr<BaseVecT> buildTreeRecursive(vector<AABB_t>& work, uint32_t depth = 0);
+    BVHNodePtr buildTree(const vector<float>& vertices, const vector<uint32_t>& faces);
+    BVHNodePtr buildTreeRecursive(vector<AABB>& work, uint32_t depth = 0);
 
     void createCFTree();
-    void createCFTreeRecursive(BVHNodePtr<BaseVecT> currentNode, uint32_t& idxBoxes);
+    void createCFTreeRecursive(BVHNodePtr currentNode, uint32_t& idxBoxes);
 
     void convertTrianglesIntersectionData();
 };
