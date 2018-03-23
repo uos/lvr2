5dc1a57b7710a2367dbf29407a986a3821cb0c37
Christian Swan <cswan@uos.de>
2018-03-23 13:14:24 +0100

    fix whitespaces in doc

--- include/lvr/io/Hdf5IO.hpp
+++ include/lvr/io/Hdf5IO.hpp
@@ -48,7 +48,7 @@ namespace lvr
  *
  * This also tries to mitgate any erros while saving the file to disc, since
  * the HDF5 implementation is very picky about saving anything to an already opened
- * file or any other strange things on the file system. Thus if the first try of 
+ * file or any other strange things on the file system. Thus if the first try of
  * saving it the defined filename it tries to make up an new one and saves everything there.
  */
 class Hdf5IO : public BaseIO
