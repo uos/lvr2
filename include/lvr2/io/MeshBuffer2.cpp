32f85676e88e137cfdd386b65bb8684981e87e13
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2018-08-01 11:00:26 +0200

    Basic structure for MeshBuffer2, adding header documentation

--- include/lvr2/io/MeshBuffer2.cpp
+++ include/lvr2/io/MeshBuffer2.cpp
@@ -4,3 +4,83 @@ MeshBuffer2::MeshBuffer2()
 {
 
 }
+
+void MeshBuffer2::addVertices(floatArr vertices, size_t n)
+{
+
+}
+
+void MeshBuffer2::addVertexNormals(floatArr normals)
+{
+
+}
+
+void MeshBuffer2::addVertexColors(ucharArr colors, unsigned w)
+{
+
+}
+
+void MeshBuffer2::addTextureCoordinates(floatArr coordinates)
+{
+
+}
+
+void MeshBuffer2::addFaceIndices(indexArray indices, size_t n)
+{
+
+}
+
+void MeshBuffer2::addFaceMaterialIndices(indexArray indices)
+{
+
+}
+
+void MeshBuffer2::addFaceNormals(floatArr normals)
+{
+
+}
+
+void MeshBuffer2::addFaceColors(ucharArr colors, unsigned w)
+{
+
+}
+
+size_t MeshBuffer2::numVertices()
+{
+
+}
+
+size_t MeshBuffer2::numFaces()
+{
+
+}
+
+floatArr MeshBuffer2::getVertices()
+{
+
+}
+
+ucharArr MeshBuffer2::getVertexColors(unsigned& w)
+{
+
+}
+
+floatArr MeshBuffer2::getVertexNormals()
+{
+
+}
+
+floatArr MeshBuffer2::getTextureCoordinates()
+{
+
+}
+
+indexArray MeshBuffer2::getFaceIndices()
+{
+
+}
+
+ucharArr MeshBuffer2::getFaceColors(unsigned& w)
+{
+
+}
