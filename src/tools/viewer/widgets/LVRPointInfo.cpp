98070b73464bc8001c9b3ef43a85a7f768df46ef
mluetkedreim <mluetkedreim@uos.de>
2018-08-01 14:29:25 +0200

    rework LVRPointInfo

--- src/tools/viewer/widgets/LVRPointInfo.cpp
+++ src/tools/viewer/widgets/LVRPointInfo.cpp
@@ -16,17 +16,15 @@
 namespace lvr
 {
 
-LVRPointInfo::LVRPointInfo(QTreeWidget* treeWidget)
+LVRPointInfo::LVRPointInfo(QWidget* parent, PointBufferPtr points, int pointId) :
+    QDialog(parent), m_points(points), m_pointId(pointId)
 {
-    // Setup DialogUI and events
-    m_dialog = new QDialog(treeWidget);
-    m_pointInfo = new PointInfo;
-    m_pointInfo->setupUi(m_dialog);
+    setAttribute(Qt::WA_DeleteOnClose);
 
-    m_plotter = new LVRPlotter(m_dialog, true);
-    m_pointInfo->gridLayout->addWidget(m_plotter, 4, 0, 1, 1);
+    m_pointInfo.setupUi(this);
 
-    QObject::connect(m_pointInfo->shouldScale, SIGNAL(stateChanged(int)), this, SLOT(refresh(int)));
+    QObject::connect(m_pointInfo.shouldScale, SIGNAL(stateChanged(int)), this, SLOT(refresh()));
+    refresh();
 }
 
 LVRPointInfo::~LVRPointInfo()
@@ -34,71 +32,49 @@ LVRPointInfo::~LVRPointInfo()
     // TODO Auto-generated destructor stub
 }
 
-void LVRPointInfo::setPointBuffer(PointBufferPtr points)
+void LVRPointInfo::refresh()
 {
-    m_points = points;
-}
-
-int LVRPointInfo::getPoint() const
-{
-    return m_pointId;
-}
-
-bool LVRPointInfo::isVisible() const
-{
-    return m_dialog->isVisible();
-}
-
-void LVRPointInfo::setPoint(int pointId)
-{
-    m_pointId = pointId;
-
     size_t n;
     floatArr points = m_points->getPointArray(n);
 
-    if (pointId < 0 || pointId >= n)
+    if (m_pointId < 0 || m_pointId >= n)
     {
-        m_dialog->done(0);
+        done(0);
         return;
     }
 
-    m_pointInfo->infoText->setText(QString("Selected: %1, %2, %3")
-        .arg(points[pointId * 3], 10, 'g', 4)
-        .arg(points[pointId * 3 + 1], 10, 'g', 4)
-        .arg(points[pointId * 3 + 2], 10, 'g', 4));
+    m_pointInfo.infoText->setText(QString("Selected: %1, %2, %3")
+        .arg(points[m_pointId * 3], 10, 'g', 4)
+        .arg(points[m_pointId * 3 + 1], 10, 'g', 4)
+        .arg(points[m_pointId * 3 + 2], 10, 'g', 4));
     
     size_t n_spec, n_channels;
     floatArr spec = m_points->getPointSpectralChannelsArray(n_spec, n_channels);
     
-    if (pointId >= n_spec)
+    if (m_pointId >= n_spec)
     {
-        m_plotter->removePoints();
+        m_pointInfo.plotter->removePoints();
     }
     else
     {
         floatArr data = floatArr(new float[n_channels]);
         for (int i = 0; i < n_channels; i++)
         {
-            data[i] = spec[pointId * n_channels + i];
+            data[i] = spec[m_pointId * n_channels + i];
         }
-        if (m_pointInfo->shouldScale->isChecked())
+        if (m_pointInfo.shouldScale->isChecked())
         {
-            m_plotter->setPoints(data, n_channels);
+            m_pointInfo.plotter->setPoints(data, n_channels);
         }
         else
         {
-            m_plotter->setPoints(data, n_channels, 0, 1);
+            m_pointInfo.plotter->setPoints(data, n_channels, 0, 1);
         }
     }
 
-    m_dialog->show();
-    m_dialog->raise();
-    m_dialog->activateWindow();
-}
-
-void LVRPointInfo::refresh(int)
-{
-    setPoint(m_pointId);
+    show();
+    raise();
+    activateWindow();
 }
 
 }
