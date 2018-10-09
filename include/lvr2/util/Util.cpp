8d5358c5f072cd4bdf7197c8cbd1b7d3a37fad69
aloehr <aloehr@uos.de>
2018-10-09 17:04:24 +0200

    more refactoring

--- include/lvr2/util/Util.cpp
+++ include/lvr2/util/Util.cpp
@@ -8,17 +8,14 @@ namespace lvr2
 int Util::getSpectralChannel(int wavelength, PointBuffer2Ptr pcloud, int fallback)
 {
     FloatChannelOptional spectral_channels = pcloud->getFloatChannel("spectral_channels");
-
     if (!spectral_channels)
     {
         return fallback;
     }
 
     int minWavelength = *pcloud->getIntAttribute("spectral_wavelength_min");
-    int maxWavelength = *pcloud->getIntAttribute("spectral_wavelength_max");
 
-    float wavelengthPerChannel = (maxWavelength - minWavelength) / static_cast<float>(spectral_channels->width());
-    int channel = (wavelength - minWavelength) / wavelengthPerChannel;
+    int channel = (wavelength - minWavelength) / wavelengthPerChannel(pcloud);
 
     if (channel < 0 || channel >= spectral_channels->width())
     {
@@ -26,6 +23,38 @@ int Util::getSpectralChannel(int wavelength, PointBuffer2Ptr pcloud, int fallbac
     }
 
     return channel;
+}
+
+int Util::getSpectralWavelength(int channel, PointBuffer2Ptr pcloud, int fallback)
+{
+    FloatChannelOptional spectral_channels = pcloud->getFloatChannel("spectral_channels");
+    if (!spectral_channels)
+    {
+        return fallback;
+    }
+    
+    int minWavelength = *pcloud->getIntAttribute("spectral_wavelength_min");
+
+    if (channel < 0 || channel >= spectral_channels->width())
+    {
+        return fallback;
+    }
+
+    return channel * wavelengthPerChannel(pcloud) + minWavelength;
+}
+
+float Util::wavelengthPerChannel(PointBuffer2Ptr pcloud)
+{
+    FloatChannelOptional spectral_channels = pcloud->getFloatChannel("spectral_channels");
+    if (!spectral_channels)
+    {
+        return -1.0f;
+    }
+
+    int minWavelength = *pcloud->getIntAttribute("spectral_wavelength_min");
+    int maxWavelength = *pcloud->getIntAttribute("spectral_wavelength_max");
+
+    return (maxWavelength - minWavelength) / static_cast<float>(spectral_channels->width());
 }
 
 } // namespace lvr2
