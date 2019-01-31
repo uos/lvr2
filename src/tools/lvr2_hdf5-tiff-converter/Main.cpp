8f60bc9a939a4aadcb9849d9e4a53fa01f6088fd
ndettmer <ndettmer@uni-osnabrueck.de>
2019-01-31 17:39:03 +0100

    able to read spectral dimensions

--- src/tools/lvr2_hdf5-tiff-converter/Main.cpp
+++ src/tools/lvr2_hdf5-tiff-converter/Main.cpp
@@ -1,11 +1,12 @@
 //
-// Created by ndettmer on 11.12.18.
+// Created by ndettmer on 31.01.19.
 //
 
 #include <lvr2/io/HDF5IO.hpp>
 #include <lvr2/io/ScanDataManager.hpp>
 
 #include <boost/range/iterator_range.hpp>
+#include <boost/foreach.hpp>
 
 #include <string>
 #include <fstream>
@@ -29,17 +30,18 @@ int main()
     vector<ScanData> scandatas;
 /*    vector<cv::Mat2d> channels;*/
 
-    ScanDataManager scanman(filename);
-    HDF5IO hdf5(filename);
-
-/*    scandatas = scanman.getScanData("/raw/scans");*/
+    HDF5IO hdf5(filename, false);
 
     std::vector<size_t> dim;
-    string groupname = "annotation/position_00001";
+
+    string groupname = "annotation/position_00010";
     string datasetname = "spectral";
     boost::shared_array<unsigned int> spectrals = hdf5.getArray<unsigned int>(groupname, datasetname, dim);
 
+    for(size_t size: dim)
+    {
+        cout << size << endl;
+    }
 
     return 0;
 }
-
