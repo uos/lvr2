3d0035c3ff15bae892c0aeb000504deb1748fc56
ndettmer <ndettmer@uni-osnabrueck.de>
2019-02-05 16:02:06 +0100

    TIFFIO, Problem: conversion from cv::Mat.data to unsigned int*

---
+++ src/tools/lvr2_hdf5-tiff-converter/TIFFIO.hpp
@@ -0,0 +1,39 @@
+//
+// Created by hitech-gandalf on 05.02.19.
+//
+
+#ifndef TIFFIO_HPP
+#define TIFFIO_HPP
+
+#include <tiffio.h>
+#include <opencv2/core/core.hpp>
+#include <string>
+
+namespace lvr2
+{
+
+/**
+ * @brief wrapper class for TIFFIO functions
+ * @author ndettmer <ndettmer@uos.de>
+ */
+    class TIFFIO {
+    public:
+        TIFFIO(std::string filename);
+
+        ~TIFFIO();
+
+        /**
+         * @brief write the next scanline to the tiff file
+         * @return true if no errors occurred
+         */
+        int writeLevel(cv::Mat *mat);
+
+        int setFields(cv::Mat *mat, int sampleperpixel);
+
+    private:
+        TIFF* m_tiffile;
+    };
+
+}
+
+#endif //TIFFIO_HPP
