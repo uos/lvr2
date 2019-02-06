be51e4d6c4ff83c9b12e932d14ab5657f944f2ea
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2019-02-07 08:58:52 +1300

    Hopefully fixed line endings now

--- CMakeModules/FindLZ4.cmake
+++ CMakeModules/FindLZ4.cmake
@@ -1,39 +1,81 @@
-# Finds liblz4.
+# Copyright 2012 Cloudera Inc.
 #
-# This module defines:
-# LZ4_FOUND
-# LZ4_INCLUDE_DIR
-# LZ4_LIBRARY
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-find_path(LZ4_INCLUDE_DIR NAMES lz4.h)
-find_library(LZ4_LIBRARY NAMES lz4)
+# - Find LZ4 (lz4.h, liblz4.a, liblz4.so, and liblz4.so.1)
+# This module defines
+#  LZ4_INCLUDE_DIR, directory containing headers
+#  LZ4_LIBS, directory containing lz4 libraries
+#  LZ4_STATIC_LIB, path to liblz4.a
+#  LZ4_LIBRARY, path to liblz4.so
+#  LZ4_FOUND, whether lz4 has been found
 
-# We require LZ4_compress_default() which was added in v1.7.0
-if (LZ4_LIBRARY)
-  include(CheckCSourceRuns)
-  set(CMAKE_REQUIRED_INCLUDES ${LZ4_INCLUDE_DIR})
-  set(CMAKE_REQUIRED_LIBRARIES ${LZ4_LIBRARY})
-  check_c_source_runs("
-#include <lz4.h>
-int main() {
-  int good = (LZ4_VERSION_MAJOR > 1) ||
-    ((LZ4_VERSION_MAJOR == 1) && (LZ4_VERSION_MINOR >= 7));
-return !good;
-}" LZ4_GOOD_VERSION)
-  set(CMAKE_REQUIRED_INCLUDES)
-  set(CMAKE_REQUIRED_LIBRARIES)
-endif()
+set(LZ4_SEARCH_HEADER_PATHS
+  ${THIRDPARTY_PREFIX}/include
+  /usr/include
+  /usr/local/include
+)
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    LZ4 DEFAULT_MSG
-    LZ4_LIBRARY LZ4_INCLUDE_DIR LZ4_GOOD_VERSION)
+set(LZ4_SEARCH_LIB_PATH
+  ${THIRDPARTY_PREFIX}/lib
+  /usr/local/lib
+  /usr/lib/
+  /usr/lib/x86_64-linux-gnu/
+)
 
-if (NOT LZ4_FOUND)
-  message(STATUS "Using third-party bundled LZ4")
-else()
-  message(STATUS "Found LZ4: ${LZ4_LIBRARY}")
-endif (NOT LZ4_FOUND)
+find_path(LZ4_INCLUDE_DIR lz4.h PATHS
+  ${LZ4_SEARCH_HEADER_PATHS}
+  # make sure we don't accidentally pick up a different version
+  NO_DEFAULT_PATH
+)
 
-mark_as_advanced(LZ4_INCLUDE_DIR LZ4_LIBRARY)
+find_library(LZ4_LIB_PATH NAMES liblz4.so liblz4.dylib PATHS ${LZ4_SEARCH_LIB_PATH} NO_DEFAULT_PATH)
+
+message(STATUS "Header ${LZ4_INCLUDE_DIR} LIB ")
+if (LZ4_INCLUDE_DIR AND LZ4_LIB_PATH)
+  set(LZ4_FOUND TRUE)
+  set(LZ4_LIBS ${LZ4_SEARCH_LIB_PATH})
+  set(LZ4_STATIC_LIB ${LZ4__LIB_PATH}/liblz4.a)
+
+  # mac os is using dylib instead of so for lz4
+  if (APPLE)
+    set(LZ4_LIBRARY liblz4.dylib)
+  else ()
+    set(LZ4_LIBRARY liblz4.so)
+  endif ()
+else ()
+  set(LZ4_FOUND FALSE)
+endif ()
+
+if (LZ4_FOUND)
+  if (NOT Lz4_FIND_QUIETLY)
+    message(STATUS "Found the Lz4 library: ${LZ4_LIB_PATH}")
+  endif ()
+else ()
+  if (NOT Lz4_FIND_QUIETLY)
+    set(LZ4_ERR_MSG "Could not find the Lz4 library. Looked for headers")
+    set(LZ4_ERR_MSG "${LZ4_ERR_MSG} in ${LZ4_SEARCH_HEADER_PATHS}, and for libs")
+    set(LZ4_ERR_MSG "${LZ4_ERR_MSG} in ${LZ4_SEARCH_LIB_PATH}")
+    if (Lz4_FIND_REQUIRED)
+      message(FATAL_ERROR "${LZ4_ERR_MSG}")
+    else (Lz4_FIND_REQUIRED)
+      message(STATUS "${LZ4_ERR_MSG}")
+    endif (Lz4_FIND_REQUIRED)
+  endif ()
+endif ()
+
+mark_as_advanced(
+  LZ4_INCLUDE_DIR
+  LZ4_LIBS
+  LZ4_STATIC_LIB
+)
