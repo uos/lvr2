d07ba241b61535c001dab9b530ad428f4907e314
pahoffmann <paddy.123456@web.de>
2019-02-08 16:04:48 +0100

    created options and base for new implementation of gcs and gss, one tool, userfriendly

---
+++ include/lvr2/reconstruction/GrowingCellStructures.hpp
@@ -0,0 +1,14 @@
+//
+// Created by patrick on 13.01.19.
+//
+
+#ifndef LAS_VEGAS_GROWINGCELLSTRUCTURES_H
+#define LAS_VEGAS_GROWINGCELLSTRUCTURES_H
+
+#include <lvr2/geometry/HalfEdgeMesh.hpp>
+
+
+
+
+
+#endif //LAS_VEGAS_GROWINGCELLSTRUCTURES_H
