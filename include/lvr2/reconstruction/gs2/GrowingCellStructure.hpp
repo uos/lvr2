63a1c824be1d17c424a7653e0353545a952471f3
pahoffmann <paddy.123456@web.de>
2019-02-10 22:12:48 +0100

    added pointsetsurfacecreation, added GCS-Base, updated options

---
+++ include/lvr2/reconstruction/gs2/GrowingCellStructure.hpp
@@ -0,0 +1,20 @@
+//
+// Created by patrick on 10.02.19.
+//
+
+#ifndef LAS_VEGAS_GROWINGCELLSTRUCTURES_HPP
+#define LAS_VEGAS_GROWINGCELLSTRUCTURES_HPP
+
+
+namespace lvr2{
+
+    template <typename BaseVecT, typename NormalT>
+    class GrowingCellStructures {
+        
+    };
+}
+
+
+
+
+#endif //LAS_VEGAS_GROWINGCELLSTRUCTURES_HPP
