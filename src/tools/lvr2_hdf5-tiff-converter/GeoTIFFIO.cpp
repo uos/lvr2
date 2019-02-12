7a091a28de4320ebdf9db1079762bc6f75c706be
ndettmer <ndettmer@uni-osnabrueck.de>
2019-02-12 15:12:10 +0100

    clean Converter

--- src/tools/lvr2_hdf5-tiff-converter/GeoTIFFIO.cpp
+++ src/tools/lvr2_hdf5-tiff-converter/GeoTIFFIO.cpp
@@ -2,6 +2,8 @@
 // Created by ndettmer on 07.02.19.
 //
 
+#include <iostream>
+
 #include "GeoTIFFIO.hpp"
 
 using namespace lvr2;
@@ -16,6 +18,12 @@ GeoTIFFIO::GeoTIFFIO(std::string filename, int cols, int rows, int bands) : m_co
 
 int GeoTIFFIO::writeBand(cv::Mat *mat, int band)
 {
+    if (!m_gtif_dataset)
+    {
+        std::cout << "GeoTIFF dataset not initialized!" << std::endl;
+        return -1;
+    }
+
     uint16_t *rowBuff = (uint16_t *) CPLMalloc(sizeof(uint16_t) * m_cols);
     for (int row = 0; row < m_rows; row++)
     {
@@ -23,9 +31,13 @@ int GeoTIFFIO::writeBand(cv::Mat *mat, int band)
         {
             rowBuff[col] = mat->at<uint16_t>(row, col);
         }
-        // TODO: handle return value
-        m_gtif_dataset->GetRasterBand(band)->RasterIO(
-                GF_Write, 0, row, m_cols, 1, rowBuff, m_cols, 1, GDT_UInt16, 0, 0);
+        if (m_gtif_dataset->GetRasterBand(band)->RasterIO(
+                GF_Write, 0, row, m_cols, 1, rowBuff, m_cols, 1, GDT_UInt16, 0, 0) != CPLE_None)
+        {
+            std::cout << "An error occurred in GDAL while writing band "
+                << band << " in row " << row << "." << std::endl;
+            return -1;
+        }
     }
     return 0;
 }
--- src/tools/lvr2_hdf5-tiff-converter/Main.cpp
+++ src/tools/lvr2_hdf5-tiff-converter/Main.cpp
@@ -1,47 +1,51 @@
-//
-// Created by ndettmer on 31.01.19.
-//
+/**
+ * @brief main file for HDF5 to GeoTIFF conversion
+ * @author ndettmer <ndettmer@uos.de>
+ */
 
 #include <lvr2/io/HDF5IO.hpp>
 #include <lvr2/io/ScanDataManager.hpp>
 
 #include <boost/range/iterator_range.hpp>
 #include <boost/foreach.hpp>
-
-#include <tiffio.h>
+#include <boost/filesystem.hpp>
 
 #include <string>
 #include <fstream>
+
+#include <sys/stat.h>
 
 #include "GeoTIFFIO.hpp"
 
 
 using namespace lvr2;
-using std::string;
-using std::vector;
-
-// DEBUG
-using std::ifstream;
-using std::cout;
-using std::endl;
 
 void printUsage()
 {
     std::cout << "Please submit the path to a .h5 file." << std::endl;
-    std::cout << "Usage: ./hdf5-tiff-converter <input_path>.h5 [<output_path>.tif]" << std::endl;
+    std::cout << "Usage: ./hdf5-tiff-converter <input_path>.h5 <position code> [<output_path>.tif]" << std::endl;
 }
 
-int processConversion(std::string input_filename, std::string output_filename)
+/**
+ * @brief Extraction of radiometric data from a given HDF5 file into a new GeoTIFF file in given output path
+ * @param input_filename Path to the input HDF5 file formatted due to lvr_2 convention
+ * @param position_code 5 character code of the scan position (e.g. 00000)
+ * @param output_filename Path to the output GeoTIFF file
+ * @return standard C++ return value
+ */
+int processConversion(std::string input_filename, std::string position_code, std::string output_filename)
 {
     /* =----------------- HDF5 INPUT ----------------------- */
     HDF5IO hdf5(input_filename, false);
     std::vector<size_t> dim;
 
+    // extract radiometric data
     // TODO: really use raw data?
-    string groupname = "raw/spectral/position_00010";
-    string datasetname = "spectral";
+    std::string groupname = "raw/spectral/position_" + position_code;
+    std::string datasetname = "spectral";
     boost::shared_array<uint16_t> spectrals = hdf5.getArray<uint16_t>(groupname, datasetname, dim);
 
+    // extract array dimension information
     size_t num_channels = dim[0];
     size_t num_rows = dim[1];
     size_t num_cols = dim[2];
@@ -49,7 +53,7 @@ int processConversion(std::string input_filename, std::string output_filename)
     GeoTIFFIO gtifio(output_filename, num_cols, num_rows, num_channels);
 
     /* -------------- FILE CONVERSION ------------------- */
-    // for each channel create a cv::Mat containing the spectral intensity data for the channel
+    // for each channel create a cv::Mat containing the spectral intensity data for the channel ...
     for(size_t channel = 0; channel < num_channels; channel++)
     {
         cv::Mat *mat = new cv::Mat(num_rows, num_cols, CV_16UC1);
@@ -60,7 +64,12 @@ int processConversion(std::string input_filename, std::string output_filename)
                 mat->at<uint16_t>(row, col) = spectrals.get()[channel * num_cols * num_rows + row * num_cols + col];
             }
         }
-        gtifio.writeBand(mat, channel + 1);
+        // ... and write it to the output GeoTIFF file
+        int ret = gtifio.writeBand(mat, channel + 1);
+        if (ret != 0)
+        {
+            return ret;
+        }
     }
 
 }
@@ -68,18 +77,31 @@ int processConversion(std::string input_filename, std::string output_filename)
 int main(int argc, char**argv)
 {
     /* ---------------- COMMAND LINE INPUT ----------------- */
-    if(!argv[1] || argv[1] == "--help")
+    if(!argv[1] || argv[1] == "--help" || !argv[2])
     {
         printUsage();
         return 0;
     }
-    string input_filename = argv[1];
+    boost::filesystem::path input_filename(argv[1]);
+    string position_code = argv[2];
 
-    string output_filename = "../../../../lvr_output/out.tif";
-    if(argv[2])
+    string output_filename_str = "../../../../lvr_output/out.tif";
+    if(argv[3])
     {
-        output_filename = argv[2];
+        output_filename_str = argv[3];
+    }
+    boost::filesystem::path output_filename(output_filename_str);
+
+    boost::filesystem::path output_dir = output_filename.parent_path();
+    if (!boost::filesystem::exists(output_dir))
+    {
+        boost::filesystem::create_directory(output_dir);
     }
 
-    return processConversion(input_filename, output_filename);
+    if (processConversion(input_filename.string(), position_code, output_filename.string()) < 0)
+    {
+        std::cout << "An Error occurred during conversion." << std::endl;
+    }
+
+    return 0;
 }
