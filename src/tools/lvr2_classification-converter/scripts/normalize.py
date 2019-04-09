d5536f2874f999adbe30f56b37f027754143c802
ndettmer <ndettmer@uni-osnabrueck.de>
2019-04-09 18:56:52 +0200

    finished normalizing

--- src/tools/lvr2_classification-converter/scripts/normalize.py
+++ src/tools/lvr2_classification-converter/scripts/normalize.py
@@ -26,15 +26,27 @@ values = np.empty((raster_count, y_size, x_size), dtype=np.float32)
 for i in range(0, num_channels):
     values[i] = np.array(ds.GetRasterBand(i + 1).ReadAsArray()).astype(np.float32)
 
+ds = None
+
 # source: https://gitlab.informatik.uni-osnabrueck.de/figelbri/hyperspace
 # normalize data
 shape = values.shape
 values = values.reshape(values.shape[0], -1)
 means = values.mean(axis=0) # mean of every spectrum
 mins = values.min(axis=0) # min of every spectrum
-values = ((values - mins) / (means-mins)).astype(np.float32).reshape(shape)
+#maxs = values.max(axis=0)
+vmax = np.amax(values)
+values = ((values - mins) / (means - mins))
 #image_spectral.data.Image.values = values # minmax_scale(values.ravel()).reshape(shape)
 #image_spectral.data.Image.values = minmax_scale(normalize_spectral_image(image_spectral.data.Image.values.astype(np.float32)).ravel()).reshape(shape)
+
+mins = None
+means = None
+
+#values = values * (maxs / 2.)
+values = values * vmax / 2.
+
+values = values.reshape(shape)
 
 # source: https://gitlab.informatik.uni-osnabrueck.de/figelbri/hyperspace
 # smooth spectral data
@@ -42,7 +54,7 @@ values = savgol_filter(values, 11, 2, axis=0)
 
 # write normalized dataset
 driver = gdal.GetDriverByName("GTiff")
-nds = driver.Create(outname, x_size, y_size, raster_count, gdal.GDT_Float32)
+ds = driver.Create(outname, x_size, y_size, raster_count, gdal.GDT_UInt16)
 for i in range(0, num_channels):
-    nds.GetRasterBand(i + 1).WriteArray(values[i])
+    ds.GetRasterBand(i + 1).WriteArray(values[i])
 
