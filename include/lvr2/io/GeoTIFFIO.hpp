f0d3ac76556c48d0f1e52b8c405502edffd11dc2
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2019-06-06 10:16:10 +0200

    Fixed cmake and some issues in GeoTiffIO

--- include/lvr2/io/GeoTIFFIO.hpp
+++ include/lvr2/io/GeoTIFFIO.hpp
@@ -11,62 +11,62 @@
 
 namespace lvr2
 {
+/**
+ * @brief class providing and encapsulating GDAL GeoTIFF I/O functions
+ * @author Niklas Dettmer <ndettmer@uos.de>
+ */
+class GeoTIFFIO
+{
+public:
     /**
-     * @brief class providing and encapsulating GDAL GeoTIFF I/O functions
-     * @author Niklas Dettmer <ndettmer@uos.de>
+     * @param filename filename of output GeoTIFF file
+     * @param cols number of columns / width of the image
+     * @param rows number of rows / length of the image
+     * @param bands number of bands
      */
-    class GeoTIFFIO
-    {
-    public:
-        /**
-         * @param filename filename of output GeoTIFF file
-         * @param cols number of columns / width of the image
-         * @param rows number of rows / length of the image
-         * @param bands number of bands
-         */
-        GeoTIFFIO(std::string filename, int cols, int rows, int bands);
+    GeoTIFFIO(std::string filename, int cols, int rows, int bands);
 
-        /**
-         * @param filename
-         */
-        GeoTIFFIO(std::string filename);
+    /**
+     * @param filename
+     */
+    GeoTIFFIO(std::string filename);
 
-        /**
-         * @brief Writing given band into open GeoTIFF file
-         * @param mat cv::Mat containing the band data
-         * @param band number of band to be written
-         * @return standard C++ return value
-         */
-        int writeBand(cv::Mat *mat, int band);
+    /**
+     * @brief Writing given band into open GeoTIFF file
+     * @param mat cv::Mat containing the band data
+     * @param band number of band to be written
+     * @return standard C++ return value
+     */
+    int writeBand(cv::Mat *mat, int band);
 
-        /**
-         * @return width of dataset in number of pixels
-         */
-        int getRasterWidth();
+    /**
+     * @return width of dataset in number of pixels
+     */
+    int getRasterWidth();
 
-        /**
-         * @return height of dataset in number of pixels
-         */
-        int getRasterHeight();
+    /**
+     * @return height of dataset in number of pixels
+     */
+    int getRasterHeight();
 
-        /**
-         * @return number of bands of dataset
-         */
-        int getNumBands();
+    /**
+     * @return number of bands of dataset
+     */
+    int getNumBands();
 
-        /**
-         * @param band_index index of the band to be read
-         * @return indexed band of the dataset as cv::Mat *
-         */
-        cv::Mat *readBand(int band_index);
+    /**
+     * @param band_index index of the band to be read
+     * @return indexed band of the dataset as cv::Mat *
+     */
+    cv::Mat *readBand(int band_index);
 
-        ~GeoTIFFIO();
+    ~GeoTIFFIO();
 
-    private:
-        GDALDataset *m_gtif_dataset;
-        GDALDriver *m_gtif_driver;
-        int m_cols, m_rows, m_bands;
-    };
+private:
+    GDALDataset *m_gtif_dataset;
+    GDALDriver *m_gtif_driver;
+    int m_cols, m_rows, m_bands;
+};
 }
 
 
