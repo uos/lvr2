e1566b13d0e6c7c14a9682efc1b8173b8452bd9f
amock <amock@kbs8fyh.informatik.uni-osnabrueck.de>
2019-07-30 16:45:14 +0200

    improved random

--- include/lvr2/algorithm/BaseBufferManipulators.hpp
+++ include/lvr2/algorithm/BaseBufferManipulators.hpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <chrono>
 #include <algorithm>
+#include <unordered_set>
 
 #include "lvr2/types/MultiChannelMap.hpp"
 
@@ -62,20 +63,42 @@ public:
 
         Channel<T> res(m_num_samples, channel.width());
 
-        std::vector<size_t> indices(channel.numElements());
-        std::iota(indices.begin(), indices.end(), 0);
-        std::shuffle(indices.begin(), indices.end(), std::default_random_engine(m_seed));
-        
-        for(size_t i=0; i<m_num_samples; i++)
+        if(m_num_samples > channel.numElements() / 2)
         {
-            for(size_t j=0; j<channel.width(); j++)
+            std::vector<size_t> indices(channel.numElements());
+            std::iota(indices.begin(), indices.end(), 0);
+            std::shuffle(indices.begin(), indices.end(), std::default_random_engine(m_seed));
+            
+            for(size_t i=0; i<m_num_samples; i++)
             {
-                res[i][j] = channel[indices[i]][j];
+                for(size_t j=0; j<channel.width(); j++)
+                {
+                    res[i][j] = channel[indices[i]][j];
+                }
+            }
+        } else {
+            std::unordered_set<size_t> indices;
+            std::srand(m_seed);
+            auto eng = std::default_random_engine(m_seed);
+            std::uniform_int_distribution<> distr(0, channel.numElements() - 1);
+
+            while(indices.size() < m_num_samples )
+            {
+                indices.insert(distr(eng));
+            }
+
+            size_t i = 0;
+            for(const size_t& index : indices)
+            {
+                for(size_t j=0; j<channel.width(); j++)
+                {
+                    res[i][j] = channel[index][j];
+                }
+                i++;
             }
         }
 
         vres = res;
-
         return vres;
     }
 
