1a434255865ec47a615bf4e3e9006219808a9168
amock <amock@kbs8fyh.informatik.uni-osnabrueck.de>
2019-08-15 19:49:55 +0200

    added coordinate conversions. slam6d <-> lvr, opencv <-> lvr

--- include/lvr2/registration/TransformUtils.hpp
+++ include/lvr2/registration/TransformUtils.hpp
@@ -204,36 +204,386 @@ static Transform<T> rieglToSLAM6DTransform(const Transform<T>& in)
 template <typename T>
 static Transform<T> slam6dToRieglTransform(const Transform<T> &in)
 {
-        Transform<T> ret;
+    Transform<T> ret;
 
-        ret(0) = in(10);
-        ret(1) = -in(2);
-        ret(2) = in(6);
-        ret(3) = in(14)/100.0;
-        ret(4) = -in(8);
-        ret(5) = in(0);
-        ret(6) = -in(4);
-        ret(7) = -in(12)/100.0;
-        ret(8) = in(9);
-        ret(9) = -in(1);
-        ret(10) = in(5);
-        ret(11) = in(13)/100.0;
-        ret(12) = in(11);
-        ret(13) = -in(3);
-        ret(14) = in(7);
-        ret(15) = in(15);
+    ret(0) = in(10);
+    ret(1) = -in(2);
+    ret(2) = in(6);
+    ret(3) = in(14)/100.0;
+    ret(4) = -in(8);
+    ret(5) = in(0);
+    ret(6) = -in(4);
+    ret(7) = -in(12)/100.0;
+    ret(8) = in(9);
+    ret(9) = -in(1);
+    ret(10) = in(5);
+    ret(11) = in(13)/100.0;
+    ret(12) = in(11);
+    ret(13) = -in(3);
+    ret(14) = in(7);
+    ret(15) = in(15);
 
-        return ret;
+    return ret;
 }
 
 template <typename T>
 static Vector3<T> slam6DToRieglPoint(const Vector3<T> &in)
 {
-        return {
-                in.z   / (T) 100.0,
-                - in.x / (T) 100.0,
-                in.y   / (T) 100.0
-        };
+    return Vector3<T>(
+        in.coeff(2)   / static_cast<T>(100.0),
+        - in.coeff(0) / static_cast<T>(100.0),
+        in.coeff(1)   / static_cast<T>(100.0)
+    );
+}
+
+/////////////////////////////////
+// Change Coodinate Systems
+/////////////////////////////////
+
+///////////////////////
+// 1) CS -> LVR
+//////////////
+
+/**
+ * @brief Slam6D to LVR coordinate change: Point
+ * 
+ *  Slam6D
+ * 
+ *     y  z    
+ *     | /
+ *     |/___ x
+ *     
+ *  - x: right
+ *  - y: up
+ *  - z: front
+ *  - scale: cm
+ * 
+ *  LVR / ROS
+ *        z  x    
+ *        | /
+ *   y ___|/ 
+ * 
+ *  - x: front
+ *  - y: left
+ *  - z: up
+ *  - scale: m
+ * 
+ */
+template <typename T>
+static Vector3<T> slam6dToLvr(const Vector3<T>& in)
+{
+    return Vector3<T>(
+        in.coeff(2) / static_cast<T>(100.0),
+        - in.coeff(0) / static_cast<T>(100.0),
+        in.coeff(1) / static_cast<T>(100.0)
+    );
+}
+
+template<typename T>
+static Rotation<T> slam6dToLvr(const Rotation<T>& in)
+{
+    Rotation<T> ret;
+    
+    // use coeff functions to access elements without range check
+    // should be faster?
+
+    // OLD CODE: why transpose?
+    // ret.coeffRef(0, 0) =  in.coeff(2, 2);
+    // ret.coeffRef(0, 1) = -in.coeff(0, 2);
+    // ret.coeffRef(0, 2) =  in.coeff(1, 2);
+    // ret.coeffRef(1, 0) = -in.coeff(2, 0);
+    // ret.coeffRef(1, 1) =  in.coeff(0, 0);
+    // ret.coeffRef(1, 2) = -in.coeff(1, 0);
+    // ret.coeffRef(2, 0) =  in.coeff(2, 1);
+    // ret.coeffRef(2, 1) = -in.coeff(0, 1);
+    // ret.coeffRef(2, 2) =  in.coeff(1, 1);
+
+    ret.coeffRef(0, 0) =  in.coeff(2, 2);
+    ret.coeffRef(1, 0) = -in.coeff(0, 2);
+    ret.coeffRef(2, 0) =  in.coeff(1, 2);
+    ret.coeffRef(0, 1) = -in.coeff(2, 0);
+    ret.coeffRef(1, 1) =  in.coeff(0, 0);
+    ret.coeffRef(2, 1) = -in.coeff(1, 0);
+    ret.coeffRef(0, 2) =  in.coeff(2, 1);
+    ret.coeffRef(1, 2) = -in.coeff(0, 1);
+    ret.coeffRef(2, 2) =  in.coeff(1, 1);
+    
+    return ret;
+}
+
+template <typename T>
+static Transform<T> slam6dToLvr(const Transform<T> &in)
+{
+    Transform<T> ret;
+
+    // Rotation
+    const Rotation<T> R = in.template block<3,3>(0,0);
+    ret.template block<3,3>(0,0) = slam6dToLvr(R);
+
+    // Translation
+    ret.coeffRef(0, 3) =  in.coeff(2, 3)/static_cast<T>(100.0);
+    ret.coeffRef(1, 3) = -in.coeff(0, 3)/static_cast<T>(100.0);
+    ret.coeffRef(2, 3) =  in.coeff(1, 3)/static_cast<T>(100.0);
+    ret.coeffRef(3, 0) =  in.coeff(3, 2)/static_cast<T>(100.0);
+    ret.coeffRef(3, 1) = -in.coeff(3, 0)/static_cast<T>(100.0);
+    ret.coeffRef(3, 2) =  in.coeff(3, 1)/static_cast<T>(100.0);
+    ret.coeffRef(3, 3) =  in.coeff(3, 3);
+
+    return ret;
+}
+
+/**
+ * @brief OpenCV to Lvr coordinate change: Point
+ * 
+ *  OpenCV
+ * 
+ *        z    
+ *       /
+ *      /___ x
+ *     |
+ *     |
+ *     y
+ *  
+ *  - x: right
+ *  - y: down
+ *  - z: front
+ *  - scale: m
+ * 
+ *  LVR / ROS
+ *        z  x    
+ *        | /
+ *   y ___|/ 
+ * 
+ *  - x: front
+ *  - y: left
+ *  - z: up
+ *  - scale: m
+ * 
+ */
+template <typename T>
+static Vector3<T> openCvToLvr(const Vector3<T>& in)
+{
+    return Vector3<T>(
+        in.coeff(2),
+        -in.coeff(0),
+        -in.coeff(1)
+    );
+}
+
+
+
+
+template <typename T>
+static Rotation<T> openCvToLvr(const Rotation<T> &in)
+{
+    Rotation<T> ret;
+
+    ret.coeffRef(0,0) =  in.coeff(2,2);
+    ret.coeffRef(0,1) = -in.coeff(2,0);
+    ret.coeffRef(0,2) = -in.coeff(2,1);
+    ret.coeffRef(1,0) = -in.coeff(0,2);
+    ret.coeffRef(1,1) =  in.coeff(0,0);
+    ret.coeffRef(1,2) =  in.coeff(0,1);
+    ret.coeffRef(2,0) = -in.coeff(1,2);
+    ret.coeffRef(2,1) =  in.coeff(1,0);
+    ret.coeffRef(2,2) =  in.coeff(1,1);
+
+    return ret;
+}
+
+template <typename T>
+static Transform<T> openCvToLvr(const Transform<T> &in)
+{
+    Transform<T> ret;
+
+    const Rotation<T> R = in.template block<3,3>(0,0);
+    ret.template block<3,3>(0,0) = openCvToLvr(R);
+
+    ret.coeffRef(0,3) =  in.coeff(2,3);
+    ret.coeffRef(1,3) = -in.coeff(0,3);
+    ret.coeffRef(2,3) = -in.coeff(1,3);
+
+    ret.coeffRef(3,0) =  in.coeff(3,2);
+    ret.coeffRef(3,1) = -in.coeff(3,0);
+    ret.coeffRef(3,2) = -in.coeff(3,1);
+
+    ret.coeffRef(3,3) =  in.coeff(3,3);
+
+    return ret;
+}
+
+
+////////////////
+// 2) Lvr -> CS
+////////////////
+
+
+
+/**
+ * @brief Lvr to Slam6D coordinate change: Point
+ * 
+ *  Slam6D
+ * 
+ *     y  z    
+ *     | /
+ *     |/___ x
+ *     
+ *  - x: right
+ *  - y: up
+ *  - z: front
+ *  - scale: cm
+ * 
+ *  LVR / ROS
+ *        z  x    
+ *        | /
+ *   y ___|/ 
+ * 
+ *  - x: front
+ *  - y: left
+ *  - z: up
+ *  - scale: m
+ */
+template <typename T>
+static Vector3<T> lvrToSlam6d(const Vector3<T>& in)
+{
+    return Vector3<T>(
+        -in.coeff(1) * static_cast<T>(100.0),
+        in.coeff(2) * static_cast<T>(100.0),
+        in.coeff(0) * static_cast<T>(100.0)
+    );
+}
+
+template<typename T>
+static Rotation<T> lvrToSlam6d(const Rotation<T>& in)
+{
+    Rotation<T> ret;
+    
+    // use coeff functions to access elements without range check
+    // should be faster?
+
+    // OLD CODE: why transpose?
+    // ret.coeffRef(0, 0) =  in.coeff(1, 1); // in.coeff(1, 1)
+    // ret.coeffRef(0, 1) = -in.coeff(2, 1); // in.coeff(1, 2)
+    // ret.coeffRef(0, 2) = -in.coeff(0, 1); // in.coeff(1, 0)
+    // ret.coeffRef(1, 0) = -in.coeff(1, 2);
+    // ret.coeffRef(1, 1) =  in.coeff(2, 2);
+    // ret.coeffRef(1, 2) =  in.coeff(0, 2);
+    // ret.coeffRef(2, 0) = -in.coeff(1, 0);
+    // ret.coeffRef(2, 1) =  in.coeff(2, 0);
+    // ret.coeffRef(2, 2) =  in.coeff(0, 0);
+
+    ret.coeffRef(0, 0) =  in.coeff(1, 1); // in.coeff(1, 1)
+    ret.coeffRef(1, 0) = -in.coeff(2, 1); // in.coeff(1, 2)
+    ret.coeffRef(2, 0) = -in.coeff(0, 1); // in.coeff(1, 0)
+    ret.coeffRef(0, 1) = -in.coeff(1, 2);
+    ret.coeffRef(1, 1) =  in.coeff(2, 2);
+    ret.coeffRef(2, 1) =  in.coeff(0, 2);
+    ret.coeffRef(0, 2) = -in.coeff(1, 0);
+    ret.coeffRef(1, 2) =  in.coeff(2, 0);
+    ret.coeffRef(2, 2) =  in.coeff(0, 0);
+    
+    return ret;
+}
+
+template <typename T>
+static Transform<T> lvrToSlam6d(const Transform<T> &in)
+{
+    Transform<T> ret;
+
+    // Rotation
+    const Rotation<T> R = in.template block<3,3>(0,0);
+    ret.template block<3,3>(0,0) = lvrToSlam6d(R);
+
+    // Translation
+    ret.coeffRef(0, 3) = -static_cast<T>(100.0) * in.coeff(1, 3);
+    ret.coeffRef(1, 3) =  static_cast<T>(100.0) * in.coeff(2, 3);
+    ret.coeffRef(2, 3) =  static_cast<T>(100.0) * in.coeff(0, 3);
+    ret.coeffRef(3, 0) = -static_cast<T>(100.0) * in.coeff(3, 1);
+    ret.coeffRef(3, 1) =  static_cast<T>(100.0) * in.coeff(3, 2);
+    ret.coeffRef(3, 2) =  static_cast<T>(100.0) * in.coeff(3, 0);
+
+    ret.coeffRef(3, 3) = in.coeff(3, 3);
+
+    return ret;
+}
+
+
+
+/**
+ * @brief Lvr to OpenCV coordinate change: Point
+ * 
+ *  OpenCV
+ * 
+ *        z    
+ *       /
+ *      /___ x
+ *     |
+ *     |
+ *     y
+ *  
+ * - x: right
+ * - y: down
+ * - z: front
+ * - scale: m
+ * 
+ *  LVR / ROS
+ *        z  x    
+ *        | /
+ *   y ___|/ 
+ * 
+ *  - x: front
+ *  - y: left
+ *  - z: up
+ *  - scale: m
+ * 
+ */
+template <typename T>
+static Vector3<T> lvrToOpenCv(const Vector3<T>& in)
+{
+    return Vector3<T>(
+        -in.coeff(1),
+        -in.coeff(2),
+        in.coeff(0)
+    );
+}
+
+template <typename T>
+static Rotation<T> lvrToOpenCv(const Rotation<T>& in)
+{
+    Rotation<T> ret;
+
+    ret.coeffRef(0,0) =  in.coeff(1,1);
+    ret.coeffRef(0,1) =  in.coeff(1,2);
+    ret.coeffRef(0,2) = -in.coeff(1,0);
+    ret.coeffRef(1,0) =  in.coeff(2,1);
+    ret.coeffRef(1,1) =  in.coeff(2,2);
+    ret.coeffRef(1,2) = -in.coeff(2,0);
+    ret.coeffRef(2,0) = -in.coeff(0,1);
+    ret.coeffRef(2,1) = -in.coeff(0,2);
+    ret.coeffRef(2,2) =  in.coeff(0,0);
+
+    return ret;
+}
+
+template <typename T>
+static Transform<T> lvrToOpenCv(const Transform<T> &in)
+{
+    Transform<T> ret;
+
+    const Rotation<T> R = in.template block<3,3>(0,0);
+    ret.template block<3,3>(0,0) = lvrToOpenCv(R);
+
+    ret.coeffRef(0,3) = -in.coeff(1,3);
+    ret.coeffRef(1,3) = -in.coeff(2,3);
+    ret.coeffRef(2,3) =  in.coeff(0,3);
+
+    ret.coeffRef(3,0) = -in.coeff(3,1);
+    ret.coeffRef(3,1) = -in.coeff(3,2);
+    ret.coeffRef(3,2) =  in.coeff(3,0);
+
+    ret.coeffRef(3,3) =  in.coeff(3,3);
+
+    return ret;
 }
 
 template<typename T>
@@ -271,6 +621,8 @@ void eigenToEuler(Transform<T>& mat, T* pose)
                 pose[2] = m[14];
         }
 }
+
+
 
 } // namespace lvr2
 
