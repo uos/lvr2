1cdb7abdb176b81a2dde49bd4eac460f3ac7061e
amock <amock@kbs8fyh.informatik.uni-osnabrueck.de>
2019-08-19 21:24:15 +0200

    added example for feature based io

--- examples/lvr2_io_features/Main.cpp
+++ examples/lvr2_io_features/Main.cpp
@@ -12,42 +12,46 @@
 #include "lvr2/registration/TransformUtils.hpp"
 #include "lvr2/types/MatrixTypes.hpp"
 
-#include "hdf5_features/ChannelIO.hpp"
-#include "hdf5_features/HyperIO.hpp"
-#include "hdf5_features/ImageIO.hpp"
+#include "lvr2/io/Hdf5IO.hpp"
+#include "lvr2/io/hdf5/ArrayIO.hpp"
+#include "lvr2/io/hdf5/ChannelIO.hpp"
 
-#include "types/Channel.hpp"
-#include "types/Image.hpp"
-#include "types/Hyper.hpp"
+// #include "hdf5_features/ChannelIO.hpp"
+// #include "hdf5_features/HyperIO.hpp"
+// #include "hdf5_features/ImageIO.hpp"
 
-template<template<typename> typename ...Features>
-class HDF5IO : public Features<HDF5IO<Features...> >...
-{
-public:
-    static constexpr std::size_t N = sizeof...(Features);
-    using feature_tuple = std::tuple<Features<HDF5IO<Features...> >...>;
-    using index_sequence = std::make_index_sequence<N>;
+// #include "types/Channel.hpp"
+// #include "types/Image.hpp"
+// #include "types/Hyper.hpp"
 
-    #if __cplusplus <= 201500L
-        // feature of c++17
-        #pragma message("using Tp::save... needs c++17 at least or a newer compiler")
-    #endif
+// template<template<typename> typename ...Features>
+// class HDF5IO : public Features<HDF5IO<Features...> >...
+// {
+// public:
+//     static constexpr std::size_t N = sizeof...(Features);
+//     using feature_tuple = std::tuple<Features<HDF5IO<Features...> >...>;
+//     using index_sequence = std::make_index_sequence<N>;
 
-    using Features<HDF5IO<Features...> >::save...;
+//     #if __cplusplus <= 201500L
+//         // feature of c++17
+//         #pragma message("using Tp::save... needs c++17 at least or a newer compiler")
+//     #endif
 
-    void open(std::string filename)
-    {
-        std::cout << index_sequence::size() << std::endl;
-        m_filename = filename;
-    }
+//     using Features<HDF5IO<Features...> >::save...;
 
-    std::string filename() {
-        return m_filename;
-    }
+//     void open(std::string filename)
+//     {
+//         std::cout << index_sequence::size() << std::endl;
+//         m_filename = filename;
+//     }
 
-protected:
-    std::string m_filename;
-};
+//     std::string filename() {
+//         return m_filename;
+//     }
+
+// protected:
+//     std::string m_filename;
+// };
 
 
 /////////////////////
@@ -71,22 +75,67 @@ protected:
 
 int main(int argc, char** argv)
 {
-    Channel ch;
-    Image im;
-    Hyper hy;
+    // Channel ch;
+    // Image im;
+    // Hyper hy;
 
-    using MyHDF5IO = HDF5IO<hdf5_features::ChannelIO, hdf5_features::ImageIO, hdf5_features::HyperIO>;
-    MyHDF5IO io;
+    // using MyHDF5IO = HDF5IO<hdf5_features::ChannelIO, hdf5_features::ImageIO, hdf5_features::HyperIO>;
+    // MyHDF5IO io;
 
-    io.open("hello.h5");
+    // io.open("hello.h5");
 
-    io.save(ch);
-    io.save(im);
-    io.save(hy);
+    // io.save(ch);
+    // io.save(im);
+    // io.save(hy);
 
-    ch = io.ChannelIO::load();
-    im = io.ImageIO::load();
-    hy = io.HyperIO::load();
+    // ch = io.ChannelIO::load();
+    // im = io.ImageIO::load();
+    // hy = io.HyperIO::load();
+
+    
+    
+    using MyHDF5IO = lvr2::Hdf5IO<
+        lvr2::hdf5features::ArrayIO,
+        lvr2::hdf5features::ChannelIO
+    >;
+
+
+    size_t num_data = 10000;
+    boost::shared_array<float> data(new float[num_data]);
+
+
+    lvr2::Channel<double> channel(1000, 3);
+
+
+    MyHDF5IO my_io;
+    my_io.open("test.h5");
+
+    // ARRAYIO
+    my_io.save("agroup", "anarray", num_data, data);
+    size_t N;
+    boost::shared_array<float> data_loaded = my_io.ArrayIO::load<float>("agroup", "anarray", N);
+    
+    if(N == num_data)
+    {
+        std::cout << "ArrayIO successful" << std::endl;
+    }
+
+    // CHANNELIO
+    my_io.save("agroup", "achannel", channel);
+
+    lvr2::ChannelOptional<float> channel_loaded = my_io.ChannelIO::load<float>("agroup", "achannel");
+
+    if(channel_loaded)
+    {
+        if(channel_loaded->numElements() == channel.numElements() && channel_loaded->width() == channel.width())
+        {
+            std::cout << "ChannelIO successful" << std::endl;
+        }
+        
+    } else {
+        std::cout << "channel not found" << std::endl;
+    }
+
 
 
 }
