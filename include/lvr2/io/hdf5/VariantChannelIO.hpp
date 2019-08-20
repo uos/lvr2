640fa37d484b79bf8f5d9241d77a153138ce2018
amock <amock@kbs8fyh.informatik.uni-osnabrueck.de>
2019-08-20 19:15:01 +0200

    added empty hdf5feature VariantChannelIO

---
+++ include/lvr2/io/hdf5/VariantChannelIO.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef LVR2_IO_HDF5_VARIANTCHANNELIO_HPP
+#define LVR2_IO_HDF5_VARIANTCHANNELIO_HPP
+
+#include "lvr2/types/VariantChannel.hpp"
+
+namespace lvr2 {
+
+namespace hdf5features {
+
+template<typename Derived>
+class VariantChannelIO {
+public:
+
+    template<typename ...Tp>
+    void save(std::string groupName, std::string datasetName, const VariantChannel<Tp...>& vchannel);
+    
+    template<typename ...Tp>
+    void save(HighFive::Group& group, std::string datasetName, const VariantChannel<Tp...>& vchannel);
+    
+    template<typename ...Tp>
+    VariantChannelOptional<Tp...> load(std::string groupName, std::string datasetName);
+    
+    template<typename ...Tp>
+    VariantChannelOptional<Tp...> load(HighFive::Group& group, std::string datasetName);
+    
+protected:
+
+    Derived* m_file_access = static_cast<Derived*>(this);
+    ChannelIO<Derived>* m_channel_io = static_cast<ChannelIO<Derived>*>(m_file_access);
+};
+
+
+} // hdf5features
+
+} // namespace lvr2 
+
+#include "VariantChannelIO.tcc"
+
+#endif // LVR2_IO_HDF5_VARIANTCHANNELIO_HPP
