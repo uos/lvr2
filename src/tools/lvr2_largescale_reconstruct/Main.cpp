37c0f01f7489141367229e583a4df1b61ad6dfb5
Marcel Wiegand <mawiegand@uni-osnabrueck.de>
2020-01-20 17:43:06 +0100

    Revert "Move LargeScaleOptions files."
    This reverts commit bdfcbd45ea9a0218d6e113d3ca7c192c04186b54.

--- src/tools/lvr2_largescale_reconstruct/Main.cpp
+++ src/tools/lvr2_largescale_reconstruct/Main.cpp
@@ -25,7 +25,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "lvr2/config/LargeScaleOptions.hpp"
+#include "LargeScaleOptions.hpp"
 #include "lvr2/reconstruction/LargeScaleReconstruction.hpp"
 #include "lvr2/algorithm/GeometryAlgorithms.hpp"
 #include <algorithm>
