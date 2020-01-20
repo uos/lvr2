7ee4b55de02aaf4e1febd01c4e12664cd721dab7
Benedikt Schumacher <bschumacher@uos.de>
2020-01-20 20:00:58 +0100

    implemented example for scanIO

---
+++ examples/lvr2_scan_io/Main.cpp
@@ -0,0 +1,35 @@
+#include "lvr2/io/MeshBuffer.hpp"
+#include "lvr2/io/PointBuffer.hpp"
+#include "lvr2/io/ScanIOUtils.hpp"
+#include "lvr2/types/ScanTypes.hpp"
+#include "lvr2/util/Synthetic.hpp"
+
+using namespace lvr2;
+
+int main(int argc, char** argv)
+{
+    // generate synthetic sphere
+    MeshBufferPtr sphere = synthetic::genSphere(10, 10);
+
+    // create scan object
+    ScanOptional scan_opt = Scan();
+
+    // extract points to a pointBufferPtr
+    PointBufferPtr points(new PointBuffer(sphere->getVertices(), sphere->numVertices()));
+
+    // add points to scan object
+    scan_opt->m_points = points;
+
+    // create ScanPositionPtr
+    ScanPositionPtr scan_pos(new ScanPosition());
+    scan_pos->scan = scan_opt;
+
+    // create ScanProjectPtr
+    ScanProjectPtr scan_proj(new ScanProject());
+    scan_proj->positions.push_back(scan_pos);
+    scan_proj->pose = Transformd::Identity();
+
+    saveScanProjectToDirectory("example_sav", *scan_proj);
+
+    return 0;
+}
