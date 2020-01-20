7e61ded599c23a75aadba911bed4a327d83b1f85
Benedikt Schumacher <bschumacher@uos.de>
2020-01-20 20:52:40 +0100

    added test scenarios

--- examples/lvr2_scan_io/Main.cpp
+++ examples/lvr2_scan_io/Main.cpp
@@ -8,27 +8,34 @@ using namespace lvr2;
 
 int main(int argc, char** argv)
 {
-    // generate synthetic sphere
-    MeshBufferPtr sphere = synthetic::genSphere(10, 10);
-
-    // create scan object
-    ScanOptional scan_opt = Scan();
-
-    // extract points to a pointBufferPtr
-    PointBufferPtr points(new PointBuffer(sphere->getVertices(), sphere->numVertices()));
-
-    // add points to scan object
-    scan_opt->m_points = points;
-
-    // create ScanPositionPtr
-    ScanPositionPtr scan_pos(new ScanPosition());
-    scan_pos->scan = scan_opt;
-
     // create ScanProjectPtr
     ScanProjectPtr scan_proj(new ScanProject());
-    scan_proj->positions.push_back(scan_pos);
-    scan_proj->pose = Transformd::Identity();
 
+    for (int i = 0; i < 4; i++)
+    {
+        // generate synthetic sphere
+        MeshBufferPtr sphere = synthetic::genSphere(10, 10);
+
+        // create scan object
+        ScanOptional scan_opt = Scan();
+
+        // extract points to a pointBufferPtr
+        PointBufferPtr points(new PointBuffer(sphere->getVertices(), sphere->numVertices()));
+
+        // add points to scan object
+        scan_opt->m_points = points;
+        scan_opt->m_phiMin = i + 0.5;
+        scan_opt->m_phiMax = i + 1.0;
+
+        // create ScanPositionPtr
+        ScanPositionPtr scan_pos(new ScanPosition());
+        scan_pos->scan = scan_opt;
+
+        // add scans to scanProjectPtr
+        scan_proj->positions.push_back(scan_pos);
+    }
+
+    scan_proj->pose = Transformd::Identity();
     saveScanProjectToDirectory("example_sav", *scan_proj);
 
     return 0;
