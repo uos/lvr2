801b5bd12e13de74b1f5fe613da88e335499f26e
Timo <tosterkamp@uni-osnabrueck.de>
2020-01-22 16:57:24 +0100

    small fix and registration test

--- src/tools/lvr2_registration/Main.cpp
+++ src/tools/lvr2_registration/Main.cpp
@@ -379,6 +379,7 @@ int main(int argc, char** argv)
 
     // DEBUG
     ScanProjectEditMark proj;
+    proj.project = ScanProjectPtr(new ScanProject);
 
     int count = end - start + 1;
 
@@ -430,30 +431,27 @@ int main(int argc, char** argv)
             // DEBUG
             ScanPosition pos;
             pos.scan = boost::optional<Scan>(*tempScan);
-            proj.project = ScanProjectPtr(new ScanProject);
             proj.project->positions.push_back(std::make_shared<ScanPosition>(pos));
             proj.changed.push_back(false);
 
-            
-
             SLAMScanPtr slamScan = SLAMScanPtr(new SLAMScanWrapper(tempScan));
 
-            scans.push_back(slamScan);
-            align.addScan(slamScan);
+           // scans.push_back(slamScan);
+           // align.addScan(slamScan);
         }
         // DEBUG
 
-        // cout << "vor Pipe Konstruktor" << endl;
-        // ScanProjectEditMarkPtr projPtr = make_shared<ScanProjectEditMark>(proj);
-        // RegistrationPipeline pipe(&options, projPtr);
-        // pipe.doRegistration();
-        // cout << "Nach doRegistration" << endl;
-        // for (size_t i = 0; i < projPtr->changed.size(); i++)
-        // {
-        //     cout << "Reconstruct indivcator ans Stelle: " << i << " ist: " << projPtr->changed.at(i)<< endl;
-        // }
+         cout << "vor Pipe Konstruktor" << endl;
+         ScanProjectEditMarkPtr projPtr = make_shared<ScanProjectEditMark>(proj);
+         RegistrationPipeline pipe(&options, projPtr);
+         pipe.doRegistration();
+         cout << "Nach doRegistration" << endl;
+         for (size_t i = 0; i < projPtr->changed.size(); i++)
+         {
+             cout << "Reconstruct indivcator ans Stelle: " << i << " ist: " << projPtr->changed.at(i)<< endl;
+         }
 
-        // cout << "Eine Pose aus dem Project:" << endl << projPtr->project->positions.at(1)->scan->m_registration << endl;
+         cout << "Eine Pose aus dem Project:" << endl << projPtr->project->positions.at(1)->scan->m_registration << endl;
     }
     else
     {
