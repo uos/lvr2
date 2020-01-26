d5df61631b589e2468e8ace8f378ed065fc33d34
lennart <niecklen@gmail.com>
2020-01-26 16:19:22 +0100

    WIP lod worker starts workerthread.

--- src/tools/lvr2_viewer/vtkBridge/LVRChunkedMeshBridge.hpp
+++ src/tools/lvr2_viewer/vtkBridge/LVRChunkedMeshBridge.hpp
@@ -15,6 +15,10 @@
 
 #include "MeshChunkActor.hpp"
 
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+
 
 
 namespace lvr2 {
@@ -28,13 +32,28 @@ namespace lvr2 {
                     
                     //std::unordered_map<size_t, vtkSmartPointer<MeshChunkActor> >& actors);
             void addInitialActors(vtkSmartPointer<vtkRenderer> renderer);
+
+            void getHighRes(double x, double y, double z);
         protected:
             void computeMeshActors();
+            inline vtkSmartPointer<MeshChunkActor> computeMeshActor(size_t& id, MeshBufferPtr& chunk);
 
         private:
+
+            std::thread worker;
+            std::mutex mutex;
+            std::condition_variable cond_;
+            double dist_;
+            bool getNew_;
+            bool running_;
+            BoundingBox<BaseVector<float> > region_;
+
+            void highResWorker();
             lvr2::ChunkManager m_chunkManager;
             std::unordered_map<size_t, MeshBufferPtr> m_chunks;
+            std::unordered_map<size_t, MeshBufferPtr> m_highRes;
             std::unordered_map<size_t, vtkSmartPointer<MeshChunkActor> > m_chunkActors;
+            std::unordered_map<size_t, vtkSmartPointer<MeshChunkActor> > m_highResActors;
 
             MeshOctree<BaseVector<float> >* m_oct;
 //            std::unordered_map<size_t, std::vector<vtkPolyData> > > m_chunkActors;
