a48fc776d837838e815a287d59c2616a02efbff2
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2020-04-02 14:39:15 +0200

    Added write image to HDF5 kernel

--- src/liblvr2/io/descriptions/HDF5Kernel.cpp
+++ src/liblvr2/io/descriptions/HDF5Kernel.cpp
@@ -32,11 +32,144 @@ void HDF5Kernel::savePointBuffer(
 
 
 void HDF5Kernel::saveImage(
-    const std::string &group,
-    const std::string &container,
-    const cv::Mat &image) const
+    const std::string &groupName,
+    const std::string &datasetName,
+    const cv::Mat &img) const
 {
+    if(m_hdf5File && m_hdf5File->isValid())
+    {
+        HighFive::Group group = hdf5util::getGroup(m_hdf5File, groupName, true);
 
+        int w = img.cols;
+        int h = img.rows;
+        const char* interlace = "INTERLACE_PIXEL";
+
+        // TODO: need to remove dataset if exists because of H5IMmake_image_8bit. do it better
+        
+        if(img.type() == CV_8U) 
+        {
+            if(group.exist(datasetName))
+            {
+                H5Ldelete(group.getId(), datasetName.data(), H5P_DEFAULT);
+            }
+            H5IMmake_image_8bit(group.getId(), datasetName.c_str(), w, h, img.data);
+        } 
+        else if(img.type() == CV_8UC3) 
+        {
+            if(group.exist(datasetName))
+            {
+                H5Ldelete(group.getId(), datasetName.data(), H5P_DEFAULT);
+            }
+            // bgr -> rgb?
+            H5IMmake_image_24bit(group.getId(), datasetName.c_str(), w, h, interlace, img.data);
+        } 
+        else 
+        {
+            // std::cout << "[Hdf5IO - ImageIO] WARNING: OpenCV type not implemented -> " 
+            //     << img.type() << ". Saving image as data blob." << std::endl;
+
+            // Couldnt write as H5Image, write as blob
+
+            std::vector<size_t> dims = {static_cast<size_t>(img.rows), static_cast<size_t>(img.cols)};
+            std::vector<hsize_t> chunkSizes = {static_cast<hsize_t>(img.rows), static_cast<hsize_t>(img.cols)};
+
+            if(img.channels() > 1)
+            {
+                dims.push_back(img.channels());
+                chunkSizes.push_back(img.channels());
+            }
+
+            HighFive::DataSpace dataSpace(dims);
+            HighFive::DataSetCreateProps properties;
+
+            // if(m_hdf5File->m_chunkSize)
+            // {
+            //     for(size_t i = 0; i < chunkSizes.size(); i++)
+            //     {
+            //         if(chunkSizes[i] > dims[i])
+            //         {
+            //             chunkSizes[i] = dims[i];
+            //         }
+            //     }
+            //     properties.add(HighFive::Chunking(chunkSizes));
+            // }
+            // if(m_hdf5File->m_compress)
+            // {
+            //     properties.add(HighFive::Deflate(9));
+            // }
+
+            // Single Channel Type
+            const int SCTYPE = img.type() % 8;
+    
+
+            if(SCTYPE == CV_8U) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<unsigned char>(
+                    group, datasetName, dataSpace, properties
+                );
+                const unsigned char* ptr = reinterpret_cast<unsigned char*>(img.data);
+                dataset->write(ptr);
+            } 
+            else if(SCTYPE == CV_8S) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<char>(
+                    group, datasetName, dataSpace, properties
+                );
+                const char* ptr = reinterpret_cast<char*>(img.data);
+                dataset->write(ptr);
+            } 
+            else if(SCTYPE == CV_16U) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<unsigned short>(
+                    group, datasetName, dataSpace, properties
+                );
+                const unsigned short* ptr = reinterpret_cast<unsigned short*>(img.data);
+                dataset->write(ptr);
+            } 
+            else if(SCTYPE == CV_16S) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<short>(
+                    group, datasetName, dataSpace, properties
+                );
+                const short* ptr = reinterpret_cast<short*>(img.data);
+                dataset->write(ptr);
+            } 
+            else if(SCTYPE == CV_32S) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<int>(
+                    group, datasetName, dataSpace, properties
+                );
+                const int* ptr = reinterpret_cast<int*>(img.data);
+                dataset->write(ptr);
+            } 
+            else if(SCTYPE == CV_32F) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<float>(
+                    group, datasetName, dataSpace, properties
+                );
+                const float* ptr = reinterpret_cast<float*>(img.data);
+                dataset->write(ptr);
+            } 
+            else if(SCTYPE == CV_64F) 
+            {
+                std::unique_ptr<HighFive::DataSet> dataset = hdf5util::createDataset<double>(
+                    group, datasetName, dataSpace, properties
+                );
+                const double* ptr = reinterpret_cast<double*>(img.data);
+                dataset->write(ptr);
+            } 
+            else 
+            {
+                std::cout << timestamp << "HDF5Kernel:SaveImage: Warning: unknown opencv type " << img.type() << std::endl;
+            }
+        }
+        m_hdf5File->flush();
+    
+    } 
+    else 
+    {
+        throw std::runtime_error("[Hdf5IO - ChannelIO]: Hdf5 file not open.");
+    }
 }
 
 void HDF5Kernel::saveMetaYAML(
