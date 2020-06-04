2bdfb6ea8c73b590c943f500b7bd90c17f10c752
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2020-06-04 15:23:42 +0200

    Added directory io

---
+++ include/lvr2/io/descriptions/DirectoryIO.hpp
@@ -0,0 +1,30 @@
+#ifndef DIRECTORYIO_HPP
+#define DIRECTORYIO_HPP
+
+#include "lvr2/io/descriptions/DirectoryKernel.hpp"
+#include "lvr2/io/descriptions/ScanProjectSchema.hpp"
+#include "lvr2/io/descriptions/FeatureBase.hpp"
+#include "lvr2/io/descriptions/ScanProjectIO.hpp"
+#include "lvr2/types/ScanTypes.hpp"
+
+namespace lvr2
+{
+
+class DirectoryIO
+{
+    DirectoryIO() = delete;
+    DirectoryIO(DirectoryKernelPtr kernel, DirectorySchemaPtr schema) : m_kernel(kernel), m_schema(schema) {}
+
+    void saveScanProject(ScanProjectPtr project);
+    ScanProjectPtr loadScanProject();
+
+private:
+    DirectoryKernelPtr m_kernel;
+    DirectorySchemaPtr m_schema;
+};
+
+using DirectoryIOPtr = std::shared_ptr<DirectoryIO>;
+
+} // namespace lvr2
+
+#endif
