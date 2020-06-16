f91eaeb0ea93907f5238260929e877be330e2745
Thomas Wiemann <twiemann@uni-osnabrueck.de>
2020-06-16 14:38:41 +0200

    First version of HDF5 schema V2

---
+++ src/liblvr2/io/descriptions/ScanProjectSchemaHDF5V2.cpp
@@ -0,0 +1,112 @@
+#include <sstream> 
+#include <iomanip>
+
+#include "lvr2/io/descriptions/ScanProjectSchemaHDF5V2.hpp"
+#include "lvr2/io/IOUtils.hpp"
+#include "lvr2/io/yaml/MatrixIO.hpp"
+#include "lvr2/types/ScanTypes.hpp"
+
+#include <boost/filesystem.hpp>
+
+namespace lvr2
+{
+
+Description ScanProjectSchemaHDF5V2::scanProject() const
+{
+    Description d;
+    d.groupName = "";           // All data is saved in the root dir
+    d.dataSetName = boost::none;    // No dataset name for project root
+    d.metaData = boost::none;       // No metadata for project 
+    return d;
+}
+Description ScanProjectSchemaHDF5V2::position(const size_t &scanPosNo) const
+{
+    Description d; 
+
+    // Group name
+    std::stringstream scan_stream;
+    scan_stream << "/raw/" << std::setw(8) << scanPosNo;
+    d.groupName = scan_stream.str();
+
+    // No dataset name 
+    d.dataSetName = boost::none;
+
+    // No meta data, currently handled by meta data description
+    d.metaData = boost::none;
+    d.metaName = boost::none;
+
+    return d;
+}
+
+Description ScanProjectSchemaHDF5V2::scan(const size_t &scanPosNo, const size_t &scanNo) const 
+{
+    // Group name
+    std::stringstream group_stream;
+    group_stream << "/raw/" << std::setw(8) << scanPosNo << "/scans/data/";
+  
+    return scan(group_stream.str(), scanNo);
+}
+
+Description ScanProjectSchemaHDF5V2::scan(const std::string& scanPositionPath, const size_t &scanNo) const
+{
+    Description d; 
+
+    d.groupName = scanPositionPath;
+
+    // Scan name
+    std::stringstream scan_stream;
+    scan_stream << std::setw(8) << scanNo;
+
+    d.dataSetName = scan_stream.str();
+
+    return d;
+}
+
+Description ScanProjectSchemaHDF5V2::scanCamera(const size_t &scanPositionNo, const size_t &camNo) const
+{
+    // Scan camera is not supported
+    Description d;
+    d.groupName = boost::none;
+    d.dataSetName = boost::none;
+    d.metaData = boost::none;
+    d.metaName = boost::none;
+    return d;
+}
+
+Description ScanProjectSchemaHDF5V2::scanCamera(const std::string &scanPositionPath, const size_t &camNo) const
+{
+    // Scan camera is not supported
+    Description d;
+    d.groupName = boost::none;
+    d.dataSetName = boost::none;
+    d.metaData = boost::none;
+    d.metaName = boost::none;
+    return d;
+}
+
+Description ScanProjectSchemaHDF5V2::scanImage(
+    const size_t &scanPosNo, const size_t &scanNo,
+    const size_t &scanCameraNo, const size_t &scanImageNo) const
+{
+    // Scan images are not supported
+    Description d;
+    d.groupName = boost::none;
+    d.dataSetName = boost::none;
+    d.metaData = boost::none;
+    d.metaName = boost::none;
+    return d;
+}
+
+Description ScanProjectSchemaHDF5V2::scanImage(
+    const std::string &scanImagePath, const size_t &scanImageNo) const
+{
+    // Scan images are not supported
+    Description d;
+    d.groupName = boost::none;
+    d.dataSetName = boost::none;
+    d.metaData = boost::none;
+    d.metaName = boost::none;
+    return d; 
+}
+
+} // namespace lvr2
