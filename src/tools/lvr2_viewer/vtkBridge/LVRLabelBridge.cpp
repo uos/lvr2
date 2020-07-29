072ee3db9c83d6597f351b487a474ec971df73a4
mloepmei <479-mloepmei@users.noreply.gitlab.informatik.uni-osnabrueck.de>
2020-07-29 20:20:45 +0200

    added the forgotten files

---
+++ src/tools/lvr2_viewer/vtkBridge/LVRLabelBridge.cpp
@@ -0,0 +1,53 @@
+#include "LVRLabelBridge.hpp"
+#include "lvr2/registration/TransformUtils.hpp"
+
+namespace lvr2
+{
+
+
+LVRLabelBridge::LVRLabelBridge(LabelRootPtr project) :
+    m_labelRoot(project)
+{
+    m_pointBridge = PointBufferBridgePtr(new LVRPointBufferBridge(project->points));
+}
+
+LVRLabelBridge::LVRLabelBridge(const LVRLabelBridge& b)
+{
+   m_labelRoot = b.m_labelRoot;
+   m_pointBridge = b.m_pointBridge;
+}
+
+bool LVRLabelBridge::validPointBridge()
+{
+    return (m_pointBridge->getNumPoints() > 0) ? true : false;
+}
+
+void LVRLabelBridge::addActors(vtkSmartPointer<vtkRenderer> renderer)
+{
+    if(validPointBridge())
+    {
+    	renderer->AddActor(m_pointBridge->getPointCloudActor());
+    }
+
+
+}
+
+void LVRLabelBridge::removeActors(vtkSmartPointer<vtkRenderer> renderer)
+{
+    if(validPointBridge()) renderer->RemoveActor(m_pointBridge->getPointCloudActor());
+}
+void LVRLabelBridge::setVisibility(bool visible)
+{
+    if(validPointBridge()) m_pointBridge->setVisibility(visible);
+}
+LabelRootPtr LVRLabelBridge::getLabelRoot()
+{
+    return m_labelRoot;
+}
+
+
+LVRLabelBridge::~LVRLabelBridge()
+{
+    // TODO Auto-generated destructor stub
+}
+}
