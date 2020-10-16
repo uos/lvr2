faedaa7915644f450a9f45a8e2749a7315bbb2b4
lennart <lniecksch@uni-osnabrueck.de>
2020-10-16 10:55:44 +0200

    use double for precision and refactor cl sor tool.

--- src/liblvr2/reconstruction/opencl/ClStatisticalOutlierFilter.cpp
+++ src/liblvr2/reconstruction/opencl/ClStatisticalOutlierFilter.cpp
@@ -156,13 +156,13 @@ void ClSOR::calcStatistics()
     // TODO PARALLELIZE VARIANCE AND MEAN
     for(int i = 0; i < (this->m_distances.dim * this->m_distances.width); i++)
     {
-        sum += this->m_distances.elements[i];
-        sq_sum += this->m_distances.elements[i] * this->m_distances.elements[i];
+        sum += static_cast<double>(this->m_distances.elements[i]);
+        sq_sum += static_cast<double>(this->m_distances.elements[i]) * static_cast<double>(this->m_distances.elements[i]);
     }
     // does this need casts
     std::cout  << "sum " << sum << "sq_sum " << sq_sum << std::endl;
-    m_mean_ = sum/this->m_distances.width;
-    m_std_dev_ = (sq_sum - sum * sum / this->m_distances.width) / (this->m_distances.width - 1);
+    m_mean_ = sum/static_cast<double>(m_distances.width);
+    m_std_dev_ = (sq_sum - sum * sum / static_cast<double>(this->m_distances.width)) / static_cast<double>((this->m_distances.width - 1));
     m_std_dev_ = std::sqrt(m_std_dev_);
     std::cout  << "Mean " << m_mean_ << "dev " << m_std_dev_ << std::endl;
 }
--- src/tools/lvr2_cl_sor/Main.cpp
+++ src/tools/lvr2_cl_sor/Main.cpp
@@ -37,48 +37,8 @@
 
 using namespace lvr2;
 
-
-int main(int argc, char** argv){
-    ModelPtr model = ModelFactory::readModel(argv[1]);
-    size_t num_points;
-
-    floatArr points;
-    if (model && model->m_pointCloud )
-    {
-        num_points = model->m_pointCloud->numPoints();
-        points = model->m_pointCloud->getPointArray();
-        cout << timestamp << "Read " << num_points << " points from " << argv[1] << endl;
-    }
-    else
-    {
-        cout << timestamp << "Warning: No point cloud data found in " << argv[1] << endl;
-        return 0;
-    }
-
-    lvr2::uintArr inlier = lvr2::uintArr(new unsigned int[num_points]);
-
-    cout << timestamp << "Constructing kd-tree..." << endl;
-    ClSOR sor(points, num_points, 500);
-    cout << timestamp << "Finished kd-tree construction." << endl;
-
-
-    sor.calcDistances();
-    cout << timestamp << "Got Nearest Neighbors" << std::endl;
-    sor.calcStatistics();
-    cout << timestamp << "Got Statistics" << std::endl;
-    sor.setMult(0.25);
-
-    int j  = sor.getInliers(inlier);
-
-    std::cout << timestamp << "outliers " << num_points - j << std::endl;
-    std::cout << timestamp << "inliers " << j << std::endl;
-//    for(size_t i = 0; i < j; ++i)
-//    {
-//        size_t index = inlier[j];
-//        std::cout << index<< std::endl;
-//    }
-
-    lvr2::PointBufferPtr cloud = model->m_pointCloud;
+void filter(lvr2::PointBufferPtr& cloud, lvr2::indexArray& inlier, size_t j)
+{
     lvr2::PointBufferPtr cloud_filtered(new lvr2::PointBuffer());
     int type;
     std::map<std::string, lvr2::Channel<float> > floatChannels;
@@ -124,20 +84,123 @@ int main(int argc, char** argv){
 //        cloud->removeUCharChannel(name);
         cloud_filtered->addUCharChannel(filtered, name);
     }
-    lvr2::ModelPtr model_filtered(new lvr2::Model(cloud_filtered, lvr2::MeshBufferPtr()));
-    lvr2::ModelFactory::saveModel(model_filtered, "filtered.ply");
 
-    //sor.finalizeCl();
+    cloud = cloud_filtered;
+}
+
+int main(int argc, char** argv){
+    ModelPtr model = ModelFactory::readModel(argv[1]);
+    size_t num_points;
 
 
-//    gpu_surface.getNormals(normals);
-//    cout << timestamp << "Finished Normal Calculation. " << endl;
+    UCharChannelOptional colorsOpt = model->m_pointCloud->getUCharChannel("colors"); 
+
+    // filter based on grayscale from rgb
+    // 
+//    if(colorsOpt)
+//    {
+//     		lvr2::uintArr inlier2 = lvr2::uintArr(new unsigned int[model->m_pointCloud->numPoints()]);
+//            std::cout << timestamp << "filter based on grayscale" << std::endl;
+//            size_t k = 0;
+//            for(size_t i = 0; i < colorsOpt->numElements(); ++i)
+//            {
+//        	    int scalar = (*colorsOpt)[i][0] +
+//        		         (*colorsOpt)[i][1] +
+//        		         (*colorsOpt)[i][2];
+//        	    scalar /= 3;
+//        	    if(scalar < 255)
+//        	    {
+//        		inlier2[k++] = i;
+//        	    }
+//            }
 //
-//    buffer->setPointArray(points, num_points);
-//    buffer->setNormalArray(normals, num_points);
-//
-//    gpu_surface.freeGPU();
+//	    std::cout << timestamp << "outliers " << model->m_pointCloud->numPoints() - k << std::endl;
+//	    std::cout << timestamp << "inliers " << k << std::endl;
+//	    filter(model->m_pointCloud, inlier2, k);
+//    }
 
+    floatArr points;
+    if (model && model->m_pointCloud )
+    {
+        num_points = model->m_pointCloud->numPoints();
+        points = model->m_pointCloud->getPointArray();
+        cout << timestamp << "Read " << num_points << " points from " << argv[1] << endl;
+    }
+    else
+    {
+        cout << timestamp << "Warning: No point cloud data found in " << argv[1] << endl;
         return 0;
+    }
+
+    lvr2::uintArr inlier = lvr2::uintArr(new unsigned int[num_points]);
+
+    cout << timestamp << "Constructing kd-tree..." << endl;
+    ClSOR sor(points, num_points, 20);
+    cout << timestamp << "Finished kd-tree construction." << endl;
+
+
+    sor.calcDistances();
+    cout << timestamp << "Got Nearest Neighbors" << std::endl;
+    sor.calcStatistics();
+    cout << timestamp << "Got Statistics" << std::endl;
+    sor.setMult(1.5);
+
+    int j  = sor.getInliers(inlier);
+
+    std::cout << timestamp << "outliers " << num_points - j << std::endl;
+    std::cout << timestamp << "inliers " << j << std::endl;
+    
+    filter(model->m_pointCloud, inlier, j);
+
+//    lvr2::PointBufferPtr cloud = model->m_pointCloud;
+//    lvr2::PointBufferPtr cloud_filtered(new lvr2::PointBuffer());
+//    int type;
+//    std::map<std::string, lvr2::Channel<float> > floatChannels;
+//    type = cloud->getAllChannelsOfType<float>(floatChannels);
+//
+//    std::vector<lvr2::FloatChannelPtr> channels;
+//    for(auto channelPair: floatChannels)
+//    {
+//        std::string name = channelPair.first;
+//        auto channel2 = channelPair.second;
+//        lvr2::FloatChannelPtr filtered(new lvr2::FloatChannel(j, channel2.width()));
+//        channels.push_back(filtered);
+//        for(size_t i = 0; i < j; ++i)
+//        {
+//            size_t index = inlier[i];
+//            for(size_t k = 0; k < channel2.width(); ++k)
+//            {
+//                (*filtered)[i][k] = channel2[index][k];
+//            }
+//        }
+////        cloud->removeFloatChannel(name);
+//        cloud_filtered->addFloatChannel(filtered, name);
+//        
+//    }
+//    
+//    std::vector<lvr2::UCharChannelPtr> channels_uchar;
+//    std::map<std::string, lvr2::Channel<unsigned char> >  uCharChannels;
+//    int ucharType = cloud->getAllChannelsOfType<unsigned char>(uCharChannels);
+//    for(auto channelPair: uCharChannels)
+//    {
+//        std::string name = channelPair.first;
+//        auto channel2 = channelPair.second;
+//        lvr2::UCharChannelPtr filtered(new lvr2::UCharChannel(j, channel2.width()));
+//        channels_uchar.push_back(filtered);
+//        for(size_t i = 0; i < j; ++i)
+//        {
+//            size_t index = inlier[i];
+//            for(size_t k = 0; k < channel2.width(); ++k)
+//            {
+//                (*filtered)[i][k] = channel2[index][k];
+//            }
+//        }
+////        cloud->removeUCharChannel(name);
+//        cloud_filtered->addUCharChannel(filtered, name);
+//    }
+//    lvr2::ModelPtr model_filtered(new lvr2::Model(cloud_filtered, lvr2::MeshBufferPtr()));
+    lvr2::ModelFactory::saveModel(model, argv[2]);
+
+    return 0;
 
 }
