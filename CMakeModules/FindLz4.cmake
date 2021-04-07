018c7899f8c8c0a409bbdafd18c3ed7ca475bbb1
lniecksch <lniecksch@uni-osnabrueck.de>
2021-04-07 10:33:03 +0200

    use findlz4 from vtk.

--- CMakeModules/FindLz4.cmake
+++ CMakeModules/FindLz4.cmake
@@ -1,83 +1,38 @@
-# Copyright 2012 Cloudera Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+find_path(LZ4_INCLUDE_DIR
+  NAMES lz4.h
+  DOC "lz4 include directory")
+mark_as_advanced(LZ4_INCLUDE_DIR)
+find_library(LZ4_LIBRARY
+  NAMES lz4 liblz4
+  DOC "lz4 library")
+mark_as_advanced(LZ4_LIBRARY)
 
-# - Find LZ4 (lz4.h, liblz4.a, liblz4.so, and liblz4.so.1)
-# This module defines
-#  LZ4_INCLUDE_DIR, directory containing headers
-#  LZ4_LIBS, directory containing lz4 libraries
-#  LZ4_STATIC_LIB, path to liblz4.a
-#  LZ4_LIBRARY, path to liblz4.so
-#  LZ4_FOUND, whether lz4 has been found
-
-set(LZ4_SEARCH_HEADER_PATHS
-  ${THIRDPARTY_PREFIX}/include
-  /usr/include
-  /usr/local/include
-)
-
-set(LZ4_SEARCH_LIB_PATH
-  ${THIRDPARTY_PREFIX}/lib
-  /usr/local/lib
-  /usr/lib/
-  /usr/lib/x86_64-linux-gnu/
-  /usr/lib/aarch64-linux-gnu/
-  /usr/lib/arm-linux-gnueabihf/
-)
-
-find_path(LZ4_INCLUDE_DIR lz4.h PATHS
-  ${LZ4_SEARCH_HEADER_PATHS}
-  # make sure we don't accidentally pick up a different version
-  NO_DEFAULT_PATH
-)
-
-find_library(LZ4_LIB_PATH NAMES liblz4.so liblz4.dylib PATHS ${LZ4_SEARCH_LIB_PATH} NO_DEFAULT_PATH)
-
-message(STATUS "Header ${LZ4_INCLUDE_DIR} LIB ")
-if (LZ4_INCLUDE_DIR AND LZ4_LIB_PATH)
-  set(LZ4_FOUND TRUE)
-  set(LZ4_LIBS ${LZ4_SEARCH_LIB_PATH})
-  set(LZ4_STATIC_LIB ${LZ4__LIB_PATH}/liblz4.a)
-
-  # mac os is using dylib instead of so for lz4
-  if (APPLE)
-    set(LZ4_LIBRARY liblz4.dylib)
-  else ()
-    set(LZ4_LIBRARY liblz4.so)
-  endif ()
-else ()
-  set(LZ4_FOUND FALSE)
+if (LZ4_INCLUDE_DIR)
+  file(STRINGS "${LZ4_INCLUDE_DIR}/lz4.h" _lz4_version_lines
+    REGEX "#define[ \t]+LZ4_VERSION_(MAJOR|MINOR|RELEASE)")
+  string(REGEX REPLACE ".*LZ4_VERSION_MAJOR *\([0-9]*\).*" "\\1" _lz4_version_major "${_lz4_version_lines}")
+  string(REGEX REPLACE ".*LZ4_VERSION_MINOR *\([0-9]*\).*" "\\1" _lz4_version_minor "${_lz4_version_lines}")
+  string(REGEX REPLACE ".*LZ4_VERSION_RELEASE *\([0-9]*\).*" "\\1" _lz4_version_release "${_lz4_version_lines}")
+  set(LZ4_VERSION "${_lz4_version_major}.${_lz4_version_minor}.${_lz4_version_release}")
+  unset(_lz4_version_major)
+  unset(_lz4_version_minor)
+  unset(_lz4_version_release)
+  unset(_lz4_version_lines)
 endif ()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LZ4
+  REQUIRED_VARS LZ4_LIBRARY LZ4_INCLUDE_DIR
+  VERSION_VAR LZ4_VERSION)
 
 if (LZ4_FOUND)
-  if (NOT Lz4_FIND_QUIETLY)
-    message(STATUS "Found the Lz4 library: ${LZ4_LIB_PATH}")
-  endif ()
-else ()
-  if (NOT Lz4_FIND_QUIETLY)
-    set(LZ4_ERR_MSG "Could not find the Lz4 library. Looked for headers")
-    set(LZ4_ERR_MSG "${LZ4_ERR_MSG} in ${LZ4_SEARCH_HEADER_PATHS}, and for libs")
-    set(LZ4_ERR_MSG "${LZ4_ERR_MSG} in ${LZ4_SEARCH_LIB_PATH}")
-    if (Lz4_FIND_REQUIRED)
-      message(FATAL_ERROR "${LZ4_ERR_MSG}")
-    else (Lz4_FIND_REQUIRED)
-      message(STATUS "${LZ4_ERR_MSG}")
-    endif (Lz4_FIND_REQUIRED)
+  set(LZ4_INCLUDE_DIRS "${LZ4_INCLUDE_DIR}")
+  set(LZ4_LIBRARIES "${LZ4_LIBRARY}")
+
+  if (NOT TARGET LZ4::LZ4)
+    add_library(LZ4::LZ4 UNKNOWN IMPORTED)
+    set_target_properties(LZ4::LZ4 PROPERTIES
+      IMPORTED_LOCATION "${LZ4_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${LZ4_INCLUDE_DIR}")
   endif ()
 endif ()
-
-mark_as_advanced(
-  LZ4_INCLUDE_DIR
-  LZ4_LIBS
-  LZ4_STATIC_LIB
-)
