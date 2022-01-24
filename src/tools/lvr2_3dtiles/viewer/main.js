63dcfd2ff6a34de58de1b6d4b15abef8376a87a3
mhillmann <mhillmann@uos.de>
2022-01-24 11:17:18 +0100

    Added preliminary 3dtiles viewer

---
+++ src/tools/lvr2_3dtiles/viewer/main.js
@@ -0,0 +1,48 @@
+
+class TilingScheme extends Cesium.GeographicTilingScheme {
+    getNumberOfXTilesAtLevel(level) {
+        return super.getNumberOfXTilesAtLevel(Math.min(level, 10));
+    }
+    getNumberOfYTilesAtLevel(level) {
+        return super.getNumberOfYTilesAtLevel(Math.min(level, 10));
+    }
+}
+
+var viewer = new Cesium.Viewer("cesiumContainer", {
+    vrButton: true,
+    sceneModePicker: false,
+    baseLayerPicker: false,
+    geocoder: false,
+    timeline: false,
+    infoBox: false,
+    animation: false,
+    scene3DOnly: true,
+    imageryProvider: new Cesium.GridImageryProvider({
+        tilingScheme: new TilingScheme(),
+    }),
+});
+window['viewer'] = viewer;
+
+var url = "../../../../build/chunk.3dtiles/tileset.json";
+
+var tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url,
+        debugShowBoundingVolume: true,
+        debugShowRenderingStatistics: true,
+        debugShowMemoryUsage: true,
+        backFaceCulling: false,
+    })
+);
+
+tileset.readyPromise
+    .then(function () {
+        viewer.zoomTo(tileset);
+        viewer.homeButton.viewModel.command.beforeExecute.addEventListener(function (e) {
+            viewer.zoomTo(tileset);
+            e.cancel = true;
+        });
+    })
+    .otherwise(function (error) {
+        throw error;
+    });
