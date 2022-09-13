8b3b96247094008db11a8411999143bf1771a67b
Lukas <lhoene@ous.de>
2022-09-13 16:05:45 +0200

    Erste Schritte von ScanProjectSchemaRdbx (Wichtge Frage welche Mehtoden sind wirklich nötig)

---
+++ include/lvr2/io/schema/ScanProjectSchemaRdbx.hpp
@@ -0,0 +1,94 @@
+//
+// Created by praktikum on 13.09.22.
+//
+
+#ifndef LAS_VEGAS_SCANPROJECTSCHEMARDBX_HPP
+#define LAS_VEGAS_SCANPROJECTSCHEMARDBX_HPP
+#include <string>
+
+#include <boost/optional.hpp>
+#include <boost/filesystem.hpp>
+
+#include "lvr2/io/schema/ScanProjectSchema.hpp"
+
+namespace lvr2
+{
+
+    class ScanProjectSchemaRdbx : public DirectorySchema
+            {
+            public:
+                ScanProjectSchemaRdbx(const std::string& rootDir) : DirectorySchema( rootDir){};
+                ~ScanProjectSchemaRdbx() = default;
+                virtual Description scanProject() const;
+
+
+
+        virtual Description position(
+                const size_t& scanPosNo) const;
+
+        virtual Description lidar(
+                const size_t& scanPosNo,
+                const size_t& lidarNo) const;
+
+        virtual Description camera( const size_t& scanPosNo,
+        const size_t& camNo) const;
+
+
+        virtual Description scan(
+                const size_t& scanPosNo,
+                const size_t& lidarNo,
+                const size_t& scanNo) const;
+
+        virtual Description scanChannel(
+                const size_t& scanPosNo,
+                const size_t& lidarNo,
+                const size_t& scanNo,
+                const std::string& channelName) const;
+
+        // virtual std::string scanChannelInv(
+        //     const std::string& d_data) const;
+
+        virtual Description camera(
+                const size_t& scanPosNo,
+                const size_t& camNo) const;
+
+        virtual Description cameraImage(
+                const size_t& scanPosNo,
+                const size_t& camNo,
+                const std::vector<size_t>& cameraImageNos) const;
+
+        virtual Description cameraImageGroup(
+                const size_t& scanPosNo,
+                const size_t& camNo,
+                const std::vector<size_t>& cameraImageGroupNos) const;
+
+        virtual Description hyperspectralCamera(
+                const size_t& scanPosNo,
+                const size_t& camNo) const;
+
+        virtual Description hyperspectralPanorama(
+                const size_t& scanPosNo,
+                const size_t& camNo,
+                const size_t& panoNo) const;
+
+        virtual Description hyperspectralPanoramaPreview(
+                const size_t& scanPosNo,
+                const size_t& camNo,
+                const size_t& panoNo) const;
+
+        virtual Description hyperspectralPanoramaChannel(
+                const size_t& scanPosNo,
+                const size_t& camNo,
+                const size_t& panoNo,
+                const size_t& channelNo) const;
+
+
+
+
+    };
+
+
+
+} // lvr2
+
+#endif //LAS_VEGAS_SCANPROJECTSCHEMARDBX_HPP
---
+++ src/liblvr2/io/schema/ScanProjectSchemaRdbx.cpp
@@ -0,0 +1,175 @@
+//
+// Created by praktikum on 13.09.22.
+//
+#include <sstream>
+#include <iomanip>
+
+#include "lvr2/types/ScanTypes.hpp"
+#include "lvr2/io/schema/ScanProjectSchemaRdbx.hpp"
+
+namespace lvr2 {
+
+    Description ScanProjectSchemaRdbx::scanProject() const
+    {
+        Description d;
+
+        d.dataRoot = "scans";
+
+        d.metaRoot = d.dataRoot;
+        d.meta = "final.pose";
+
+        return d;
+    }
+    Description ScanProjectSchemaRdbx::position(
+            const size_t &scanPosNo) const
+    {
+        std::stringstream sstr;
+
+
+        Description dp = scanProject();
+        Description d;
+        d.dataRoot = *dp.dataRoot +"/final.pose";
+
+        d.metaRoot = d.dataRoot;
+        d.meta = "final.pose";
+
+        return d;
+    }
+    Description ScanProjectSchemaRdbx::lidar(
+            const size_t& scanPosNo,
+            const size_t& lidarNo) const
+    {
+        std::stringstream sstr;
+        sstr << lidarNo;
+
+        Description dp = position(scanPosNo);
+
+        Description d;
+        d.dataRoot = *dp.dataRoot + "/scan" + sstr.str();
+        return d;
+    }
+    Description ScanProjectSchemaRdbx::camera(
+            const size_t& scanPosNo,
+            const size_t& camNo) const
+    {
+        std::stringstream sstr;
+        sstr << camNo;
+
+        Description dp = position(scanPosNo);
+
+        Description d;
+        d.dataRoot = *dp.dataRoot + "/images/" + sstr.str();
+
+        d.metaRoot = d.dataRoot;
+        d.meta = "final.pose";
+
+
+        return d;
+    }
+
+    Description ScanProjectSchemaRdbx::scan(
+            const size_t& scanPosNo,
+            const size_t& lidarNo,
+            const size_t& scanNo) const
+    {
+        std::stringstream sstr;
+        sstr <<"scans/"<< scanNo;
+
+        Description dp = lidar(scanPosNo, lidarNo);
+
+        Description d;
+        d.dataRoot = *dp.dataRoot + "/" + sstr.str();
+
+        d.metaRoot = d.dataRoot;
+        d.meta = "final.pose";
+
+        return d;
+    }
+    Description ScanProjectSchemaRdbx::scanChannel(
+            const size_t& scanPosNo,
+            const size_t& lidarNo,
+            const size_t& scanNo,
+            const std::string& channelName) const
+    {
+        Description d;
+
+
+
+        return d;
+    }
+
+    Description ScanProjectSchemaRdbx::cameraImage(
+            const size_t& scanPosNo,
+            const size_t& camNo,
+            const std::vector<size_t>& cameraImageNos) const
+    {
+
+
+        Description d;
+
+
+        return d;
+    }
+
+    Description ScanProjectSchemaRdbx::cameraImageGroup(
+            const size_t& scanPosNo,
+            const size_t& camNo,
+            const std::vector<size_t>& cameraImageGroupNos) const
+    {
+
+
+        Description d;
+
+
+        return d;
+    }
+
+    Description ScanProjectSchemaRdbx::hyperspectralPanorama(
+            const size_t& scanPosNo,
+            const size_t& camNo,
+            const size_t& panoNo) const
+    {
+
+
+        Description d;
+
+
+        return d;
+    }
+
+    Description ScanProjectSchemaRdbx::hyperspectralPanoramaPreview(
+            const size_t& scanPosNo,
+            const size_t& camNo,
+            const size_t& panoNo) const
+    {
+
+        Description d;
+
+
+        return d;
+    }
+
+    Description ScanProjectSchemaRdbx::hyperspectralPanoramaChannel(
+            const size_t& scanPosNo,
+            const size_t& camNo,
+            const size_t& panoNo,
+            const size_t& channelNo
+    ) const
+    {
+
+
+        Description d;
+
+
+        return d;
+    }
+
+
+
+
+
+
+
+
+
+} // lvr2
