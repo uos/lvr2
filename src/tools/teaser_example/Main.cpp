e28b6a84b7915b3cefee20f281f85d2030624b68
ssenko <ssenko@uos.de>
2022-09-21 14:04:15 +0200

    method voxeldownsample plus fpfh

--- src/tools/teaser_example/Main.cpp
+++ src/tools/teaser_example/Main.cpp
@@ -223,6 +223,38 @@ void workflowDirectlyWithISSAndDownSampling(std::string src_path, std::string ta
     solveTeaserWithoutCorrespondences(src_eigen, target_eigen);
 }
 
+void workflowCorrespondencesAndDownSampling(std::string src_path, std::string target_path) {
+
+    geometry::PointCloud src_cloud = readAndPreprocessPointCloud(src_path);
+    geometry::PointCloud target_cloud = readAndPreprocessPointCloud(target_path);
+
+    geometry::PointCloud sampled_src_cloud = *src_cloud.VoxelDownSample(0.5);
+    geometry::PointCloud sampled_target_cloud = *target_cloud.VoxelDownSample(0.5);
+    std::cout << "Voxel source size: " << sampled_src_cloud.points_.size() << std::endl;
+    std::cout << "Voxel target size: " << sampled_target_cloud.points_.size() << std::endl;
+
+    geometry::PointCloud src_iss_cloud = computeISSPointClouds(sampled_src_cloud);
+    geometry::PointCloud target_iss_cloud = computeISSPointClouds(sampled_target_cloud);
+
+    auto src_feature = computeFPFHs(src_iss_cloud);
+    auto target_feature = computeFPFHs(target_iss_cloud);
+
+    std::cout << "Computing correspondo... ";
+    std::vector<std::pair<int, int>> correspondences = MyMatching(target_feature, src_feature);
+    std::cout << "- done." << std::endl;
+
+    // convert Open3D cloud i
+    teaser::PointCloud src_teaser_cloud = convertToTeaserCloud(src_iss_cloud);
+    teaser::PointCloud target_teaser_cloud = convertToTeaserCloud(target_iss_cloud);
+
+    // convert pointcloud to Eigen3 Matrix to use teaser::solver without correspondences
+
+//    Eigen::Matrix<double, 3, Eigen::Dynamic> src_eigen = convertToEigen(src_teaser_cloud);
+//    Eigen::Matrix<double, 3, Eigen::Dynamic> target_eigen = convertToEigen(target_teaser_cloud);
+
+    solveTeaserWithCorrespondences(src_teaser_cloud, target_teaser_cloud, correspondences);
+}
+
 int main() {
     // file paths
 //    std::string source_path = "/home/praktikum/robopraktikum/scans_test/001/scans/220720_095525.ply";
@@ -244,5 +276,6 @@ int main() {
 //    int stat2 = pointtovertex(target_path, target_path_vertex);
 
 //    workflowCorrespondences(source_path, target_path);
-    workflowDirectlyWithISSAndDownSampling(source_path, target_path);
+//    workflowDirectlyWithISSAndDownSampling(source_path, target_path);
+    workflowCorrespondencesAndDownSampling(source_path, target_path);
 }
