916d777ea21bfa0bd0dcd71104f780fb53d15773
Roho1999 <rofeldmann@uni-osnabrueck.de>
2022-09-22 14:30:42 +0200

    kleine änderungen zum überschreiben

--- src/tools/teaser_example/util/pointtovertex.cpp
+++ src/tools/teaser_example/util/pointtovertex.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
 
 
 using namespace std;
@@ -25,30 +26,35 @@ s.replace(pos, toReplace.length(), replaceWith);
 
 //saves vertex format in destination path
 int pointtovertex(string src, string dest) {
-    string strReplace = "point";
-    string strNew = "vertex";
-    ifstream filein(src); //File to read from
-    ofstream fileout(dest); //File to write to
 
-    //case file error
-    if (!filein || !fileout) {
-        cout << "Error opening files!" << endl;
-        return 1;
-    }
+    if(!std::filesystem::exists(dest)) {
+        string strReplace = "point";
+        string strNew = "vertex";
+        ifstream filein(src); //File to read from
+        ofstream fileout(dest); //File to write to
 
-
-    string  strTemp;
-    bool found = false;
-    std::string line;
-    size_t i = 0;
-    while (getline(filein, line)) {
-        if (i < 10) {
-            replace_first(line, "point", "vertex");
+        //case file error
+        if (!filein || !fileout) {
+            cout << "Error opening files!" << endl;
+            return 1;
         }
-        fileout << line << "\n";
-        i++;
 
 
+        string strTemp;
+        bool found = false;
+        std::string line;
+        size_t i = 0;
+        while (getline(filein, line)) {
+            if (i < 10) {
+                replace_first(line, "point", "vertex");
+            }
+            fileout << line << "\n";
+            i++;
+
+
+        }
     }
     return 0;
+
+
 }
