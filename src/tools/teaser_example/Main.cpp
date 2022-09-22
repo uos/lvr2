d63169615da5306a069c2fc6b2d0f12a3fb1796e
ssenko <ssenko@uos.de>
2022-09-22 10:01:15 +0200

    print and some cleaning

--- src/tools/teaser_example/Main.cpp
+++ src/tools/teaser_example/Main.cpp
@@ -70,6 +70,9 @@ pipelines::registration::Feature computeFPFHs(open3d::geometry::PointCloud iss_c
         std::cout <<  "Dimensions: " << feature.Dimension() << std::endl;
         std::cout <<  "Number: " << feature.Num() << std::endl;
 
+        std::cout << "feature.rows(): " << feature.data_.rows() << std::endl;
+        std::cout << "feature.cols(): " << feature.data_.cols() << std::endl;
+
         return feature;
 }
 
@@ -94,42 +97,6 @@ Eigen::Matrix<double, 3, Eigen::Dynamic> convertToEigen(teaser::PointCloud cloud
     std::cout << "- done." << std::endl;
     return eigen;
 
-}
-
-void solveTeaserWithCorrespondences(teaser::PointCloud src_cloud, teaser::PointCloud target_cloud, std::vector<std::pair<int, int>> correspondences) {
-    // Prepare solver parameters
-    std::cout << "Teaser solving... ";
-    teaser::RobustRegistrationSolver::Params params;
-    params.noise_bound = NOISE_BOUND;
-    params.cbar2 = 1;
-    params.estimate_scaling = false;
-    params.rotation_max_iterations = 100;
-    params.rotation_gnc_factor = 1.4;
-    params.rotation_estimation_algorithm =
-            teaser::RobustRegistrationSolver::ROTATION_ESTIMATION_ALGORITHM::GNC_TLS;
-    params.rotation_cost_threshold = 0.005;
-
-    // Solve with TEASER++
-    teaser::RobustRegistrationSolver solver(params);
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    solver.solve(src_cloud, target_cloud, correspondences);
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-    auto solution = solver.getSolution();
-
-
-    // Compare results
-    std::cout << "=====================================" << std::endl;
-    std::cout << "          TEASER++ Results           " << std::endl;
-    std::cout << "=====================================" << std::endl;
-    std::cout << "Estimated rotation: " << std::endl;
-    std::cout << solution.rotation << std::endl;
-    std::cout << "Estimated translation: " << std::endl;
-    std::cout << solution.translation << std::endl;
-    std::cout << "Time taken (s): "
-              << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() /
-                 1000000.0
-              << std::endl;
 }
 
 void solveTeaserWithoutCorrespondences(Eigen::Matrix<double, 3, Eigen::Dynamic> src_cloud, Eigen::Matrix<double, 3, Eigen::Dynamic> target_cloud) {
@@ -168,6 +135,42 @@ void solveTeaserWithoutCorrespondences(Eigen::Matrix<double, 3, Eigen::Dynamic> 
               << std::endl;
 }
 
+void solveTeaserWithCorrespondences(teaser::PointCloud src_cloud, teaser::PointCloud target_cloud, std::vector<std::pair<int, int>> correspondences) {
+    // Prepare solver parameters
+    std::cout << "Teaser solving... ";
+    teaser::RobustRegistrationSolver::Params params;
+    params.noise_bound = NOISE_BOUND;
+    params.cbar2 = 1;
+    params.estimate_scaling = false;
+    params.rotation_max_iterations = 100;
+    params.rotation_gnc_factor = 1.4;
+    params.rotation_estimation_algorithm =
+            teaser::RobustRegistrationSolver::ROTATION_ESTIMATION_ALGORITHM::GNC_TLS;
+    params.rotation_cost_threshold = 0.005;
+
+    // Solve with TEASER++
+    teaser::RobustRegistrationSolver solver(params);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    solver.solve(src_cloud, target_cloud, correspondences);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    auto solution = solver.getSolution();
+
+
+    // Compare results
+    std::cout << "=====================================" << std::endl;
+    std::cout << "          TEASER++ Results           " << std::endl;
+    std::cout << "=====================================" << std::endl;
+    std::cout << "Estimated rotation: " << std::endl;
+    std::cout << solution.rotation << std::endl;
+    std::cout << "Estimated translation: " << std::endl;
+    std::cout << solution.translation << std::endl;
+    std::cout << "Time taken (s): "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() /
+                 1000000.0
+              << std::endl;
+}
+
 void workflowCorrespondences(std::string src_path, std::string target_path) {
 
     geometry::PointCloud src_cloud = readAndPreprocessPointCloud(src_path);
@@ -183,21 +186,23 @@ void workflowCorrespondences(std::string src_path, std::string target_path) {
     auto src_feature = computeFPFHs(src_iss_cloud);
     auto target_feature = computeFPFHs(target_iss_cloud);
 
-    std::cout << "Computing correspondo... ";
-    std::vector<std::pair<int, int>> correspondences = MyMatching(target_feature, src_feature);
-    std::cout << "- done." << std::endl;
+//    std::cout << "src_feature number of rows"
 
-
-    // convert Open3D cloud i
-    teaser::PointCloud src_teaser_cloud = convertToTeaserCloud(src_iss_cloud);
-    teaser::PointCloud target_teaser_cloud = convertToTeaserCloud(target_iss_cloud);
-
-    // convert pointcloud to Eigen3 Matrix to use teaser::solver without correspondences
-
-//    Eigen::Matrix<double, 3, Eigen::Dynamic> src_eigen = convertToEigen(src_teaser_cloud);
-//    Eigen::Matrix<double, 3, Eigen::Dynamic> target_eigen = convertToEigen(target_teaser_cloud);
-
-    solveTeaserWithCorrespondences(src_teaser_cloud, target_teaser_cloud, correspondences);
+//    std::cout << "Computing correspondo... ";
+//    std::vector<std::pair<int, int>> correspondences = MyMatching(target_feature, src_feature);
+//    std::cout << "- done." << std::endl;
+//
+//
+//    // convert Open3D cloud i
+//    teaser::PointCloud src_teaser_cloud = convertToTeaserCloud(src_iss_cloud);
+//    teaser::PointCloud target_teaser_cloud = convertToTeaserCloud(target_iss_cloud);
+//
+//    // convert pointcloud to Eigen3 Matrix to use teaser::solver without correspondences
+//
+////    Eigen::Matrix<double, 3, Eigen::Dynamic> src_eigen = convertToEigen(src_teaser_cloud);
+////    Eigen::Matrix<double, 3, Eigen::Dynamic> target_eigen = convertToEigen(target_teaser_cloud);
+//
+//    solveTeaserWithCorrespondences(src_teaser_cloud, target_teaser_cloud, correspondences);
 }
 
 void workflowDirectlyWithISSAndDownSampling(std::string src_path, std::string target_path) {
@@ -205,13 +210,13 @@ void workflowDirectlyWithISSAndDownSampling(std::string src_path, std::string ta
     geometry::PointCloud src_cloud = readAndPreprocessPointCloud(src_path);
     geometry::PointCloud target_cloud = readAndPreprocessPointCloud(target_path);
 
-    geometry::PointCloud sampled_src_cloud = *src_cloud.VoxelDownSample(0.5);
-    geometry::PointCloud sampled_target_cloud = *target_cloud.VoxelDownSample(0.5);
+    geometry::PointCloud sampled_src_cloud = *src_cloud.VoxelDownSample(0.1);
+    geometry::PointCloud sampled_target_cloud = *target_cloud.VoxelDownSample(0.1);
     std::cout << "Voxel source size: " << sampled_src_cloud.points_.size() << std::endl;
     std::cout << "Voxel target size: " << sampled_target_cloud.points_.size() << std::endl;
 
-    sampled_src_cloud.EstimateNormals();
-    sampled_target_cloud.EstimateNormals();
+//    sampled_src_cloud.EstimateNormals();
+//    sampled_target_cloud.EstimateNormals();
 
     geometry::PointCloud src_iss_cloud = computeISSPointClouds(sampled_src_cloud);
     geometry::PointCloud target_iss_cloud = computeISSPointClouds(sampled_target_cloud);
@@ -276,24 +281,24 @@ void workflowCorrespondencesAndDownSamplingWithoutISS(std::string src_path, std:
     geometry::PointCloud target_cloud = readAndPreprocessPointCloud(target_path);
 
     double voxel_size = 1.0;
-    geometry::PointCloud sampled_src_cloud = *src_cloud.VoxelDownSample(voxel_size);
-    geometry::PointCloud sampled_target_cloud = *target_cloud.VoxelDownSample(voxel_size);
-    std::cout << "Voxel source size: " << sampled_src_cloud.points_.size() << std::endl;
-    std::cout << "Voxel target size: " << sampled_target_cloud.points_.size() << std::endl;
+//    geometry::PointCloud sampled_src_cloud = *src_cloud.VoxelDownSample(voxel_size);
+//    geometry::PointCloud sampled_target_cloud = *target_cloud.VoxelDownSample(voxel_size);
+//    std::cout << "Voxel source size: " << sampled_src_cloud.points_.size() << std::endl;
+//    std::cout << "Voxel target size: " << sampled_target_cloud.points_.size() << std::endl;
 
-    sampled_src_cloud.EstimateNormals();
-    sampled_target_cloud.EstimateNormals();
+    src_cloud.EstimateNormals();
+    target_cloud.EstimateNormals();
 
-    auto src_feature = computeFPFHs(sampled_src_cloud);
-    auto target_feature = computeFPFHs(sampled_target_cloud);
+    auto src_feature = computeFPFHs(src_cloud);
+    auto target_feature = computeFPFHs(target_cloud);
 
     std::cout << "Computing correspondo... ";
     std::vector<std::pair<int, int>> correspondences = MyMatching(target_feature, src_feature);
     std::cout << "- done." << std::endl;
 
     // convert Open3D cloud i
-    teaser::PointCloud src_teaser_cloud = convertToTeaserCloud(sampled_src_cloud);
-    teaser::PointCloud target_teaser_cloud = convertToTeaserCloud(sampled_target_cloud);
+    teaser::PointCloud src_teaser_cloud = convertToTeaserCloud(src_cloud);
+    teaser::PointCloud target_teaser_cloud = convertToTeaserCloud(target_cloud);
 
     solveTeaserWithCorrespondences(src_teaser_cloud, target_teaser_cloud, correspondences);
 }
@@ -318,8 +323,8 @@ int main() {
 //    int stat1 = pointtovertex(source_path, source_path_vertex);
 //    int stat2 = pointtovertex(target_path, target_path_vertex);
 
-//    workflowCorrespondences(source_path, target_path);
+    workflowCorrespondences(source_path, target_path);
 //    workflowDirectlyWithISSAndDownSampling(source_path, target_path);
 //    workflowCorrespondencesAndDownSampling(source_path, target_path);
-    workflowCorrespondencesAndDownSamplingWithoutISS(source_path, target_path);
+//    workflowCorrespondencesAndDownSamplingWithoutISS(source_path, target_path);
 }
