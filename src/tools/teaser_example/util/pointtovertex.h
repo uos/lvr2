1bc211d2c51c36c7a08b675d857fdbaf53c8c122
Roho1999 <rofeldmann@uni-osnabrueck.de>
2022-09-30 11:38:14 +0200

    pointtovertex aufräumen

--- src/tools/teaser_example/util/pointtovertex.h
+++ src/tools/teaser_example/util/pointtovertex.h
@@ -1,5 +1,5 @@
 //
-// Created by praktikum on 15.09.22.
+// Created by Gruppe teaser on 15.09.22.
 //
 
 #ifndef LAS_VEGAS_POINTTOVERTEX_H
@@ -10,9 +10,6 @@
 #include <string>
 
 
-//reads original ply files with point in header
-//and creates new .ply with vertex header
-
 
 //replaces the first instance of toReplace with replaceWith in given string
 void replace_first(
@@ -20,7 +17,13 @@ void replace_first(
         std::string const &toReplace,
         std::string const &replaceWith
 );
-//saves vertex format in destination path
+/**
+    * @brief reads original ply files with point in header and creates new .ply with vertex in header
+    *
+    * @param src, path to ply
+    * @param dest, path to new ply
+    * @return status int
+    */
 int pointtovertex(std::string src, std::string dest);
 
 #endif //LAS_VEGAS_POINTTOVERTEX_H
