f6102b242e97e05579b964ff295f89abe8e14041
Alexander Mock <amock@uos.de>
2024-12-06 11:42:02 +0100

    fixed broken example code in comment

--- include/lvr2/io/deprecated/hdf5/MeshIO.hpp
+++ include/lvr2/io/deprecated/hdf5/MeshIO.hpp
@@ -32,7 +32,7 @@ namespace hdf5features {
  * 
  * // writing
  * io.open("test.h5");
- * io.save("ames#include "lvr2/io/hdf5/HDF5FeatureBase.hpp"h", mesh);
+ * io.save("amesh", mesh);
  * 
  * // reading
  * mesh_in = io.loadMesh("amesh");
